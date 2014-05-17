8af4a371954c30f0e55c71f76e2c190eb242a991
Joe Bordes <joe@tsolucio.com>
2014-05-17 18:12:53 +0200

    branding: eliminate vtiger reference

--- modules/Settings/language/es_es.lang.php
+++ modules/Settings/language/es_es.lang.php
@@ -780,7 +780,7 @@ $mod_strings = array (
 
       'VTLIB_LBL_SELECT_PACKAGE_FILE'=>'Selecciona el Módulo Empaquetado',
       'VTLIB_LBL_FILE_LOCATION'=>'Ubicación fichero:',
-      'VTLIB_LBL_PACKAGE_FILE_HELP'=>'el módulo vtiger se empaqueta como un fichero zip.',
+      'VTLIB_LBL_PACKAGE_FILE_HELP'=>'un módulo se empaqueta como un fichero zip.',
       'VTLIB_LBL_IMPORT_FAILURE'=>'Error al importar',
       'VTLIB_LBL_INVALID_FILE'=>'Fichero de módulo inválido',
       'VTLIB_LBL_INVALID_IMPORT_TRY_AGAIN'=>'proporcionado para importar! Inténtalo de nuevo.',
