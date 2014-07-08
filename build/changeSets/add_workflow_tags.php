830cbfb9fe4489121e81ce53a23ae9919191cabc
Joe Bordes <joe@tsolucio.com>
2014-07-08 17:03:06 +0200

    fix one-off error checking for dependencies on delete in Tags workflow task

--- build/changeSets/add_workflow_tags.php
+++ build/changeSets/add_workflow_tags.php
@@ -41,7 +41,7 @@ class add_workflow_tags extends cbupdaterWorker {
 		if ($this->isApplied()) {
 			global $adb;
 			$result = $adb->pquery("SELECT * FROM `com_vtiger_workflowtasks` WHERE `task` like '%CBTagTask%'",array());
-			if ($result and $adb->num_rows($result)>1) {
+			if ($result and $adb->num_rows($result)>0) {
 				$this->sendMsg('<span style="font-size:large;weight:bold;">Workflows that use this task exist!! Please eliminate them before undoing this change.</span>');
 			} else {
 				$adb->pquery("DELETE FROM com_vtiger_workflow_tasktypes WHERE
