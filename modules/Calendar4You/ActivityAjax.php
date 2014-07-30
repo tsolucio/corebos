8ee06810141a6d850f93212d7839817b73851526
Joe Bordes <joe@tsolucio.com>
2014-07-30 22:19:22 +0200

    fixes #212: minicalendar throws fatal error

--- modules/Calendar4You/ActivityAjax.php
+++ modules/Calendar4You/ActivityAjax.php
@@ -15,6 +15,9 @@ require_once("modules/Calendar/calendarLayout.php");
 require_once("modules/Calendar4You/CalendarUtils.php");
 require_once('include/utils/utils.php');
 require_once("modules/Calendar/Calendar.php");
+require_once('include/logging.php');
+$cal_log =& LoggerManager::getLogger('calendar');
+$cal_log->debug("In Calendar4YouAjax file");
 
 $mysel = vtlib_purify($_REQUEST['view']);
 $calendar_arr = Array();
