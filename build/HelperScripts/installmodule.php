e39b11913539d72ffd18399d2ebf849206650254
Joe Bordes <joe@tsolucio.com>
2014-08-18 00:46:36 +0200

    Helper Scripts: add workflow entity method and install module

---
+++ build/HelperScripts/installmodule.php
@@ -0,0 +1,18 @@
+<?php
+// Turn on debugging level
+$Vtiger_Utils_Log = true;
+
+require_once('vtlib/Vtiger/Module.php');
+require_once('vtlib/Vtiger/Package.php');
+
+global $current_user,$adb;
+set_time_limit(0);
+ini_set('memory_limit','1024M');
+$current_user = new Users();
+$current_user->retrieveCurrentUserInfoFromFile(1); // admin
+$package = new Vtiger_Package();
+//$rdo = $package->importManifest('modules/cbupdater/manifest.xml');
+//$rdo = $package->importManifest('modules/Webforms/manifest.xml');
+//$package->initImport('TSEmail_540.zip', true);
+$rdo = $package->importManifest('include/language/it_it.manifest.xml');
+?>
