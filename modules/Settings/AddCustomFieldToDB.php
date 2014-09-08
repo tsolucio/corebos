00837a97af1b40e84e97a2ae5632e781b4bb0e1f
Joe Bordes <joe@tsolucio.com>
2014-09-09 00:02:29 +0200

    fixes #122: Currency fields are badly rounded

--- modules/Settings/AddCustomFieldToDB.php
+++ modules/Settings/AddCustomFieldToDB.php
@@ -134,6 +134,7 @@ if($adb->num_rows($checkresult) > 0) {
 	elseif($fldType == 'Currency')
 	{
 		$uitype = 71;
+		if ($decimal<2) $decimal=2;
 		$dbfldlength = $fldlength + $decimal + 1;
 		$type="N(".$dbfldlength.".".$decimal.")"; //adodb type
 	$uichekdata='N~O~'.$fldlength .','.$decimal;
