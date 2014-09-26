e049f2c145810ac3bb59c41edbff3308a29bf1df
loridacito <l.cito@studiosynthesis.biz>
2014-09-26 14:08:07 +0200

    fixes

--- include/utils/CommonUtils.php
+++ include/utils/CommonUtils.php
@@ -3373,17 +3373,17 @@ function getReturnPath($host, $from_email) {
 	return $returnpath;
 }
 
-function fetch_logo($type)
-{
-global $adb;
-        $logodir ="test/logo/";
-        $sql="select * from vtiger_organizationdetails";
-        $result = $adb->pquery($sql, array());
-        if($type == 1) $logoname = decode_html($adb->query_result($result,0,'logoname'));
-        if($type == 2) $logoname = decode_html($adb->query_result($result,0,'frontlogo'));
-        if($type == 3) $logoname = decode_html($adb->query_result($result,0,'faviconlogo'));
-	return $logodir.$logoname;
-}
+//function fetch_logo($type)
+//{
+//global $adb;
+//        $logodir ="test/logo/";
+//        $sql="select * from vtiger_organizationdetails";
+//        $result = $adb->pquery($sql, array());
+//        if($type == 1) $logoname = decode_html($adb->query_result($result,0,'logoname'));
+//        if($type == 2) $logoname = decode_html($adb->query_result($result,0,'frontlogo'));
+//        if($type == 3) $logoname = decode_html($adb->query_result($result,0,'faviconlogo'));
+//	return $logodir.$logoname;
+//}
 
 function picklistHasDependency($keyfldname,$modulename) {
 	global $adb;
