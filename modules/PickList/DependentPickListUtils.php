cd8181c77c66814c28f54cbba7a7e62b2cb24552
Omar Llorens <omarllorens@gmail.com>
2014-11-19 00:22:20 +0100

    Fix picklist values for non admin users

--- modules/PickList/DependentPickListUtils.php
+++ modules/PickList/DependentPickListUtils.php
@@ -179,7 +179,7 @@ class Vtiger_DependencyPicklist {
 			}
 			if(empty($picklistDependencyDatasource[$sourceField]['__DEFAULT__'][$targetField])) {
 				foreach(getAllPicklistValues($targetField) as $picklistValue) {
-					$pickArray[] = $picklistValue;
+					$pickArray[] = decode_html($picklistValue);
 				}
 				$picklistDependencyDatasource[$sourceField]['__DEFAULT__'][$targetField] = $pickArray;
 			}
--- modules/PickList/PickListUtils.php
+++ modules/PickList/PickListUtils.php
@@ -212,8 +212,8 @@ function getAssignedPicklistValues($tableName, $roleid, $adb, $lang=array()){
 		
 		if($count) {
 			while($resultrow = $adb->fetch_array($result)) {
-				$pick_val = $resultrow[$tableName];
-				
+				$pick_val = decode_html($resultrow[$tableName]);
+				//$pick_val = decode_html($pick_val);  // we have to do it twice for it to work on listview!!
 				if($lang[$pick_val] != '') {
 					$arr[$pick_val] = $lang[$pick_val];
 				}
