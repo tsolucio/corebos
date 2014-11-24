2a2c8e29710cb6cde49fc93498f6683192c65701
root <root@drivalda-HP-500B-Microtower.(none)>
2014-11-24 11:10:53 +0100

    addinglangfiles1

---
+++ modules/Contacts/language/it_it.lang.php
@@ -0,0 +1,236 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header: /advent/projects/wesat/vtiger_crm/sugarcrm/modules/Contacts/language/en_us.lang.php,v 1.14 2005/03/24 17:47:43 rank Exp $
+ * Description:  Defines the English language pack for Contacts Module
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+$mod_strings = array (
+  'LBL_MODULE_NAME' => 'Contatti',
+  'LBL_INVITEE' => 'Riporta direttamente a',
+  'LBL_MODULE_TITLE' => 'Contatti: Home',
+  'LBL_SEARCH_FORM_TITLE' => 'Contatti Cerca',
+  'LBL_LIST_FORM_TITLE' => 'Lista Contatti',
+  'LBL_NEW_FORM_TITLE' => 'Nuovo Contatto',
+  'LBL_CONTACT_OPP_FORM_TITLE' => 'Contatto-Opportunit&agrave;',
+  'LBL_CONTACT' => 'Contatto:',
+
+  'LBL_LIST_NAME' => 'Nome',
+  'LBL_LIST_LAST_NAME' => 'Cognome',
+  'LBL_LIST_FIRST_NAME' => 'Nome',
+  'LBL_LIST_CONTACT_NAME' => 'Nome Contatto',
+  'LBL_LIST_TITLE' => 'Titolo',
+  'LBL_LIST_ACCOUNT_NAME' => 'Nome Azienda',
+  'LBL_LIST_EMAIL_ADDRESS' => 'Email',
+  'LBL_LIST_PHONE' => 'Telefono',
+  'LBL_LIST_CONTACT_ROLE' => 'Ruolo',
+
+
+  'db_last_name' => 'LBL_LIST_LAST_NAME',
+  'db_first_name' => 'LBL_LIST_FIRST_NAME',
+  'db_title' => 'LBL_LIST_TITLE',
+  'db_email1' => 'LBL_LIST_EMAIL_ADDRESS',
+  'db_email2' => 'LBL_LIST_EMAIL_ADDRESS',
+
+
+  'LBL_EXISTING_CONTACT' => 'Utilizza un contatto esistente',
+  'LBL_CREATED_CONTACT' => 'Crea un nuovo contatto',
+  'LBL_EXISTING_ACCOUNT' => 'Utilizza un`Azienda esistente',
+  'LBL_CREATED_ACCOUNT' => 'Crea una nuova Azienda',
+  'LBL_CREATED_CALL' => 'Crea una nuova chiamata',
+  'LBL_CREATED_MEETING' => 'Crea un nuovo meeting',
+  'LBL_ADDMORE_BUSINESSCARD' => 'Aggiungi un altro biglietto da visita',
+
+  'LBL_BUSINESSCARD' => 'Biglietto da visita',
+
+  'LBL_NAME' => 'Nome:',
+  'LBL_CONTACT_NAME' => 'Nome Contatto:',
+  'LBL_CONTACT_INFORMATION' => 'Informazioni Contatto',
+  'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+  'LBL_FIRST_NAME' => 'Nome:',
+  'LBL_OFFICE_PHONE' => 'Telefono Ufficio:',
+  'LBL_ACCOUNT_NAME' => 'Nome Azienda:',
+  'LBL_ANY_PHONE' => 'Qualsiasi Telefono:',
+  'LBL_PHONE' => 'Telefono',
+  'LBL_LAST_NAME' => 'Cognome:',
+  'LBL_MOBILE_PHONE' => 'Cellulare:',
+  'LBL_HOME_PHONE' => 'Home:',
+  'LBL_LEAD_SOURCE' => 'Fonte Lead:',
+  'LBL_OTHER_PHONE' => 'Altro Telefono:',
+  'LBL_FAX_PHONE' => 'Fax:',
+  'LBL_TITLE' => 'Titolo',
+  'LBL_DEPARTMENT' => 'Dipartimento:',
+  'LBL_BIRTHDATE' => 'Compleanno:',
+  'LBL_EMAIL_ADDRESS' => 'Email:',
+  'LBL_OTHER_EMAIL_ADDRESS' => 'Altra Email:',
+  'LBL_ANY_EMAIL' => 'Qualsiasi Email:',
+  'LBL_REPORTS_TO' => 'Riporta a:',
+  'LBL_ASSISTANT' => 'Assistente:',
+  'LBL_YAHOO_ID' => 'Yahoo! ID:',
+  'LBL_ASSISTANT_PHONE' => 'Telefono Assistente:',
+  'LBL_DO_NOT_CALL' => 'Non chiamare:',
+  'LBL_EMAIL_OPT_OUT' => 'Blocca Email',
+  'LBL_PRIMARY_ADDRESS' => 'Indirizzo Primario:',
+  'LBL_ALTERNATE_ADDRESS' => 'Altro Indirizzo:',
+  'LBL_ANY_ADDRESS' => 'Qualsiasi Indirizzo:',
+  'LBL_CITY' => 'Citta:',
+  'LBL_STATE' => 'Provincia:',
+  'LBL_POSTAL_CODE' => 'Codice Postale:',
+  'LBL_COUNTRY' => 'Paese:',
+  'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+  'LBL_IMAGE_INFORMATION' => 'Informazioni Immagine Contatto:',
+  'LBL_ADDRESS_INFORMATION' => 'Informazioni Indirizzo',
+  'LBL_DESCRIPTION' => 'Descrizione:',
+  'LBL_CONTACT_ROLE' => 'Ruolo:',
+  'LBL_OPP_NAME' => 'Nome Opportunit&agrave; :',
+  'LBL_DUPLICATE' => 'Contatto Potenzialmente Duplicato',
+  'MSG_DUPLICATE' => 'Creando questo contatto potresti potenzialmente creare un duplicato.  Puoi selezionare un contatto dalla lista sotto o  cliccare su Crea Nuovo Contatto  per continuare creando un nuovo contatto con i dati precedentemente inseriti.',
+
+  'LNK_NEW_APPOINTMENT' => 'Nuovo Appuntamento',
+  'LBL_ADD_BUSINESSCARD' => 'Aggiungi Biglietto da Visita',
+  'NTC_DELETE_CONFIRMATION' => 'Sei sicuro di voler cancellare questo elemento?',
+  'NTC_REMOVE_CONFIRMATION' => 'Sei sicuro di voler rimuovere questo contatto da questo caso?',
+  'NTC_REMOVE_DIRECT_REPORT_CONFIRMATION' => 'Sei sicuro di voler rimuovere questo record come un report diretto?',
+  'ERR_DELETE_RECORD' => 'en_us Un numero di record deve essere specificato per cancellare il contatto.',
+  'NTC_COPY_PRIMARY_ADDRESS' => 'Copia l`indirizzo primario nell`indirizzo alternativo',
+  'NTC_COPY_ALTERNATE_ADDRESS' => 'Copia l`indirizzo alternativo nell`indirizzo primario',
+
+  'LBL_SELECT_CONTACT' => 'Seleziona Contatto',
+  'LBL_GENERAL_INFORMATION' => 'Informazioni generali',
+
+  'LBL_NEW_POTENTIAL' => 'Nuova Opportunit&agrave;',
+  'LBL_POTENTIAL_TITLE' => 'Opportunit&agrave;',
+
+  'LBL_NEW_TASK' => 'Nuovo Compito',
+  'LBL_TASK_TITLE' => 'Compiti',
+  'LBL_NEW_CALL' => 'Nuova Chiamata',
+  'LBL_CALL_TITLE' => 'Chiamate',
+  'LBL_NEW_MEETING' => 'Nuova Riunione',
+  'LBL_MEETING_TITLE' => 'Riunioni',
+  'LBL_NEW_EMAIL' => 'Nuova Email',
+  'LBL_EMAIL_TITLE' => 'Email',
+  'LBL_NEW_NOTE' => 'Nuova Nota',
+  'LBL_NOTE_TITLE' => 'Note',
+
+  'LBL_TOOL_FORM_TITLE' => 'Strumenti Contatto',
+
+  'Salutation' => 'Formula di saluto',
+  'First Name' => 'Nome',
+  'Office Phone' => 'Telefono Ufficio',
+  'Last Name' => 'Cognome',
+  'Mobile' => 'Cellulare',
+  'Account Name' => 'Nome Azienda',
+  'Home Phone' => 'Telefono Casa',
+  'Lead Source' => 'Fonte Lead',
+  'Other Phone' => 'Other Phone',
+  'Title' => 'Titolo',
+  'Fax' => 'Fax',
+  'Department' => 'Dipartimento',
+  'Birthdate' => 'Compleanno',
+  'Email' => 'Email',
+  'Reports To' => 'Riporta a',
+  'Assistant' => 'Assistente',
+  'Yahoo Id' => 'Yahoo Id',
+  'Assistant Phone' => 'Telefono Assistente',
+  'Do Not Call' => 'Non Chiamare',
+  'Email Opt Out' => 'Blocca Email',
+  'Assigned To' => 'Assegnato a',
+  'Campaign Source' => 'Fonte Campagna',
+  'Reference' => 'Riferimento',
+  'Created Time' => 'Orario creazione',
+  'Modified Time' => 'Orario modifica',
+  'Mailing Street' => 'Via (spedizione)',
+  'Other Street' => 'Altra Via',
+  'Mailing City' => 'Citt&agrave;  (spedizione)',
+  'Mailing State' => 'Provincia (spedizione)',
+  'Mailing Zip' => 'CAP (spedizione)',
+  'Mailing Country' => 'Stato (spedizione)',
+  'Mailing Po Box' => 'BOX (spedizione)',
+  'Other Po Box' => 'Altro BOX (spedizione)',
+  'Other City' => 'Altra Citt&agrave;',
+  'Other State' => 'Altra Provincia',
+  'Other Zip' => 'Altro CAP',
+  'Other Country' => 'Altro Stato',
+  'Contact Image' => 'Immagine Contatto',
+  'Description' => 'Descrizione',
+
+  'LBL_NEW_CONTACT' => 'Nuovo Contatto',
+  'LBL_NEW_ACCOUNT' => 'Nuova Azienda',
+  'LBL_NOTE_SUBJECT' => 'Soggetto Nota:',
+  'LBL_NOTE' => 'Nota:',
+  'LBL_WEBSITE' => 'Website:',
+  'LBL_NEW_APPOINTMENT' => 'Nuovo Appuntamento:',
+  'LBL_SUBJECT' => 'Soggetto:',
+  'LBL_START_DATE' => 'Data Inizio:',
+  'LBL_START_TIME' => 'Orario inizio:',
+
+  'Portal User' => 'Utente Portale',
+  'LBL_CUSTOMER_PORTAL_INFORMATION' => 'Informazioni Portale Clienti',
+  'Support Start Date' => 'Data Inizio Supporto',
+  'Support End Date' => 'Data Fine Supporto',
+
+  'Name' => 'Nome',
+  'LBL_ALL' => 'Tutti',
+  'LBL_MAXIMUM_LIMIT_ERROR' => 'Attenzione, il file caricato eccede il limite massimo, riprova con uno pi&ugrave; piccolo di 800000 bytes',
+  'LBL_UPLOAD_ERROR' => 'Problemi nel caricamento del file. Riprova',
+  'LBL_IMAGE_ERROR' => 'Questo file non &egrave; con estensione immagine (.gif/.jpg/.png)',
+  'LBL_INVALID_IMAGE' => 'File Invalido o File senza dati',
+
+  'Notify Owner' => 'Notifica al Proprietario',
+
+  '--None--' => '--Nessuno--',
+
+  'Mr.' => 'Sig.',
+  'Ms.' => 'Sig.ra',
+  'Mrs.' => 'Sig.ina',
+  'Dr.' => 'Dr.',
+  'Prof.' => 'Prof.',
+
+  'Cold Call' => 'Chiamata a freddo',
+  'Existing Customer' => 'Cliente Esistente',
+  'Self Generated' => 'Autogenerato',
+  'Employee' => 'Dipendente',
+  'Partner' => 'Partner',
+  'Public Relations' => 'Public Relation',
+  'Direct Mail' => 'Mail Diretta',
+  'Conference' => 'Conferenza',
+  'Trade Show' => 'Fiera',
+  'Web Site' => 'Web Site',
+  'Word of mouth' => 'Passaparola',
+  'Other' => 'Altro',
+  'User List' => 'Lista Utente',
+
+  'Customer Portal Login Details' => 'Dettagli di Accesso al Portale Clienti',
+  'Dear' => 'Gentile',
+  'Your Customer Portal Login details are given below:' => 'I dettagli del vostro Account sul Portale Clienti sono:',
+  'User Id :' => 'Identificativo Utente :',
+  'Password :' => 'Password :',
+  'Please Login Here' => 'Prego eseguire il login qui',
+  'Note :' => 'Note :',
+  'We suggest you to change your password after logging in first time' => 'Ti suggeriamo di modificare la password dopo aver eseguito il primo login.',
+  'Support Team' => 'Team Supporto Clienti',
+
+  'TITLE_AJAX_CSS_POPUP_CHAT' => 'Chat (ajax popup)',
+
+'Contact Id' => 'Nr. Contatto',
+'Secondary Email'=>'Email Secondaria',
+
+'Contacts ID'=>'ID Contatti',
+);
+
+?>
