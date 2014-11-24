b69dcba08355d7eee7c0ee52d2c0f2e45ccb611f
root <root@drivalda-HP-500B-Microtower.(none)>
2014-11-24 12:00:14 +0100

    addinglangfiles4

---
+++ modules/Services/language/it_it.lang.php
@@ -0,0 +1,64 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+'LBL_MODULE_NAME'=>'Servizi',
+'SINGLE_Services'=>'Servizio',
+'LBL_LIST_SERVICE_NAME'=>'Nome Servizio',
+'LBL_PRICEBOOK'=>'Listino:',
+'LBL_PRICEBOOK_INFORMATION'=>'Informazioni Listino',
+'LBL_PRICING_INFORMATION'=>'Informazioni Prezzo',
+'LBL_LIST_FORM_TITLE'=>'Lista Servizio',
+'LBL_LIST_PRICEBOOK_FORM_TITLE'=>'Lista Listino',
+'LBL_SERVICE_UNIT_PRICE'=>'Prezzo Unitario',
+'LBL_PB_LIST_PRICE'=>'Listino',
+'LBL_SERVICE_INFORMATION'=>'Informazioni Servizio',
+'LBL_CUSTOM_INFORMATION'=>'Informationi Personalizzate',
+'LBL_DESCRIPTION_INFORMATION'=>'Informazioni Descrizione',
+
+'LBL_ADD_PRICEBOOK_BUTTON_LABEL'=>'Aggiungi a Listino',
+
+'Services'=>'Servizi',
+'Service Name'=>'Nome Servizio',
+'Service Active'=>'Attivo',
+'Service Category'=>'Categoria',
+'Service No'=>'Nr. Servizio',
+'Owner'=>'Proprietario',
+'Days'=>'Giorni',
+'Hours'=>'Ore',
+'Incidents'=>'Incidenti',
+'No of Units'=>'N. di Unit&agrave;',
+'Commission Rate'=>'Tasso Commissione (%)',
+'Unit Price'=>'Prezzo Unitario',
+'Price'=>'Prezzo',
+'Support Start Date'=>'Data Inizio Supporto',
+'Support Expiry Date'=>'Data Fine Supporto',
+'Sales Start Date'=>'Data Inizio Vendita',
+'Sales End Date'=>'Data Fine Vendita',
+'Created Time'=>'Periodo Creazione',
+'Modified Time'=>'Periodo Modifica',
+'Related To'=>'Relazionato a',
+'Usage Unit'=>'Unit&agrave; di Misura',
+'Currency'=>'Valuta',
+'Tax Class'=>'Tipologia Tassa',
+'Website'=>'Website',
+'Description'=>'Descrizione',
+
+'Price Book Name'=>'Nome Listino',
+
+'LBL_NO_TAXES_ASSOCIATED'=>'Nessuna tassa associata a questo servizio',
+'--None--'=>'--Nessuno--',
+'Installation'=>'Installazione',
+'Migration'=>'Migrazione',
+'Customization'=>'Personalizzazione',
+'Training'=>'Formazione',
+);
+
+?>
