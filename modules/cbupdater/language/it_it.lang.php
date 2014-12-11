97d7061a4123523474061f8beeca32765293e430
root <root@drivalda-HP-500B-Microtower.(none)>
2014-12-11 11:13:25 +0100

    adding missing language files

---
+++ modules/cbupdater/language/it_it.lang.php
@@ -0,0 +1,51 @@
+<?php
+/***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is: coreBOS Open Source
+ * The Initial Developer of the Original Code is vtiger
+ * Portions created by JPL TSolucio, S.L are Copyright (C) JPL TSolucio, S.L.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'cbupdater' => 'Aggiornamento coreBOS',
+	'SINGLE_cbupdater' => 'Aggiornamento coreBOS',
+	'cbupdater ID' => 'coreBOS Aggiorna ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Informazione personalizzata',
+	'LBL_cbupdater_INFORMATION' => 'coreBOS Aggiorna Informazione',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descrizione',
+
+	'cbupd_no' => 'coreBOS Aggiornamento No',
+	'author' => 'Autore',
+	'filename' => 'Nome Schedario',
+	'classname' => 'Nome classe',
+	'execstate' => 'Stato di esecuzione',
+	'execdate' => 'Data di esecuzione',
+	'execorder' => 'Ordine di esecuzione',
+	'systemupdate' => 'Aggiornamento di sistema',
+	'Pending' => 'Sospeso',
+	'Executed' => 'Eseguito',
+	'Continuous' => 'In Continuazione',
+	'Error' => 'Error',
+	'perspective' => 'Perspectiva',
+	'blocked' => 'Bloccato',
+	'Block Changeset' => 'Bloccare Changeset',
+	'UnBlock Changeset' => 'Sbloccare Changeset',
+	'Execute' => 'Esegui',
+	'Undo' => 'Disfare',
+	'ExportXML' => 'Esporti Cambiamenti',
+	'GetUpdates' => 'Carica Aggiornamenti',
+	'Apply' => 'Applica',
+	'ApplyAll' => 'Applica Tutto',
+	'ChangeSet' => 'Aggiorna',
+	'err_noupdatefile' => 'Schedario di aggiornamento non esiste! Vi preghiamo di rivedere l\'installazione',
+	'err_invalidupdatefile' => "Schedario di aggiornamento non valido! Vi invitiamo a controllare il suo contenuto.",
+	'err_invalidchangeset' => "Definizione di modifiche non valida! Probabilmente non riesce a trovare il nome del schedario. Si prega di verificare questo changeset.",
+	'err_noupdatesselected' => 'Definizione di modifiche non valido! Probabilmente non riesce a trovare il nome del schedario. Si prega di verificare questo changeset.',
+	'err_invalidclass' => 'Nome della classe specificata non esiste! Vi preghiamo di rivedere la configurazione di aggiornamento.',
+	'err_nomethod' => 'L\'aggiornamento specificato non ha la struttura corretta: metodo di aggiornamento mancante. Vi preghiamo di rivedere il codice di aggiornamento.',
+);
+
+?>
