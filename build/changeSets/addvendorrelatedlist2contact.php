36deb55cee0ca3ef9e31cd081795a07d9a133d5b
Joe Bordes <joe@tsolucio.com>
2015-01-30 20:01:49 +0100

    Vendor Related List on Contacts

---
+++ build/changeSets/addvendorrelatedlist2contact.php
@@ -0,0 +1,42 @@
+<?php
+class addvendorrelatedlist2contact extends cbupdaterWorker {
+	function applyChange() {
+		global $adb;
+		if ($this->hasError()) $this->sendError();
+		if ($this->isApplied()) {
+			$this->sendMsg('Changeset '.get_class($this).' already applied!');
+		} else {
+			global $adb;
+			$modname = 'Vendors';
+			$module = Vtiger_Module::getInstance($modname);
+			$ctModule = Vtiger_Module::getInstance('Contacts');
+			$ctModule->setRelatedList($module , $modname, Array("SELECT"),'get_vendors');
+			$this->sendMsg('Changeset '.get_class($this).' applied!');
+			$this->markApplied();
+		}
+		$this->finishExecution();
+	}
+	
+	function undoChange() {
+		if ($this->isBlocked()) return true;
+		if ($this->hasError()) $this->sendError();
+		if ($this->isSystemUpdate()) {
+			$this->sendMsg('Changeset '.get_class($this).' is a system update, it cannot be undone!');
+		} else {
+			if ($this->isApplied()) {
+				// undo your magic here
+				$modname = 'Vendors';
+				$module = Vtiger_Module::getInstance($modname);
+				$ctModule = Vtiger_Module::getInstance('Contacts');
+				$module->unsetRelatedList($ctModule , $modname, 'get_vendors');
+				$this->sendMsg('Changeset '.get_class($this).' undone!');
+				$this->markUndone();
+			} else {
+				$this->sendMsg('Changeset '.get_class($this).' not applied!');
+			}
+		}
+		$this->finishExecution();
+	}
+}
+
+?>
--- modules/Contacts/Contacts.php
+++ modules/Contacts/Contacts.php
@@ -937,6 +937,63 @@ class Contacts extends CRMEntity {
 		return $return_value;
 	}
 
+    /**
+	* Function to get Contact related vendors.
+	* @param  integer   $id      - contactid
+	* returns related vendor records in array format
+	*/
+	function get_vendors($id, $cur_tab_id, $rel_tab_id, $actions=false) {
+		global $log, $singlepane_view,$currentModule,$current_user;
+		$log->debug("Entering get_vendors(".$id.") method ...");
+		$this_module = $currentModule;
+
+        $related_module = vtlib_getModuleNameById($rel_tab_id);
+		require_once("modules/$related_module/$related_module.php");
+		$other = new $related_module();
+        vtlib_setup_modulevars($related_module, $other);
+		$singular_modname = vtlib_toSingular($related_module);
+
+		$parenttab = getParentTab();
+
+		if($singlepane_view == 'true')
+			$returnset = '&return_module='.$this_module.'&return_action=DetailView&return_id='.$id;
+		else
+			$returnset = '&return_module='.$this_module.'&return_action=CallRelatedList&return_id='.$id;
+
+		$button = '';
+
+		if($actions && getFieldVisibilityPermission($related_module, $current_user->id, 'parent_id', 'readwrite') == '0') {
+			if(is_string($actions)) $actions = explode(',', strtoupper($actions));
+			if(in_array('SELECT', $actions) && isPermitted($related_module,4, '') == 'yes') {
+				$button .= "<input title='".getTranslatedString('LBL_SELECT')." ". getTranslatedString($related_module). "' class='crmbutton small edit' type='button' onclick=\"return window.open('index.php?module=$related_module&return_module=$currentModule&action=Popup&popuptype=detailview&select=enable&form=EditView&form_submit=false&recordid=$id&parenttab=$parenttab','test','width=640,height=602,resizable=0,scrollbars=0');\" value='". getTranslatedString('LBL_SELECT'). " " . getTranslatedString($related_module) ."'>&nbsp;";
+			}
+			if(in_array('ADD', $actions) && isPermitted($related_module,1, '') == 'yes') {
+				$button .= "<input title='".getTranslatedString('LBL_ADD_NEW'). " ". getTranslatedString($singular_modname) ."' class='crmbutton small create'" .
+					" onclick='this.form.action.value=\"EditView\";this.form.module.value=\"$related_module\"' type='submit' name='button'" .
+					" value='". getTranslatedString('LBL_ADD_NEW'). " " . getTranslatedString($singular_modname) ."'>&nbsp;";
+			}
+		}
+
+		$userNameSql = getSqlForNameInDisplayFormat(array('first_name'=>
+							'vtiger_users.first_name', 'last_name' => 'vtiger_users.last_name'), 'Users');
+		$query = "SELECT case when (vtiger_users.user_name not like '') then $userNameSql else vtiger_groups.groupname end as user_name,
+				vtiger_crmentity.crmid, vtiger_vendor.*,  vtiger_vendorcf.*
+				from vtiger_vendor inner join vtiger_crmentity on vtiger_crmentity.crmid=vtiger_vendor.vendorid
+                INNER JOIN vtiger_vendorcontactrel on vtiger_vendorcontactrel.vendorid=vtiger_vendor.vendorid
+				LEFT JOIN vtiger_vendorcf on vtiger_vendorcf.vendorid=vtiger_vendor.vendorid
+				LEFT JOIN vtiger_users on vtiger_users.id=vtiger_crmentity.smownerid
+				LEFT JOIN vtiger_groups on vtiger_groups.groupid=vtiger_crmentity.smownerid
+				WHERE vtiger_crmentity.deleted=0 and vtiger_vendorcontactrel.contactid=".$id;
+
+		$return_value = GetRelatedList($this_module, $related_module, $other, $query, $button, $returnset);
+
+		if($return_value == null) $return_value = Array();
+		$return_value['CUSTOM_BUTTON'] = $button;
+
+		$log->debug("Exiting get_vendors method ...");
+		return $return_value;
+	}
+
 	/** Function to export the contact records in CSV Format
 	* @param reference variable - where condition is passed when the query is executed
 	* Returns Export Contacts Query.
