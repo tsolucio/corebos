34c1cbced5f25a46acb088202bc7cfd209e9f68a
Joe Bordes <joe@tsolucio.com>
2015-03-08 19:26:01 +0100

    style(Campaigns) upgrade module files to be more in line with base code files

--- modules/Campaigns/ListView.php
+++ modules/Campaigns/ListView.php
@@ -1,5 +1,5 @@
 <?php
-/*+********************************************************************************
+/*+**********************************************************************************
  * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -8,6 +8,6 @@
  * All Rights Reserved.
  ************************************************************************************/
 
-require_once('modules/Vtiger/ListView.php');
+include_once('modules/Vtiger/ListView.php');
 
 ?>
--- modules/Campaigns/index.php
+++ modules/Campaigns/index.php
@@ -1,17 +1,15 @@
 <?php
-/*********************************************************************************
-** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
-*
- ********************************************************************************/
+ ************************************************************************************/
+global $currentModule;
 
-global $theme;
-$theme_path="themes/".$theme."/";
-$image_path=$theme_path."images/";
+checkFileAccessForInclusion("modules/$currentModule/ListView.php");
+include_once("modules/$currentModule/ListView.php");
 
-	include ('modules/Campaigns/ListView.php');
 ?>
