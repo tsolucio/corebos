64ce74a100ff87d716c7b907dd3b3ce871cca049
Omar Llorens <omar@tsolucio.com>
2015-03-20 13:33:08 +0100

    install(InventoryDetails) New module to save the vtiger_inventoryproductrel register, to have more details about the productlines, add workflows, reports ....

---
+++ modules/InventoryDetails/language/es_mx.lang.php
@@ -0,0 +1,38 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'InventoryDetails' => 'Líneas de Detalle',
+	'SINGLE_InventoryDetails' => 'Líneas de Detalle',
+	'InventoryDetails ID' => 'Líneas de Detalle ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_INVENTORYDETAILS_INFORMATION' => 'Información Líneas de Detalle',
+	'LBL_DESCRIPTION_INFORMATION' => 'Información Adicional',
+	
+	'Related To' => 'Relacionado con',
+	'Sequence No' => 'Número de Secuencia',
+	'Line Item ID' => 'ID de la Línea',
+	'Quantity' => 'Unidades',
+	'Listprice' => 'Precio Unitario',
+	'Tax Percent' => 'Porcentaje de Impuesto',
+	'Extgross' => 'Precio Bruto',
+	'Discount Percent' => 'Porcentaje de Descuento',
+	'Discount Amount' => 'Total Descuento',
+	'Extnet' => 'Precio Neto',
+	'Line Tax' => 'Impuesto de la Línea',
+	'Line Total' => 'Precio Total',
+	'Units Delivered Received' => 'Unidades Entregadas/Recibidas',
+	'Line Completed' => 'Línea Completada',
+	'Description' => 'Descripción',
+
+);
+
+?>
---
+++ modules/InventoryDetails/language/pt_br.lang.php
@@ -0,0 +1,37 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'InventoryDetails' => 'Inventory Details',
+	'SINGLE_InventoryDetails' => 'Inventory Details',
+	'InventoryDetails ID' => 'Inventory Details ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_INVENTORYDETAILS_INFORMATION' => 'Inventory Details Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Additional Information',
+
+	'Related To' => 'Related To',
+	'Sequence No' => 'Sequence No',
+	'Line Item ID' => 'Line Item ID',
+	'Quantity' => 'Quantity',
+	'Listprice' => 'Listprice',
+	'Tax Percent' => 'Tax Percent',
+	'Extgross' => 'Extgross',
+	'Discount Percent' => 'Discount Percent',
+	'Discount Amount' => 'Discount Amount',
+	'Extnet' => 'Extnet',
+	'Line Tax' => 'Line Tax',
+	'Line Total' => 'Line Total',
+	'Units Delivered Received' => 'Units Delivered/Received',
+	'Line Completed' => 'Line Completed',
+	'Description' => 'Description',
+);
+
+?>
