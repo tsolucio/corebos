64bae21b3bfb1ab2d5b5153ac06717fbe057d0ee
Joe Bordes <joe@tsolucio.com>
2015-04-11 00:28:04 +0200

    feat(coreBOS Updater) script to load and apply changeset from CLI

---
+++ modules/cbupdater/loadapplychanges.php
@@ -0,0 +1,38 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2015 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+* Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+* file except in compliance with the License. You can redistribute it and/or modify it
+* under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+* granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+* the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+* applicable law or agreed to in writing, software distributed under the License is
+* distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied. See the License for the specific language governing
+* permissions and limitations under the License. You may obtain a copy of the License
+* at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+*************************************************************************************************
+*  Module       : cbupdater
+*  Version      : 5.5.0
+*  Author       : JPL TSolucio, S. L.
+*************************************************************************************************/
+
+include 'modules/cbupdater/getupdatescli.php';
+
+if ($error==0) {
+	if (count($argv)==1) {
+		$argv[2] = 'all';
+	} elseif (empty($argv[2])) {
+		$argv[2] = trim($cbupdate_ids,',');
+	} else {
+		$argv[2] = 'all';
+	}
+	$argv[1] = 'apply';
+	include 'modules/cbupdater/doworkcli.php';
+} else {
+	echo "** ERROR **\n;";
+	echo $errmsg."\n";
+	echo "** ERROR **\n;";
+}
+?>
