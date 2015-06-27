a471e67edba7db1524999e8f16f2841353c3b821
Joe Bordes <joe@tsolucio.com>
2015-06-28 00:46:35 +0200

    i18n/style(CurrencyList) code cleanup and translate status

--- modules/Settings/CurrencyListView.php
+++ modules/Settings/CurrencyListView.php
@@ -1,5 +1,4 @@
 <?php
-
 /*********************************************************************************
 ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
@@ -7,7 +6,6 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
-*
  ********************************************************************************/
 require_once('Smarty_setup.php');
 require_once('include/database/PearDatabase.php');
@@ -16,12 +14,12 @@ global $mod_strings,$adb,$theme,$app_strings,$default_charset;
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
 $smarty=new vtigerCRM_Smarty;
-   $parenttab = getParentTab();
-   $sql = "select * from vtiger_currency_info where deleted=0";
-   $result = $adb->pquery($sql, array());
-   $temprow = $adb->fetch_array($result);
-   $cnt=1;
-   $currency = Array();
+$parenttab = getParentTab();
+$sql = "select * from vtiger_currency_info where deleted=0";
+$result = $adb->pquery($sql, array());
+$temprow = $adb->fetch_array($result);
+$cnt=1;
+$currency = Array();
 do
 {
 	$currency_element = Array();
@@ -37,7 +35,7 @@ do
 	}
 	else
 		$currency_element['tool']= '';
- 	$currency[] = $currency_element; 
+	$currency[] = $currency_element; 
 	$cnt++;
 }while($temprow = $adb->fetch_array($result));
 $smarty->assign("PARENTTAB",$parenttab);
@@ -46,8 +44,7 @@ $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MOD",$mod_strings);
 $smarty->assign("CURRENCY_LIST",$currency);
 if($_REQUEST['ajax'] !='')
-        $smarty->display("CurrencyListViewEntries.tpl");
+	$smarty->display("CurrencyListViewEntries.tpl");
 else
-        $smarty->display("CurrencyListView.tpl");
+	$smarty->display("CurrencyListView.tpl");
 ?>
-
