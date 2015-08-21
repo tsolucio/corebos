3e126eaac36f5d12e47a62625d9cb368151cff70
Joe Bordes <joe@tsolucio.com>
2015-08-21 02:35:15 +0200

    fix(ServiceCotracts) sync module code with vtlib base module code

--- modules/ServiceContracts/Delete.php
+++ modules/ServiceContracts/Delete.php
@@ -15,14 +15,12 @@ $module = vtlib_purify($_REQUEST['module']);
 $return_module = vtlib_purify($_REQUEST['return_module']);
 $return_action = vtlib_purify($_REQUEST['return_action']);
 $return_id = vtlib_purify($_REQUEST['return_id']);
+$parenttab = getParentTab();
 
 //Added to fix 4600
 $url = getBasic_Advance_SearchURL();
 
 DeleteEntity($currentModule, $return_module, $focus, $record, $return_id);
 
-$parenttab = getParentTab();
-
 header("Location: index.php?module=$return_module&action=$return_action&record=$return_id&parenttab=$parenttab&relmodule=$module".$url);
-
 ?>
--- modules/ServiceContracts/EditView.php
+++ modules/ServiceContracts/EditView.php
@@ -7,7 +7,6 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-
 require_once 'modules/Vtiger/EditView.php';
 
 if(!empty($_REQUEST['service_id'])) {
@@ -15,7 +14,7 @@ if(!empty($_REQUEST['service_id'])) {
 	if(isRecordExists($_REQUEST['service_id'])) {
 		$serviceObj->retrieve_entity_info($_REQUEST['service_id'], 'Services');
 		$focus->column_fields['tracking_unit'] = $serviceObj->column_fields['service_usageunit'];
-	}	
+	}
 }
 if(!empty($_REQUEST['return_id']) && !empty($_REQUEST['return_module'])) {
 	$invModule = $_REQUEST['return_module'];
@@ -27,11 +26,10 @@ if(!empty($_REQUEST['return_id']) && !empty($_REQUEST['return_module'])) {
 			$focus->column_fields['sc_related_to'] = $inventoryObj->column_fields['account_id'];
 		} else if(!empty($inventoryObj->column_fields['contact_id'])) {
 			$focus->column_fields['sc_related_to_type'] = 'Contacts';
-			$focus->column_fields['sc_related_to'] = $inventoryObj->column_fields['contact_id'];		
+			$focus->column_fields['sc_related_to'] = $inventoryObj->column_fields['contact_id'];
 		}
 	}
 }
 
 	$smarty->display('salesEditView.tpl');
-
 ?>
