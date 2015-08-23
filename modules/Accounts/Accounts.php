08028ca6e55fd30d6a1de53f395a77d0ffcb1a3d
Joe Bordes <joe@tsolucio.com>
2015-08-23 16:36:15 +0200

    fix(Accounts) add email fields to the get_emails query

--- modules/Accounts/Accounts.php
+++ modules/Accounts/Accounts.php
@@ -421,19 +421,19 @@ class Accounts extends CRMEntity {
 		$userNameSql = getSqlForNameInDisplayFormat(array('first_name'=>
 						'vtiger_users.first_name', 'last_name' => 'vtiger_users.last_name'), 'Users');
 		$query = "SELECT case when (vtiger_users.user_name not like '') then $userNameSql else vtiger_groups.groupname end as user_name,
-			vtiger_activity.activityid, vtiger_activity.subject,
+			vtiger_activity.activityid, vtiger_activity.subject, vtiger_emaildetails.*,
 			vtiger_activity.activitytype, vtiger_crmentity.modifiedtime,
 			vtiger_crmentity.crmid, vtiger_crmentity.smownerid, vtiger_activity.date_start, vtiger_seactivityrel.crmid as parent_id
-			FROM vtiger_activity, vtiger_seactivityrel, vtiger_account, vtiger_users, vtiger_crmentity
+			FROM vtiger_activity, vtiger_seactivityrel, vtiger_account, vtiger_emaildetails, vtiger_users, vtiger_crmentity
 			LEFT JOIN vtiger_groups ON vtiger_groups.groupid=vtiger_crmentity.smownerid
 			WHERE vtiger_seactivityrel.activityid = vtiger_activity.activityid
 				AND vtiger_seactivityrel.crmid IN (". implode(',', $entityIds) .")
+				AND vtiger_emaildetails.emailid = vtiger_activity.activityid
 				AND vtiger_users.id=vtiger_crmentity.smownerid
 				AND vtiger_crmentity.crmid = vtiger_activity.activityid
 				AND vtiger_account.accountid = ".$id."
 				AND vtiger_activity.activitytype='Emails'
 				AND vtiger_crmentity.deleted = 0";
-
 		$return_value = GetRelatedList($this_module, $related_module, $other, $query, $button, $returnset);
 
 		if($return_value == null) $return_value = Array();
