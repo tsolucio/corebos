febf61a33e126b55b2e73027646befe030151181
Joe Bordes <joe@tsolucio.com>
2015-09-20 12:34:17 +0200

    style(javascript) missing semicolons, eliminate unused code, cleanup code

--- modules/Webmails/js/script.js
+++ modules/Webmails/js/script.js
@@ -1,31 +1,25 @@
 /*********************************************************************************
-  ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
-   * ("License"); You may not use this file except in compliance with the License
-   * The Original Code is:  vtiger CRM Open Source
-   * The Initial Developer of the Original Code is vtiger.
-   * Portions created by vtiger are Copyright (C) vtiger.
-   * All Rights Reserved.
-  *
- ********************************************************************************/
+ ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+********************************************************************************/
 function showhide(argg)
 {
 	var x=document.getElementById(argg).style;
-	if (x.display=="none") 
-	{
-		x.display="block"
-	
+	if (x.display=="none") {
+		x.display="block";
+	} else {
+		x.display="none";
 	}
-	else {
-			x.display="none"
-		  }
 }
-
 
 function showhideRepeat(argg1,argg2)
 {
 	var x=document.getElementById(argg2).style;
 	var y=document.getElementById(argg1).checked;
-	
 	if (y)
 	{
 		x.display="block";
@@ -33,20 +27,14 @@ function showhideRepeat(argg1,argg2)
 	else {
 		x.display="none";
 	}
-	
 }
-
-
 
 function gshow(argg1)
 {
 	var y=document.getElementById(argg1).style;
-	
 	if (y.display=="none") 
 	{
 		y.display="block";
-		
-	
 	}
 }
 
@@ -55,18 +43,17 @@ function ghide(argg2)
 	var z=document.getElementById(argg2).style;
 	if (z.display=="block" ) 
 	{
-		z.display="none"
-	
+		z.display="none";
 	}
 }
 
- function moveMe(arg1) {
+function moveMe(arg1) {
 	var posx = 0;
 	var posy = 0;
 	var e=document.getElementById(arg1);
-	
+
 	if (!e) var e = window.event;
-	
+
 	if (e.pageX || e.pageY)
 	{
 		posx = e.pageX;
@@ -77,15 +64,14 @@ function ghide(argg2)
 		posx = e.clientX + document.body.scrollLeft;
 		posy = e.clientY + document.body.scrollTop;
 	}
- }
+}
 
 function switchClass(myModule,toStatus) {
 	var x=document.getElementById(myModule);
 	if (toStatus=="on") {
 		x.className="dvtSelectedCell";
-		}
+	}
 	if (toStatus=="off") {
 		x.className="dvtUnSelectedCell";
-		}
-		
+	}
 }
