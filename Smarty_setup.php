24aa8b1535a00018911b3ce77c22689ccebb11ec
Joe Bordes <joe@tsolucio.com>
2015-09-27 01:16:41 +0200

    fix/style(warnings) eliminate warnings, related to issue #144. cleanup code

--- Smarty_setup.php
+++ Smarty_setup.php
@@ -1,20 +1,18 @@
 <?php
 /*********************************************************************************
-  ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
-   * ("License"); You may not use this file except in compliance with the License
-   * The Original Code is:  vtiger CRM Open Source
-   * The Initial Developer of the Original Code is vtiger.
-   * Portions created by vtiger are Copyright (C) vtiger.
-   * All Rights Reserved.
-  *
- ********************************************************************************/
-
+ ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+********************************************************************************/
 require('Smarty/libs/Smarty.class.php');
 class vtigerCRM_Smarty extends Smarty{
-	
+
 	/** Cache the tag cloud display information for re-use */
 	static $_tagcloud_display_cache = array();
-	
+
 	static function lookupTagCloudView($userid) {
 		if(!isset(self::$_tagcloud_display_cache[$userid])) {
 			self::$_tagcloud_display_cache[$userid] = getTagCloudView($userid);
@@ -22,9 +20,8 @@ class vtigerCRM_Smarty extends Smarty{
 		return self::$_tagcloud_display_cache[$userid];
 	}
 	/** END */
-	
-	/**This function sets the smarty directory path for the member variables	
-	*/
+
+	/** This function sets the smarty directory path for the member variables */
 	function vtigerCRM_Smarty()
 	{
 		global $CALENDAR_DISPLAY, $WORLD_CLOCK_DISPLAY, $CALCULATOR_DISPLAY, $CHAT_DISPLAY, $current_user;
@@ -36,18 +33,18 @@ class vtigerCRM_Smarty extends Smarty{
 		$this->cache_dir = 'Smarty/cache';
 
 		//$this->caching = true;
-	        //$this->assign('app_name', 'Login');
-		$this->assign('CALENDAR_DISPLAY', $CALENDAR_DISPLAY); 
- 		$this->assign('WORLD_CLOCK_DISPLAY', $WORLD_CLOCK_DISPLAY); 
- 		$this->assign('CALCULATOR_DISPLAY', $CALCULATOR_DISPLAY); 
- 		$this->assign('CHAT_DISPLAY', $CHAT_DISPLAY);
+		//$this->assign('app_name', 'Login');
+		$this->assign('CALENDAR_DISPLAY', $CALENDAR_DISPLAY);
+		$this->assign('WORLD_CLOCK_DISPLAY', $WORLD_CLOCK_DISPLAY);
+		$this->assign('CALCULATOR_DISPLAY', $CALCULATOR_DISPLAY);
+		$this->assign('CHAT_DISPLAY', $CHAT_DISPLAY);
 		$this->assign('CURRENT_USER_ID',(isset($current_user) ? $current_user->id : 0));
- 		
- 		// Query For TagCloud only when required
- 		if(isset($_REQUEST) && $_REQUEST['action'] == 'DetailView') {
+
+		// Query For TagCloud only when required
+		if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'DetailView') {
 			//Added to provide User based Tagcloud
-            $this->assign('TAG_CLOUD_DISPLAY', self::lookupTagCloudView($current_user->id) );
- 		}
+			$this->assign('TAG_CLOUD_DISPLAY', self::lookupTagCloudView($current_user->id) );
+		}
 	}
 }
 
