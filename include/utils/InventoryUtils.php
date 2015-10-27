18274ff2ed3bc8858c0c8a04f884b565cef8f95b
Joe Bordes <joe@tsolucio.com>
2015-10-27 11:11:54 +0100

    fix(Inventory Modules) avoid error accessing group email function when assigning record to group

--- include/utils/InventoryUtils.php
+++ include/utils/InventoryUtils.php
@@ -7,6 +7,7 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ********************************************************************************/
+require_once 'modules/Emails/mail.php';
 
 /**
  * This function returns the Product detail block values in array format.
