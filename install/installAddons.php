76e7a92a92e700fefb2518bf6c29705977e05f53
Joe Bordes <joe@tsolucio.com>
2016-02-04 08:37:42 +0100

    chore(Install) default it_it to not active like the others, activate RecycleBin and add license

--- install/installAddons.php
+++ install/installAddons.php
@@ -1,4 +1,18 @@
 <?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
 require_once('vtlib/Vtiger/Module.php');
 
 function installAddons() {
@@ -24,13 +38,13 @@ function installAddons() {
 		'ProjectMilestone' => false,
 		'ProjectTask' => false,
 		'Project' => false,
-		'RecycleBin' => false,
+		'RecycleBin' => true,
 		'SMSNotifier' => false,
 		'Tooltip' => false,
 		'Webforms' => false,
 	),
 	'lang' => array(
-		'it_it' => true,
+		'it_it' => false,
 		'pt_br' => false,
 		'en_gb' => false,
 		'de_de' => false,
