d4cfca88db71f0c4ed0960ed55d19e7441a38fab
Omar Llorens <omar@tsolucio.com>
2016-02-25 10:06:58 +0100

    feat(Recyclebin) Delete phisic documents from storage if we empty the recyclebin

--- modules/RecycleBin/EmptyRecyclebin.php
+++ modules/RecycleBin/EmptyRecyclebin.php
@@ -9,13 +9,47 @@
  *********************************************************************************/
 
 require_once('RecycleBinUtils.php');
-global $adb,$log;
+global $adb,$log,$default_charset;
 $allrec=vtlib_purify($_REQUEST['allrec']);
 $idlist=vtlib_purify($_REQUEST['idlist']);
 $excludedRecords=vtlib_purify($_REQUEST['excludedRecords']);
 $selected_module = vtlib_purify($_REQUEST['selectmodule']);
 $idlists = getSelectedRecordIds($_REQUEST,$selected_module,$idlist,$excludedRecords);
 $idlists = array_filter($idlists); // this is to eliminate the empty value we always get from selection
+//Delete documents from storage
+if(empty($selected_module) || $selected_module == "Documents"){
+	$docstodel = array();
+	if(count($idlists)==0)
+	{
+		$delcrm=$adb->pquery("SELECT crmid FROM vtiger_crmentity WHERE deleted = 1 and setype=?",array('Documents'));
+		if($delcrm) {
+			while($row = $adb->fetch_array($delcrm)) {
+				$docstodel[] = $row;
+			}
+		}
+	}elseif($selected_module == "Documents"){
+		$docstodel = $idlists;
+	}
+
+	foreach ($docstodel as $key => $id) {
+		if(!empty($id)) {
+			$result = $adb->pquery('SELECT * FROM vtiger_notes WHERE notesid = ?',array($id));
+			if($adb->num_rows($result) == 1) {
+				$fileType = @$adb->query_result($result, 0, 'filetype');
+				$name = @$adb->query_result($result, 0, 'filename');
+				$name = html_entity_decode($name, ENT_QUOTES, $default_charset);
+				$seQuery = $adb->pquery("select attachmentsid from vtiger_seattachmentsrel where crmid = ?",array($id));
+				$fileid = $adb->query_result($seQuery,0,'attachmentsid');
+				$pathQuery = $adb->pquery('select path from vtiger_attachments where attachmentsid = ?',array($fileid));
+				$filepath = $adb->query_result($pathQuery,0,'path');
+				$saved_filename = $filepath.$fileid."_".$name;
+				if(file_exists($saved_filename))
+					@unlink($saved_filename);
+			}
+		}
+	}
+
+}
 if($allrec==1 and !empty($selected_module)){
 	$delcrm=$adb->pquery("DELETE FROM vtiger_crmentity WHERE deleted = 1 and setype=?",array($selected_module));
 	$delrel = $adb->pquery("DELETE FROM vtiger_relatedlists_rb WHERE entityid in (".generateQuestionMarks($idlists).")",array($idlists));
