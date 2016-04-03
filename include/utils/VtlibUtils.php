08f21c1258320b59a69d63101f00609077f4d0f0
Joe Bordes <joe@tsolucio.com>
2016-04-03 13:19:28 +0200

    fix(HTMLPurifier) update config call to latest version

--- include/utils/VtlibUtils.php
+++ include/utils/VtlibUtils.php
@@ -581,8 +581,8 @@ function vtlib_purify($input, $ignore=false) {
 			include_once ('include/htmlpurifier/library/HTMLPurifier.auto.php');
 
 			$config = HTMLPurifier_Config::createDefault();
-			$config->set('Core', 'Encoding', $use_charset);
-			$config->set('Cache', 'SerializerPath', "$use_root_directory/test/vtlib");
+			$config->set('Core.Encoding', $use_charset);
+			$config->set('Cache.SerializerPath', "$use_root_directory/test/vtlib");
 
 			$__htmlpurifier_instance = new HTMLPurifier($config);
 		}
