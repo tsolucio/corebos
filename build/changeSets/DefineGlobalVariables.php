55193f7216c73602ea09d183f65f86ebc149e32a
Joe Bordes <joe@tsolucio.com>
2016-05-22 15:40:15 +0200

    chore(GlobalVariables) reorder list to start unofficial standarization

--- build/changeSets/DefineGlobalVariables.php
+++ build/changeSets/DefineGlobalVariables.php
@@ -22,42 +22,48 @@ class DefineGlobalVariables extends cbupdaterWorker {
 			$this->sendMsg('Changeset '.get_class($this).' already applied!');
 		} else {
 			$global_variables = array(
-				'product_service_default',
 				'Debug_Record_Not_Found',
 				'Debug_Report_Query',
-				'Product_Default_Units',
-				'Service_Default_Units',
-				'Maximum_Scheduled_Workflows',
-				'Billing_Address_Checked',
-				'Shipping_Address_Checked',
-				'Tax_Type_Default',
-				'calendar_call_default_duration',
-				'calendar_other_default_duration',
-				'calendar_sort_users_by',
 				'Debug_Send_VtigerCron_Error',
-				'Import_Full_CSV',
-				'Lead_Convert_TransferToAccount',
-				'Show_Copy_Adress_Header',
-				'SalesOrderStatusOnInvoiceSave',
-				'QuoteStatusOnSalesOrderSave',
-				'GoogleCalendarSync_BaseUpdateMonths',
-				'GoogleCalendarSync_BaseCreateMonths',
-				'Report.Excel.Export.RowHeight',
-				'Calendar_Modules_Panel_Visible',
-				'Calendar_Default_Reminder_Minutes',
-				'Application_Global_Search_Binary',
-				'Calendar_Slot_Minutes',
-				'Users_ReplyTo_SecondEmail',
-				'Workflow_Send_Email_ToCCBCC',
-				'BusinessMapping_SalesOrder2Invoice',
-				'Calendar_Show_Inactive_Users',
-				'Campaign_CreatePotentialOnAccountRelation',
-				'Campaign_CreatePotentialOnContactRelation',
-				'BusinessMapping_PotentialOnCampaignRelation',
+
 				'Application_Global_Search_SelectedModules',
 				'Application_Storage_Directory',
 				'Application_Storage_SaveStrategy',
+				'Application_Global_Search_Binary',
+
+				'Calendar_Modules_Panel_Visible',
+				'Calendar_Default_Reminder_Minutes',
+				'Calendar_Slot_Minutes',
+				'Calendar_Show_Inactive_Users',
+				'calendar_call_default_duration',
+				'calendar_other_default_duration',
+				'calendar_sort_users_by',
+
+				'BusinessMapping_SalesOrder2Invoice',
+				'BusinessMapping_PotentialOnCampaignRelation',
+
+				'Users_ReplyTo_SecondEmail',
 				'Users_Default_Send_Email_Template',
+
+				'Campaign_CreatePotentialOnAccountRelation',
+				'Campaign_CreatePotentialOnContactRelation',
+				'GoogleCalendarSync_BaseUpdateMonths',
+				'GoogleCalendarSync_BaseCreateMonths',
+				'Import_Full_CSV',
+				'Lead_Convert_TransferToAccount',
+				'Workflow_Send_Email_ToCCBCC',
+
+				'Maximum_Scheduled_Workflows', // rename to Workflow_Maximum_Scheduled_Workflows
+				'Billing_Address_Checked', // rename to Application_Billing_Address_Checked
+				'Shipping_Address_Checked', // rename to Application_Shipping_Address_Checked
+				'Show_Copy_Adress_Header', // rename to Application_Show_Copy_Adress_Header
+				'Tax_Type_Default', // rename to Inventory_Tax_Type_Default
+				'product_service_default', // rename to Inventory_ProductService_Default
+				'Product_Default_Units', // rename to Inventory_Product_Default_Units
+				'Service_Default_Units', // rename to Inventory_Service_Default_Units
+				'SalesOrderStatusOnInvoiceSave', // rename to SalesOrder_StatusOnInvoiceSave
+				'QuoteStatusOnSalesOrderSave',  // rename to Quote_StatusOnSalesOrderSave
+				'Report.Excel.Export.RowHeight', // rename to Report_Excel_Export_RowHeight
 			);
 			
 			$moduleInstance = Vtiger_Module::getInstance('GlobalVariable');
