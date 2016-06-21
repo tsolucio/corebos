a5ad6258f9b10e9fedba9231f646795f2785e8da
Albert Xhani <albert.xhani@outlook.com>
2016-06-21 14:49:25 +0200

    feat (All modules) Limit duplication of dependent records by setting business maps for every module containing related modules

--- modules/Vtiger/duplicate.php
+++ modules/Vtiger/duplicate.php
@@ -1,7 +1,9 @@
 <?php
 
 error_reporting("E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_WARNING");
-require_once "include/utils/utils.php";
+require_once 'include/utils/utils.php';
+require_once 'include/utils/CommonUtils.php';
+global $current_user;
 
 if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 {
@@ -12,7 +14,7 @@ if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 	$focus = new $currentModule();
 	$focus->retrieve_entity_info($record_id, $currentModule);
 
-	//Duplicate Records that this Record is dependent of 
+	// Duplicate Records that this Record is dependent of 
 	foreach ($focus->column_fields as $fieldname => $value) {
 		$sql = "SELECT 	* FROM vtiger_field WHERE columnname = ? AND uitype IN (10,50,51,57,58,59,73,68,76,75,81,78,80)";
 		$result = $adb->pquery($sql , array($fieldname));
@@ -24,17 +26,40 @@ if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 			require_once "modules/" . $module ."/". $module .".php";
 			$entity = new $module();
 			$entity->retrieve_entity_info($value,$module);
+
+			sanitizeModuleFields($entity,$module);
 			$entity->save($module);
 			$new_entity_id = $entity->id;
 			$focus->column_fields[$fieldname] = $new_entity_id;
 		}
 	}
 
+	sanitizeModuleFields($focus,$currentModule);
 	$focus->save($currentModule);
  	$new_record_id = $focus->id;
  	$curr_tab_id = gettabid($currentModule);
  	$dependents_list = array();
  	$related_list = array();
+
+ 	// Retrieve relations map
+ 	require_once "modules/cbMap/cbMap.php";
+	require_once "modules/cbMap/cbMapcore.php";
+
+	$sql = "SELECT * FROM vtiger_cbmap WHERE mapname=? AND targetname=?";
+	$result = $adb->pquery($sql , array($currentModule."_relations",$currentModule));
+	if($adb->num_rows($result))
+	{
+		$cbmap_id = $adb->query_result($result,0,"cbmapid");
+		$module_map = new cbMap();
+		$module_map->retrieve_entity_info($cbmap_id,"cbMap");
+		$cbMapcore = new cbMapcore($module_map);
+
+		$xml = $cbMapcore->getXMLContent();
+		$maped_relations = array();
+		foreach ($xml->relatedmodules->relatedmodule as $r) {
+			$maped_relations[ (string)$r->module ] = (string)$r->relation;
+		}
+	}
 
 	// Get related list
 	$sql = "select related_tabid from vtiger_relatedlists where tabid=? and name=?";
@@ -52,7 +77,11 @@ if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 	$noofrows = $adb->num_rows($result);
 	if($noofrows){
 		while( $r = $adb->fetch_array($result) ){
-			$dependents_list[] = getTabModuleName( $r['related_tabid'] );
+			$moduleName = getTabModuleName( $r['related_tabid'] );
+			if(isset($maped_relations[$moduleName]))
+			{
+				$dependents_list[] = $moduleName;
+			}
 		}
 	}
 
@@ -100,6 +129,7 @@ if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 	 		$entity->retrieve_entity_info($record,$module); 
 			
 	 		$entity->column_fields[$related_field] = $new_record_id;
+	 		sanitizeModuleFields($entity,$module);
 	 		$entity->save($module);
 	 	}
 	 }
@@ -121,5 +151,14 @@ if(isset($_REQUEST['module_name']) && isset($_REQUEST['record_id']))
 	exit();
 }
 
+function sanitizeModuleFields($module,$module_name)
+{
+	global $adb , $current_user;
+
+	$handler = vtws_getModuleHandlerFromName($module_name, $current_user);
+	$meta = $handler->getMeta();
+	$module->column_fields = DataTransform::sanitizeForInsert($module->column_fields,$meta);
+	$module->column_fields = DataTransform::sanitizeTextFieldsForInsert($module->column_fields,$meta);
+}
 
 ?>
