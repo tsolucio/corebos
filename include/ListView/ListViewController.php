f596448a14bfd7d43d735afdd29f23fee6fa245c
Omar Llorens <omar@tsolucio.com>
2016-07-01 13:48:41 +0200

    feat(Emails) Now Emails modules show you a standard ListView

--- include/ListView/ListViewController.php
+++ include/ListView/ListViewController.php
@@ -580,6 +580,8 @@ class ListViewController {
 					$value = "<span align='right'>".textlength_check($value)."</div>";
 				} elseif ($field->getUIType() == 55) {
 					$value = getTranslatedString($value,$currentModule);
+				}elseif ($module == 'Emails' && ($fieldName == 'subject')) {
+						$value = '<a href="javascript:;" onClick="ShowEmail(\'' . $recordId . '\');">' . textlength_check($value) . '</a>';
 				} else {
 					$value = textlength_check($value);
 				}
