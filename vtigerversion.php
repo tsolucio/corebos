642cd222e7f5f68b2ccb6533a513a688f0e6fff6
Joe Bordes <joe@tsolucio.com>
2016-08-03 16:07:31 +0200

    feat(Version) add commit info on zip or archive download

--- vtigerversion.php
+++ vtigerversion.php
@@ -14,6 +14,5 @@ $_SESSION['vtiger_version'] = $vtiger_current_version;
 $coreBOS_app_version = '5.8.0';
 $coreBOS_app_name ='coreBOS';
 $coreBOS_app_url = 'http://corebos.org';
-$coreBOS_commit_date = '$Date$';
-$coreBOS_commit_hash = '$Hash$';
+$coreBOS_commit_info = '$Format:%ci$ ($Format:%h$)';
 ?>
