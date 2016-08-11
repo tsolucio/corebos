0971f5cd51c765432d5bd6b50854c9c9ef1b2675
Luke1982 <gcx3@hotmail.com>
2016-08-11 17:58:17 +0200

    Vendors (dutch) 11-8-2016

--- modules/Vendors/language/nl_nl.lang.php
+++ modules/Vendors/language/nl_nl.lang.php
@@ -1,5 +1,4 @@
 <?php
-
 /*******************************************************************************
  * The contents of this file are subject to the following licences:
  * - SugarCRM Public License Version 1.1.2 http://www.sugarcrm.com/SPL
@@ -20,78 +19,59 @@
  * - Weltevree.org <www.Weltevree.org>
  ********************************************************************************/
 
-/*******************************************************************************
- * Vicus eBusiness Solutions Version Control
- * @package 	NL-Dutch
- * Description	Dutch language pack for vtiger CRM version 5.3.x
- * @author	$Author: luuk $
- * @version 	$Revision: 1.5 $ $Date: 2011/11/14 17:07:26 $
- * @source	$Source: /var/lib/cvs/vtiger530/Dutch/modules/Vendors/language/nl_nl.lang.php,v $
- * @copyright	Copyright (c)2005-2011 Vicus eBusiness Solutions bv <info@vicus.nl>
- * @license	vtiger CRM Public License Version 1.0 (by definition)
- ********************************************************************************/
-
 $mod_strings = array(
-
-'LBL_VENDOR_INFORMATION'=>'Leveranciersinformatie:',
-'LBL_CUSTOM_INFORMATION'=>'Overige informatie',
-'LBL_VENDOR_ADDRESS_INFORMATION'=>'Adresinformatie:',
-'LBL_DESCRIPTION_INFORMATION'=>'Omschrijving naam:',
-
-
-'Vendor Name'=>'Naam leverancier',
-'Name'=>'Naam',
-'Company Name'=>'Bedrijfsnaam',
-'Phone'=>'Telefoon',
-'Email'=>'E-mail',
-'Website'=>'Website',
-'GL Account'=>'Grootboekrekening',
-'Category'=>'Categorie',
-'Street'=>'Straat',
-'City'=>'Plaats',
-'State'=>'Provincie',
-'Postal Code'=>'Postcode',
-'Country'=>'Land',
-'Description'=>'Omschrijving',
-'Po Box'=>'Postbus',
-'Vendor'=>'Leverancier',
-
-//Added for existing Picklist entries
-
-'300-Sales-Software'=>'300-Verkoop-Software',
-'301-Sales-Hardware'=>'301-Verkoop-Hardware',
-'302-Rental-Income'=>'302-Huur-Inkomsten',
-'303-Interest-Income'=>'303-Rente-Inkomsten',
-'304-Sales-Software-Support'=>'304-Verkoop-Software-Ondersteuning',
-'305-Sales Other'=>'305-Verkoop anders',
-'306-Internet Sales'=>'306-Internet verkoop',
-'307-Service-Hardware Labor'=>'307-Dienst-Hardware arbeid',
-'308-Sales-Books'=>'308-Verkoop-Boeken',
-
-// Added/Updated for vtiger CRM 5.0.4
-'Created Time'=>'Gemaakt',
-'Modified Time'=>'Gewijzigd',
-
-// Added after 5.0.4 GA
-
-//added for find duplicate
-'LBL_DUPLICATE_DATA_IN'=>'Dupliceer ',
-'LBL_DELETE'=>'Verwijder',
-'LBL_MERGE_SELECT'=>'Voeg Geselecteerde Samen',
-'LBL_MERGE'=>'Samenvoegen',
-'LBL_MERGE_DATA_IN'=>'Voeg samen in ',
-'LBL_ACTION'=>'Actie',
-'LBL_FIELDLISTS'=> 'Lijst van de velden',
-'LBL_RECORD'=>'Regel #',
-
-// Module Sequence Numbering
-'Vendor No' => 'Leverancier Nr',
-// END
-
-
-/*
- * 5.2.0 changes 
- */
-'LBL_MODULE_NAME'=>'Leveranciers',
+	'LBL_VENDOR_INFORMATION' => 'Leveranciersinformatie:',
+	'LBL_CUSTOM_INFORMATION' => 'Overige informatie',
+	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Adresinformatie:',
+	'LBL_DESCRIPTION_INFORMATION' => 'Omschrijving naam:',
+	'Vendor Name' => 'Naam leverancier',
+	'Name' => 'Naam',
+	'Company Name' => 'Bedrijfsnaam',
+	'Phone' => 'Telefoon',
+	'Email' => 'E-mail',
+	'Website' => 'Website',
+	'GL Account' => 'Grootboekrekening',
+	'Category' => 'Categorie',
+	'Street' => 'Straat',
+	'City' => 'Plaats',
+	'State' => 'Provincie',
+	'Postal Code' => 'Postcode',
+	'Country' => 'Land',
+	'Description' => 'Omschrijving',
+	'Po Box' => 'Postbus',
+	'Vendor' => 'Leverancier',
+	'300-Sales-Software' => '300-Verkoop-Software',
+	'301-Sales-Hardware' => '301-Verkoop-Hardware',
+	'302-Rental-Income' => '302-Huur-Inkomsten',
+	'303-Interest-Income' => '303-Rente-Inkomsten',
+	'304-Sales-Software-Support' => '304-Verkoop-Software-Ondersteuning',
+	'305-Sales Other' => '305-Verkoop anders',
+	'306-Internet Sales' => '306-Internet verkoop',
+	'307-Service-Hardware Labor' => '307-Dienst-Hardware arbeid',
+	'308-Sales-Books' => '308-Verkoop-Boeken',
+	'Created Time' => 'Gemaakt',
+	'Modified Time' => 'Gewijzigd',
+	'LBL_DUPLICATE_DATA_IN' => 'Dupliceer ',
+	'LBL_DELETE' => 'Verwijder',
+	'LBL_MERGE_SELECT' => 'Voeg Geselecteerde Samen',
+	'LBL_MERGE' => 'Samenvoegen',
+	'LBL_MERGE_DATA_IN' => 'Voeg samen in ',
+	'LBL_ACTION' => 'Actie',
+	'LBL_FIELDLISTS' => 'Lijst van de velden',
+	'LBL_RECORD' => 'Regel #',
+	'Vendor No' => 'Leverancier Nr',
+	'LBL_MODULE_NAME' => 'Leveranciers',
+	'Last Modified By' => 'Laatst gewijzigd door',
+	'Assigned To' => 'Toegewezen aan',
+	'Products' => 'Producten',
+	'Purchase Order' => 'Inkooporder',
+	'Contacts' => 'Contacten',
+	'Emails' => 'E-mails',
+	'CobroPago' => 'Betalingen',
+	'Services' => 'Diensten',
+	'Timecontrol' => 'Urenregistratie',
+	'Activities' => 'Activiteiten',
+	'Activities History' => 'Activiteiten historie',
+	'InventoryDetails' => 'Voorraad Details',
 );
 ?>
