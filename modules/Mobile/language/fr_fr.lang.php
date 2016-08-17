7bd0d30ace305c0bf3ba4b6e8858cb9394946ee5
Joe Bordes <joe@tsolucio.com>
2016-08-18 01:05:20 +0200

    i18n(Mobile) copy language files from en_us to avoid error

---
+++ modules/Mobile/language/fr_fr.lang.php
@@ -0,0 +1,75 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$mod_strings = Array (
+'Mobile' => 'Mobile',
+'LBL_BACK' => 'Back',
+'LBL_EDIT' => 'Edit',
+'LBL_YES' => 'Yes',
+'LBL_NO' => 'No',
+'LBL_ON' => 'On',
+'LBL_OFF' => 'Off',
+'LBL_NEW' => 'New',
+'LBL_FILTER' => 'All-Select Filter',
+'LBL_MOD_LIST' => 'CRM Modules',
+'LBL_LIST' => 'List',
+'LBL_SEARCH' => 'Search',
+'LBL_SAVE' => 'Save',
+'LBL_CANCEL' => 'Cancel',
+'LBL_ADD' => 'Add Record',
+'LBL_TODAY' => 'Today',
+'LBL_WEEK' => 'Week',
+'LBL_MONTH' => 'Month',
+'LBL_YEAR' => 'Year',
+'LBL_USER' => 'User',
+'LBL_GROUP' => 'Group',
+'LBL_NONE' => '--None--',
+'LBL_NOT_ACCESSIBLE' => 'Not Accessible',
+'LBL_SHORT_TASK' => 'T',
+'LBL_SHORT_EVENT' => 'E',
+'LBL_CREATE' => 'Create',
+'LBL_CANCEL_BUTTON' => 'Cancel',
+'LBL_TASK_BUTTON' => 'Task',
+'LBL_EVENT_BUTTON' => 'Event',
+'LBL_LISTVIEW' => 'List View',
+'LBL_SEARCH_RESULTS' => 'Search Results',
+'LBL_DIALOG' => 'Dialog',
+'COMBO_LEADS' => 'Leads',
+'COMBO_ACCOUNTS' => 'Accounts',
+'COMBO_CONTACTS' => 'Contacts',
+'COMBO_POTENTIALS' => 'Potentials',
+'COMBO_PRODUCTS' => 'Products',
+'COMBO_HELPDESK' => 'Trouble Tickets',
+'COMBO_USERS' => 'Users',
+'COMBO_CAMPAIGNS' => 'Campaigns',
+'LBL_RELATED_TO'=>'Related to:',
+'LBL_DELETE'=>'Delete Data',
+'LBL_DELETE_COMMENT'=>'Are you sure?',
+'LBL_YES_BUTTON'=>'Yes',
+'LBL_SEARCH_AUTOCOMPLETE'=>'Search ...',
+'LBL_RELATED_LISTS'=>'Related Lists',
+'LBL_RELATED_LIST'=>'Related List',
+'LBL_RELATED_LISTS_COMMENT'=>'This function will be provided later.',
+'Leads'=>'Leads',
+'Accounts'=>'Accounts',
+'Contacts'=>'Contacts',
+'Potentials'=>'Potentials',
+'Products'=>'Products',
+'Helpdesk'=>'Helpdesk',
+'Users'=>'Users',
+'Campaigns'=>'Campaigns',
+'address'=>'Address',
+'otheraddress'=>'Other address',
+'bill_address'=>'Bill address',
+'ship_address'=>'Ship address',
+
+
+);
+
+?>
