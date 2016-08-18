faadacac968ba2a852866bc1fe84de547fe53f5b
Joe Bordes <joe@tsolucio.com>
2016-08-18 15:50:20 +0200

    i18n(Mobile) pt_br

--- modules/Mobile/Js/lang/pt_br.lang.js
+++ modules/Mobile/Js/lang/pt_br.lang.js
@@ -1,5 +1,4 @@
 /*********************************************************************************
-
 ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -7,34 +6,35 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  * German translation provided by crm-now, www.crm-now.de
+ * Contributor: Ranieri Slemer | 18-8-2016 | Translate Brazilian Portuguese | www.ecrmweb.com.br
  ********************************************************************************/
 //added for List menu 
 var mobiscroll_arr = {
-		ALERT_POSTS:'No More Records!',
-		ALERT_SCROLL:'Scroll for more or click here.',
-		ALERT_CLICK:'Click for more.',
-		ALERT_LOADING:'Loading...',
-		ALERT_SELECT_AN_EXISITNG_ENTRY: 'Please enter an existing entry stored at the CRM.',
-		ERROR_SEARCH_FAILED: 'Search failed - please contact your CRM Administrator.'
+		ALERT_POSTS:'Não há mais Registros!',
+		ALERT_SCROLL:'Deslize para mais ou clique aqui.',
+		ALERT_CLICK:'Clique para mais.',
+		ALERT_LOADING:'Carregando...',
+		ALERT_SELECT_AN_EXISITNG_ENTRY: 'Por favor entre com entrada armazenada no CRM.',
+		ERROR_SEARCH_FAILED: 'Pesquisa falou - por favor contate o suporte do CRM .'
 	};
 var cal_error_arr = {
-		ERROR_STARTDATETIME:'You can only plan activities for the future.',
-		ERROR_DUEDATE:'The due date has to be after the start date.'
+		ERROR_STARTDATETIME:'Você só pode especificar atividades planejada para o futuro.',
+		ERROR_DUEDATE:'Data de conclusão antes da data de início.'
 	};
 var cal_config_arr = {
-	monthNames: [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ],
-	dayNames: [ 'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday' ],
-	dayNamesShort: [ 'Sun','Mon','Tue','Wed', 'Thu','Fri','Sat' ],
-	txt_noEvents: "There are no events in this period",
+	monthNames: [ "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho","Julho", "Agosto", "Setembro", "outubro", "Novembro", "Dezembro" ],
+	dayNames: [ 'Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado' ],
+	dayNamesShort: [ 'Dom','Seg','Ter','Qua', 'Qui','Sex','Sáb' ],
+	txt_noEvents: "Não há mais eventos neste período",
 	txt_SpecificEvents_prev: "",
-	txt_SpecificEvents_after: "events:",
-	txt_next: "next",
-	txt_prev: "prev",
-	txt_NextEvents: "Next events:",
-	txt_GoToEventUrl: "See the event",
+	txt_SpecificEvents_after: "eventos:",
+	txt_next: "próx",
+	txt_prev: "Antes",
+	txt_NextEvents: "Próximos eventos:",
+	txt_GoToEventUrl: "Veja o evento",
 	txt_NumAbbrevTh: "th",
 	txt_NumAbbrevSt: "st",
 	txt_NumAbbrevNd: "nd",
 	txt_NumAbbrevRd: "rd",
-	txt_loading: "loading..."
+	txt_loading: "Carregando..."
 	};
