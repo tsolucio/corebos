f088b47c0f356a261db6dec1ef027f1108914f15
Joe Bordes <joe@tsolucio.com>
2016-08-25 01:32:05 +0200

    feat(Validation) validation library based on Validator

---
+++ include/validation/Validations.php
@@ -0,0 +1,70 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
+
+function validate_IBAN_BankAccount($field, $iban, $params, $fields) {
+	/* Ejemplos
+	Bank Name 	IBAN 	Entidad 	Oficina 	Digito Control 	Cuenta
+	CAIXABANK 	ES9121000418450200051332 	1210 	0418 	45 	0200051332
+	BANCO SANTANDER 	ES1800491500042710151321 	0049 	1500 	04 	2710151321
+	ABANCA CORPORACION BANCARIA 	ES3320805801143040000499 	2080 	5801 	14 	3040000499
+	BANCO DE CREDITO BALEAR 	ES0200246912500600865953 	0024 	6912 	50 	0600865953
+	BANCO ESPAÃ‘OL DE CREDITO 	ES0200302053020000875271 	0030 	2053 	02 	0000875271
+	BANCO SANTANDER 	ES1800492352072414205418 	0049 	2352 	07 	2414205418
+	IBERCAJA BANCO 	ES3320852066640300082802 	2085 	2066 	64 	0300082802
+	*/
+	// definimos un array de valores con el valor de cada letra
+	$letras = array (
+		"A" => 10,		"B" => 11,
+		"C" => 12,		"D" => 13,
+		"E" => 14,		"F" => 15,
+		"G" => 16,		"H" => 17,
+		"I" => 18,		"J" => 19,
+		"K" => 20,		"L" => 21,
+		"M" => 22,		"N" => 23,
+		"O" => 24,		"P" => 25,
+		"Q" => 26,		"R" => 27,
+		"S" => 28,		"T" => 29,
+		"U" => 30,		"V" => 31,
+		"W" => 32,		"X" => 33,
+		"Y" => 34,		"Z" => 35 
+	);
+
+	// Eliminamos los posibles espacios al inicio y final
+	$iban = trim($iban);
+	// Convertimos en mayusculas
+	$iban = strtoupper($iban);
+	// eliminamos espacio y guiones que haya en el iban
+	$iban = str_replace(array(' ','-'), '', $iban);
+
+	if (strlen($iban) == 24) {
+		// obtenemos los codigos de las dos letras
+		$valorLetra1 = $letras[substr($iban, 0, 1)];
+		$valorLetra2 = $letras[substr($iban, 1, 1)];
+		// obtenemos los siguientes dos valores
+		$siguienteNumeros = substr($iban, 2, 2);
+		$valor = substr($iban, 4, strlen($iban)) . $valorLetra1 . $valorLetra2 . $siguienteNumeros;
+		if (bcmod($valor, 97) == 1) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+	return false;
+}
+
+?>
---
+++ include/validation/load_validations.php
@@ -0,0 +1,34 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
+include_once 'include/validation/Validator.php';
+include_once 'include/validation/Validations.php';
+
+class cbValidator extends Validator {
+
+	public $CustomValidations = array(
+		'IBAN_BankAccount' => 'validate_IBAN_BankAccount',
+	);
+
+	public function __construct($data, $fields = array()) {
+		global $current_language;
+		parent::__construct($data, $fields, substr($current_language, 0, 2), 'include/validation/lang');
+		foreach ($this->CustomValidations as $validation => $function) {
+			$this->addRule($validation, $function);
+		}
+	}
+}
+
+?>
