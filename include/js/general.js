4317b59b0b4dd9f329fd82938614813acab02c1d
Joe Bordes <joe@tsolucio.com>
2016-09-01 01:23:20 +0200

    fix(Validation) use native serialize for faster form field retrieval and hide block screen on confirm message cancel

--- include/js/general.js
+++ include/js/general.js
@@ -1013,14 +1013,7 @@ function doModuleValidation(edit_type,editForm,callback) {
 			},
 			success: function(data) { //Validation file exists
 				if (data == 'yes') {
-					// Create object which gets the values of all input, textarea, select and button elements from the form
-					var myFields = document.forms[formName].elements;
-					var sentForm = new Object();
-					for (f=0; f<myFields.length; f++){
-						sentForm[myFields[f].name] = myFields[f].value;
-					}
-					//JSONize form data
-					sentForm = JSON.stringify(sentForm);
+					sentForm = document.forms[formName].serialize();
 					jQuery.ajax({
 						type : 'post',
 						data : {structure: sentForm},
@@ -1035,6 +1028,8 @@ function doModuleValidation(edit_type,editForm,callback) {
 									} else {
 										submitFormForAction(formName, action);
 									}
+								} else {
+									VtigerJS_DialogBox.unblock();
 								}
 							} else if (msg.search("%%%OK%%%") > -1) { //No error
 								if (typeof callback == 'function') {
