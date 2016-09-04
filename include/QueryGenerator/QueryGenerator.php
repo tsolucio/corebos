846ffcd719e7fa21ebcc5296abb32a8b45d107ea
Joe Bordes <joe@tsolucio.com>
2016-09-04 23:39:57 +0200

    feat(QueryGenerator) set default value of empty string for startGroup

--- include/QueryGenerator/QueryGenerator.php
+++ include/QueryGenerator/QueryGenerator.php
@@ -1264,7 +1264,7 @@ class QueryGenerator {
 		return array('name'=>$fieldname,'value'=>$value,'operator'=>$operator);
 	}
 
-	public function startGroup($groupType) {
+	public function startGroup($groupType='') {
 		$this->groupInfo .= " $groupType (";
 	}
 
