43e25133db54905a1e5fdce25391704cd8a26fe2
Kiko Jover <kiko@tsolucio.com>
2016-09-08 19:28:00 +0200

    Adds relation between Vendor and Documents.

---
+++ build/changeSets/setDocRelVendor.php
@@ -0,0 +1,51 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2014 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+* Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+* file except in compliance with the License. You can redistribute it and/or modify it
+* under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+* granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+* the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+* applicable law or agreed to in writing, software distributed under the License is
+* distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied. See the License for the specific language governing
+* permissions and limitations under the License. You may obtain a copy of the License
+* at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+*************************************************************************************************/
+
+class setDocRelVendor extends cbupdaterWorker {
+	
+	function applyChange() {
+		if ($this->hasError()) $this->sendError();
+		if ($this->isApplied()) {
+			$this->sendMsg('Changeset '.get_class($this).' already applied!');
+		} else {
+                    $module = 'Vendors';
+                    $relmodule = 'Documents';
+                    $mod = Vtiger_Module::getInstance($module);
+                    $relmod = Vtiger_Module::getInstance($relmodule);
+                    $mod->setRelatedList($relmod, 'Documents', array('ADD','SELECT'),'get_attachments');
+                    $this->sendMsg('Changeset '.get_class($this).' applied!');
+                    $this->markApplied();
+		}
+		$this->finishExecution();
+	}
+	
+	function undoChange() {
+		if ($this->hasError()) $this->sendError();
+		if ($this->isApplied()) {
+                    $module = 'Vendors';
+                    $relmodule = 'Documents';
+                    $mod = Vtiger_Module::getInstance($module);
+                    $relmod = Vtiger_Module::getInstance($relmodule);
+                    $mod->unsetRelatedList($relmod, 'Documents');
+                    $this->sendMsg('Changeset '.get_class($this).' undone!');
+                    $this->markUndone();
+		} else {
+			$this->sendMsg('Changeset '.get_class($this).' not applied, it cannot be undone!');
+		}
+		$this->finishExecution();
+	}
+	
+}
