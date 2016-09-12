ee66a7c846f7e6658dba2df6605321c924b03b57
Joe Bordes <joe@tsolucio.com>
2016-09-13 01:52:10 +0200

    feat(EMail) Tracking: launch workflows on access count increment

--- modules/Emails/TrackAccess.php
+++ modules/Emails/TrackAccess.php
@@ -14,21 +14,30 @@ header('Cache-Control: private', false);
 header('Content-Type: image/gif');
 header("Content-Length: 42");
 echo base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'); //equivalent to readfile('pixel.gif')
-ini_set("include_path","../../");
+chdir('../..');
 
 require_once 'include/utils/utils.php';
 
-include_once('../../config.inc.php');
+include_once('config.inc.php');
 global $application_unique_key;
 if(vtlib_purify($_REQUEST['app_key']) != $application_unique_key) {
 	exit;
 }
 
-global $adb;
+$Vtiger_Utils_Log = false;
+global $adb, $current_user;
+include_once('vtlib/Vtiger/Module.php');
 // index.php/modules/Emails/TrackAccess.php?record=133&mailid=197&app_key=17565f92d80ec6d79c0d50bd4ce5b05f
 $crmid = vtlib_purify($_REQUEST['record']);
 $mailid = vtlib_purify($_REQUEST['mailid']);
-$adb->pquery("INSERT INTO vtiger_email_access(crmid, mailid, accessdate, accesstime) VALUES(?,?,?,?)", array($crmid, $mailid, date('Y-m-d'), date('H:i:s')));
+$current_user = Users::getActiveAdminUser();
+$em = new VTEventsManager($adb);
+// Initialize Event trigger cache
+$em->initTriggerCache();
+$entityData = VTEntityData::fromEntityId($adb, $mailid);
+//Event triggering code
+$em->triggerEvent("vtiger.entity.beforesave", $entityData);
+$adb->pquery("INSERT INTO vtiger_email_access(crmid, mailid, accessdate, accesstime) VALUES(?,?,?,?)", array($crmid, $mailid, date('Y-m-d'), '0000-00-00 '.date('H:i:s')));
 
 $result = $adb->pquery("select count(*) as count from vtiger_email_access where crmid=? and mailid=?",array($crmid, $mailid));
 $count = $adb->query_result($result,0,'count');
@@ -39,5 +48,9 @@ if($result and $adb->num_rows($result)>0){
 } else {
 	$adb->pquery("insert into vtiger_email_track(crmid,mailid,access_count) values(?,?,?)", array($crmid, $mailid, 1));
 }
+
+//Event triggering code
+$entityData->set('access_count',$count+1);
+$em->triggerEvent("vtiger.entity.aftersave", $entityData);
 die();
 ?>
