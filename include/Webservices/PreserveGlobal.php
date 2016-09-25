80401b3ef693cb7a2796ae95dc02ad62123f2fb4
Joe Bordes <joe@tsolucio.com>
2016-09-25 02:16:43 +0200

    feat(PreserveGlobal) expose internal data for testing. cleanup code

--- include/Webservices/PreserveGlobal.php
+++ include/Webservices/PreserveGlobal.php
@@ -9,13 +9,13 @@
  *************************************************************************************/
 
 class VTWS_PreserveGlobal{
-	
+
 	private static $globalData = array();
-	
+
 	static function preserveGlobal($name,$value){
 		//$name store the name of the global.
 		global $$name;
-		
+
 		if(!isset(VTWS_PreserveGlobal::$globalData[$name]) or !is_array(VTWS_PreserveGlobal::$globalData[$name])){
 			VTWS_PreserveGlobal::$globalData[$name] = array();
 			VTWS_PreserveGlobal::$globalData[$name][] = $$name;
@@ -23,23 +23,27 @@ class VTWS_PreserveGlobal{
 		$$name = $value;
 		return $$name;
 	}
-	
+
 	static function restore($name){
 		//$name store the name of the global.
 		global $$name;
-		
+
 		if(is_array(VTWS_PreserveGlobal::$globalData[$name]) && count(VTWS_PreserveGlobal::$globalData[$name]) > 0){
 			$$name = array_pop(VTWS_PreserveGlobal::$globalData[$name]);
 			unset(VTWS_PreserveGlobal::$globalData[$name]);
 		}
 		$$name;
 	}
-	
+
 	static function getGlobal($name){
 		global $$name;
 		return VTWS_PreserveGlobal::preserveGlobal($name,$$name);
 	}
-	
+
+	static function getGlobalData(){
+		return VTWS_PreserveGlobal::$globalData;
+	}
+
 	static function flush(){
 		foreach (VTWS_PreserveGlobal::$globalData as $name => $detail) {
 			//$name store the name of the global.
@@ -50,7 +54,6 @@ class VTWS_PreserveGlobal{
 			}
 		}
 	}
-	
-}
 
+}
 ?>
