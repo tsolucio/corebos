fb9f84abfcb14ebdb4670feb3cc37c825257fcb2
Omar Llorens <omarllorens@gmail.com>
2016-10-05 13:53:38 +0200

    fix(Mobile) remove task value from activity type in you are in a event, is not necessary

--- modules/Mobile/api/ws/Utils.php
+++ modules/Mobile/api/ws/Utils.php
@@ -329,22 +329,13 @@ class Mobile_WS_Utils {
 		else if($module == 'Calendar' || $module == 'Events') {
 			foreach($describeInfo['fields'] as $index => $fieldInfo) {
 				$fieldInfo['uitype'] = self::fixUIType($module, $fieldInfo['name'], $fieldInfo['uitype']); 				
-				if ($fieldInfo['name'] == 'activitytype') {
-					// Provide the option to create Todo like anyother Event.
-					$taskTypeFound = false;
-					foreach ($fieldInfo['type']['picklistValues'] as $option) {
-						if ($option['value'] == 'Task') { $taskTypeFound = true; break; }
-					}
-					if (!$taskTypeFound) {
-						array_unshift($fieldInfo['type']['picklistValues'], array('label' => 'Task', 'value' => 'Task'));
-					}
-				} else if ($fieldInfo['name'] == 'visibility') {
+				if ($fieldInfo['name'] == 'visibility') {
 					if (empty($fieldInfo['type']['picklistValues'])) {
 						$fieldInfo['type']['picklistValues'] = self::visibilityValues();
 						$fieldInfo['type']['defaultValue'] = $fieldInfo['type']['picklistValues'][0]['value'];
 					}
 				}
-				$describeInfo['fields'][$index] = $fieldInfo;				
+				$describeInfo['fields'][$index] = $fieldInfo;
 			}
 		}
 	}
