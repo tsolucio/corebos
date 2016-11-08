36f618582801f8703dabe3aaf6e0038d2a8b4038
Joe Bordes <joe@tsolucio.com>
2016-11-08 22:50:24 +0100

    chore(Tooltip) show informative message on direct access instead of error

--- modules/Tooltip/language/nl_nl.lang.php
+++ modules/Tooltip/language/nl_nl.lang.php
@@ -39,7 +39,7 @@ $mod_strings = Array (
 'LBL_TOOLTIP_HELP_TEXT'=>'Selecteer de velden die u als tooltip wilt tonen',
 'LBL_FIELD'=>'Veld',
 'LBL_NOT_ASSETS'=>'Geen activa toegewezen.',
-
+'TooltipInfo' => 'This extension adds information hover tips on different fields on each module and is accessed in the settings of each module not directly, so you can eliminate it from the menu.',
 );
 
 ?>
