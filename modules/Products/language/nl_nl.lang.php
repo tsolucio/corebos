5649fa72dae46d0ba6621b1d3a180343e48058fc
Luke1982 <gcx3@hotmail.com>
2016-11-10 15:29:30 +0100

    Adds "divisible" validation for purchase orders

--- modules/Products/language/nl_nl.lang.php
+++ modules/Products/language/nl_nl.lang.php
@@ -244,6 +244,7 @@ $mod_strings = Array(
 'Parent Product' => 'Hoofdproduct',
 'Divisible' => 'Deelbaar',
 'DIVISIBLE_WARNING' => 'kan alleen in veelvouden van een verkocht worden',
+'DIVISIBLE_WARNING_PURCHASE' => 'kan alleen in veelvouden van een ingekocht worden'
 );
 
 ?>
