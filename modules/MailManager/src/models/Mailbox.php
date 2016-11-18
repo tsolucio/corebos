97c24e39684efdc79934586c0473493e2d935c5f
Joe Bordes <joe@tsolucio.com>
2016-11-18 20:31:43 +0100

    compat(MailManager) set value for integer field for strict mysql mode. code cleanup

--- modules/MailManager/src/models/Mailbox.php
+++ modules/MailManager/src/models/Mailbox.php
@@ -96,7 +96,7 @@ class MailManager_Model_Mailbox {
 	}
 	
 	function refreshTimeOut() {
-		return $this->mRefreshTimeOut;
+		return (empty($this->mRefreshTimeOut) ? 0 : $this->mRefreshTimeOut);
 	}
 
 	function delete() {
@@ -104,7 +104,7 @@ class MailManager_Model_Mailbox {
 		$adb->pquery("DELETE FROM vtiger_mail_accounts WHERE user_id = ? AND account_id = ?", array($current_user->id, $this->mId));
 	}
 	
-	function save() {	
+	function save() {
 		global $adb, $current_user;
 		$list_max_entries_per_page = GlobalVariable::getVariable('Application_ListView_PageSize',20,'Emails');
 		$account_id = 1;
@@ -132,8 +132,7 @@ class MailManager_Model_Mailbox {
 			$this->mId = $account_id;
 		}
 	}
-	
-	
+
 	static function activeInstance() {
 		global $adb, $current_user;
 		$instance = new MailManager_Model_Mailbox();
