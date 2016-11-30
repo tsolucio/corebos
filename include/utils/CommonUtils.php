baec663a1ab74ae954b11aaddfc882852f4c7354
Omar Llorens <omar@tsolucio.com>
2016-11-30 10:41:04 +0100

    fix(Duplicate Action) Protect setObjectValuesFromRequest function to not get the source recordid. This produce that Workflow condition ON_CREATE think doesn't works, thinks that your are editing

--- include/utils/CommonUtils.php
+++ include/utils/CommonUtils.php
@@ -1749,7 +1749,7 @@ function setObjectValuesFromRequest($focus) {
 	global $log;
 	$moduleName = get_class($focus);
 	$log->debug("Entering setObjectValuesFromRequest($moduleName) method ...");
-	if (isset($_REQUEST['record'])) {
+	if (isset($_REQUEST['record']) && $_REQUEST['isDuplicate'] != 'true') {
 		$focus->id = $_REQUEST['record'];
 	}
 	if (isset($_REQUEST['mode'])) {
