d2b245af39b999f964ab773cd8cb65916aff9a77
Joe Bordes <joe@tsolucio.com>
2016-11-30 23:07:00 +0100

    style(Reports) cleanup and format, eliminate warning message

--- modules/Reports/DuplicateReport.php
+++ modules/Reports/DuplicateReport.php
@@ -26,10 +26,9 @@ $numOfRows = $adb->num_rows($res);
 $response_array = array();
 
 if($numOfRows > 0) {
-	
 	global $current_user;
 	require('user_privileges/user_privileges_'.$current_user->id.'.php');
-	
+
 	$ogReport = new Reports($reportid);
 	$primarymodule = $ogReport->primodule;
 	$restrictedmodules = array();
@@ -46,58 +45,55 @@ if($numOfRows > 0) {
 			$restrictedmodules[] = $mod;
 		}
 	}
-	
+
 	if(isPermitted($primarymodule,'index') == "yes" && $modules_permitted == true) {
-		
 		$genQueryId = $adb->getUniqueID("vtiger_selectquery");
-		if($genQueryId != "") {			
+		if($genQueryId != "") {
 			$response_array['reportid'] = $genQueryId;
 			$response_array['folderid'] = $newreportfolder;
 			$response_array['errormessage'] = '';
-			
+
 			$iquerysql = "insert into vtiger_selectquery (QUERYID,STARTINDEX,NUMOFOBJECTS) values (?,?,?)";
 			$iquerysqlresult = $adb->pquery($iquerysql, array($genQueryId,0,0));
 			$log->info("Reports :: Save->Successfully saved vtiger_selectquery");
-			
+
 			if($iquerysqlresult != false) {
-				
-				$adb->pquery("INSERT INTO vtiger_selectcolumn (queryid,columnindex,columnname) 
+				$adb->pquery("INSERT INTO vtiger_selectcolumn (queryid,columnindex,columnname)
 						SELECT $genQueryId, columnindex, columnname FROM vtiger_selectcolumn WHERE queryid = ?", array($reportid));
-				
+
 				$adb->pquery("INSERT INTO vtiger_reportsharing (reportid,shareid,setype)
 						SELECT $genQueryId,shareid,setype FROM vtiger_reportsharing WHERE reportid=?", array($reportid));
-				
+
 				$owner = $current_user->id;
 				$ireportresult = $adb->pquery("INSERT INTO vtiger_report (reportid,folderid,reportname,description,reporttype,queryid,state,owner,sharingtype)
-						SELECT $genQueryId,$newreportfolder,'$newreportname','$newreportdescription',reporttype,$genQueryId,state,$owner,sharingtype FROM vtiger_report WHERE reportid=?", 
+						SELECT $genQueryId,$newreportfolder,'$newreportname','$newreportdescription',reporttype,$genQueryId,state,$owner,sharingtype FROM vtiger_report WHERE reportid=?",
 						array($reportid));
 				$log->info("Reports :: Save->Successfully saved vtiger_report");
 				if($ireportresult != false) {
-				
 					$adb->pquery("INSERT INTO vtiger_reportmodules (reportmodulesid,primarymodule,secondarymodules)
 							SELECT $genQueryId,primarymodule,secondarymodules FROM vtiger_reportmodules WHERE reportmodulesid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_reportmodules");
-				
+
 					$adb->pquery("INSERT INTO vtiger_reportsortcol (sortcolid,reportid,columnname,sortorder)
 							SELECT sortcolid,$genQueryId,columnname,sortorder FROM vtiger_reportsortcol WHERE reportid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_reportsortcol");
-				
+
 					$adb->pquery("INSERT INTO vtiger_reportdatefilter (datefilterid,datecolumnname,datefilter,startdate,enddate)
 							SELECT $genQueryId,datecolumnname,datefilter,startdate,enddate FROM vtiger_reportdatefilter WHERE datefilterid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_reportdatefilter");
-				
+
 					$adb->pquery("INSERT INTO vtiger_reportsummary (reportsummaryid,summarytype,columnname)
 							SELECT $genQueryId,summarytype,columnname FROM vtiger_reportsummary WHERE reportsummaryid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_reportsummary");
-				
+
 					$adb->pquery("INSERT INTO vtiger_relcriteria (queryid,columnindex,columnname,comparator,value,groupid,column_condition)
 							SELECT $genQueryId,columnindex,columnname,comparator,value,groupid,column_condition FROM vtiger_relcriteria WHERE queryid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_relcriteria");
-				
+
 					$adb->pquery("INSERT INTO vtiger_relcriteria_grouping (groupid,queryid,group_condition,condition_expression)
 							SELECT groupid,$genQueryId,group_condition,condition_expression FROM vtiger_relcriteria_grouping WHERE queryid=?", array($reportid));
 					$log->info("Reports :: Save->Successfully saved vtiger_relcriteria_grouping");
-					
+
 					$advft_criteria = $_REQUEST['advft_criteria'];
 					$advft_criteria_groups = $_REQUEST['advft_criteria_groups'];
 					if(!empty($advft_criteria) && !empty($advft_criteria_groups)) {
@@ -124,28 +120,25 @@ if($numOfRows > 0) {
 
 		$errormessage = "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
 		$errormessage .= "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 80%; position: relative; z-index: 10000000;'>
-
 		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
 		<tbody><tr>
 		<td rowspan='2' width='11%'><img src='". vtiger_imageurl('denied.gif', $theme) ."' ></td>
 		<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'><span class='genHeaderSmall'>".$mod_strings['LBL_NO_ACCESS']." : ".implode(",",$restrictedmodules)." </span></td>
 		</tr>
 		<tr>
-		<td class='small' align='right' nowrap='nowrap'>			   	
-		<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br></td>
+		<td class='small' align='right' nowrap='nowrap'>
+		<a href='javascript:window.history.back();'>".$app_strings['LBL_GO_BACK']."</a><br></td>
 		</tr>
-		</tbody></table> 
-		</div>";
-		$errormessage .= "</td></tr></table>";
-					
+		</tbody></table>
+		</div>
+		</td></tr></table>";
 		$response_array['errormessage'] = $errormessage;
 	}
-	
+
 } else {
-		$errormessage = "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";	
+		$errormessage = "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";
 		$errormessage .= "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
 		$errormessage .= "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 80%; position: relative; z-index: 10000000;'>
-
 		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
 		<tbody><tr>
 		<td rowspan='2' width='11%'><img src='". vtiger_imageurl('denied.gif', $theme) ."' ></td>
@@ -153,12 +146,11 @@ if($numOfRows > 0) {
 		</tr>
 		<tr>
 		<td class='small' align='right' nowrap='nowrap'>
-		<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br>                                                                                </td>
+		<a href='javascript:window.history.back();'>".$app_strings['LBL_GO_BACK']."</a><br></td>
 		</tr>
 		</tbody></table>
-		</div>";
-		$errormessage .= "</td></tr></table>";
-					
+		</div>
+		</td></tr></table>";
 		$response_array['errormessage'] = $errormessage;
 }
 
