d0ef1bba6ba220566d161034f3d214e78cc6d445
Joe Bordes <joe@tsolucio.com>
2016-12-04 01:02:01 +0100

    chore(Reports) bunnyjs datatable paging test

--- infscroll.js
+++ infscroll.js
@@ -1,111 +1,25 @@
-'use strict';
-function getPageId(n) {
-    return 'article-page-' + n;
-}
-function getDocumentHeight() {
-    var body = document.body;
-    var html = document.documentElement;
-    return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-}
-;
-function getScrollTop() {
-    return document.getElementById('scrollrange').scrollTop;
-}
-function getArticleImage() {
-    var hash = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-    var image = new Image();
-    image.className = 'article-list__item__image article-list__item__image--loading';
-    image.src = 'http://api.adorable.io/avatars/250/' + hash;
-    image.onload = function () {
-        image.classList.remove('article-list__item__image--loading');
-    };
-    return image;
-}
 
-function getAccounts(page){
-	return new Promise(function(resolve, reject) {
-		var url = 'infscroll.php?page='+page;
-		var req = new XMLHttpRequest();
-		req.open('GET', url, true);  // make call asynchronous
+/*import { Template } from '../../src/bunny.template';
+import { DataTable } from '../../src/DataTable';*/
 
-		req.onload = function() {
-			// check the status
-			if (req.status == 200) {
-				// Resolve the promise with the response text
-				resolve(req.response);
-			} else {
-				// Otherwise reject with the status text which will hopefully be a meaningful error
-				reject(Error(req.statusText));
-			}
-		};
-
-		// Handle errors
-		req.onerror = function() {
-			reject(Error("Network/Script Error"));
-		};
-
-		// Make the request
-		req.send();
-	});
-}
-function getArticle(page) {
-    //var articleImage = getArticleImage();
-    return getAccounts(page).then(function(response) {
-    	var rsp = document.createElement('div');
-    	rsp.innerHTML=response;
-    var article = document.createElement('article');
-    article.className = 'article-list__item';
-    article.appendChild(rsp);
-    return article;
+Template.define('users_row_template', {
+    edit: (btn, user) => {
+        btn.href += user.index;
+    },
+    remove: (btn, user) => {
+        btn.href += user.index;
+        btn.addEventListener('click', (e) => {
+            e.preventDefault();
+            // show custom confirm popup
+            // in this example we will just call native confirm()
+            if (confirm('Are you sure?')) {
+                window.location = btn.href;
+            }
+        })
+    }
 });
-}
-function getArticlePage(page) {
-    var articlesPerPage = arguments.length <= 1 || arguments[1] === undefined ? 2 : arguments[1];
-    var pageElement = document.createElement('div');
-    pageElement.id = getPageId(page);
-    pageElement.className = 'article-list__page';
-    var ps = new Array();
-    while (articlesPerPage--) {
-        var prm = getArticle(page).then(function(response) {
-        pageElement.appendChild(response);
-        });
-        ps.push(prm);
-    }
-    return Promise.all(ps).then(function(resp) {
-    	return pageElement;
-    });
-}
-function addPaginationPage(page) {
-    var pageLink = document.createElement('a');
-    pageLink.href = '#' + getPageId(page);
-    pageLink.innerHTML = page;
-    var listItem = document.createElement('li');
-    listItem.className = 'article-list__pagination__item';
-    listItem.appendChild(pageLink);
-    //articleListPagination.appendChild(listItem);
-    articleListPagination.innerHTML = articleListPagination.innerHTML+' '+pageLink;
-    if (page === 2) {
-        articleListPagination.classList.remove('article-list__pagination--inactive');
-    }
-}
-function fetchPage(page) {
-	return getArticlePage(page).then(function(response){
-    articleList.appendChild(response);
-	});
-}
-function addPage(page) {
-    return fetchPage(page).then(function(response){
-    addPaginationPage(page);
-    });
-}
-var articleList = document.getElementById('article-list');
-var articleListPagination = document.getElementById('article-list-pagination');
-var page = 0;
-addPage(++page);
-function scrollrange_onscroll() {
-	var scrollobject = document.getElementById('scrollrange');
-	var soheight = scrollobject.scrollHeight-scrollobject.offsetHeight;
-	if (scrollobject.scrollTop >= soheight && scrollobject.scrollTop <= soheight+4.5) {
-		addPage(++page);
-	}
-};
+
+DataTable.onRedraw(document.getElementsByTagName('datatable')[0], (data) => {
+    console.info('Switched to page ' + data.current_page);
+    console.info('First user name is:' + data.data[0].name);
+});
--- infscroll.php
+++ infscroll.php
@@ -4,11 +4,27 @@ $Vtiger_Utils_Log = true;
 
 require_once 'include/utils/utils.php';
 include_once('vtlib/Vtiger/Module.php');
-
-$page = $_REQUEST['page'];
-$prs = $adb->pquery("select * from vtiger_account order by accountname limit $page,10",array());
-$pcontent='';
-while ($a = $adb->fetch_array($prs)) {
-	$pcontent .= $a['accountname'].'<br>';
+require_once('modules/CustomView/CustomView.php');
+require_once('modules/Reports/Reports.php');
+include_once('modules/Reports/ReportRun.php');
+$current_user = new Users();
+$current_user->retrieveCurrentUserInfoFromFile(Users::getActiveAdminId());
+if(isset($_SESSION['authenticated_user_language']) && $_SESSION['authenticated_user_language'] != '') {
+	$current_language = $_SESSION['authenticated_user_language'];
+} else {
+	if(!empty($current_user->language)) {
+		$current_language = $current_user->language;
+	} else {
+		$current_language = $default_language;
+	}
 }
-echo $pcontent;
+$app_strings = return_application_language($current_language);
+
+$reportid = 29;
+$currentModule = 'Reports';
+$ogReport = new Reports($reportid);
+$oReportRun = new ReportRun($reportid);
+$oReportRun->page = $_REQUEST['page'];
+$__oReportRunReturnValue = $oReportRun->GenerateReport("JSONPAGED", false);
+
+echo $__oReportRunReturnValue;
