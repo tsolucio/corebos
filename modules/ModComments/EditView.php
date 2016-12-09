1be5d3cceb72f8430f727d81387e7f750d67e1ea
Joe Bordes <joe@tsolucio.com>
2016-12-09 20:54:58 +0100

    feat(ModComments) convert to normal module: first step; module files

--- modules/ModComments/EditView.php
+++ modules/ModComments/EditView.php
@@ -7,21 +7,7 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-global $app_strings, $mod_strings, $current_language, $currentModule, $theme;
-require_once('Smarty_setup.php');
-
-// Forcefully disable Editing For Any User
-if (!empty($_REQUEST['record'])) {
-	$smarty = new vtigerCRM_Smarty;
-	$smarty->assign("MOD",$mod_strings);
-	$smarty->assign("APP",$app_strings);
-	$smarty->assign("THEME", "$theme");
-	$smarty->assign("IMAGE_PATH", "themes/$theme/images/");
-
-	$smarty->display(vtlib_getModuleTemplate('Vtiger','OperationNotPermitted.tpl'));
-	exit;
-}
-
 require_once 'modules/Vtiger/EditView.php';
+
 $smarty->display('salesEditView.tpl');
 ?>
