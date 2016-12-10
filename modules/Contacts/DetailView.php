79d2da165fb3834dd4169c9f8fd45796caddad06
Joe Bordes <joe@tsolucio.com>
2016-12-10 13:07:05 +0100

    fix(Contacts) sync detail view with base class to get current functionality and eliminate warnings

--- modules/Contacts/DetailView.php
+++ modules/Contacts/DetailView.php
@@ -11,13 +11,14 @@ require_once('Smarty_setup.php');
 require_once('data/Tracker.php');
 require_once('include/CustomFieldUtil.php');
 require_once('include/utils/utils.php');
-require('user_privileges/default_module_view.php');
 
 global $mod_strings, $app_strings, $currentModule, $current_user, $theme, $log;
 
 $focus = CRMEntity::getInstance($currentModule);
 
-$tool_buttons = Button_Check($currentModule);
+if(isset($tool_buttons)==false) {
+	$tool_buttons = Button_Check($currentModule);
+}
 $smarty = new vtigerCRM_Smarty();
 
 $record = vtlib_purify($_REQUEST['record']);
@@ -34,14 +35,19 @@ if($record != '') {
 
 	$focus->id = $record;
 	$focus->retrieve_entity_info($record, $currentModule);
+	$focus->name=$focus->column_fields[$focus->list_link_field];
 	$focus->firstname=$focus->column_fields['firstname'];
 	$focus->lastname=$focus->column_fields['lastname'];
 }
 if($isduplicate == 'true') $focus->id = '';
+$errormessageclass = isset($_REQUEST['error_msgclass']) ? vtlib_purify($_REQUEST['error_msgclass']) : '';
+$errormessage = isset($_REQUEST['error_msg']) ? vtlib_purify($_REQUEST['error_msg']) : '';
+$smarty->assign('ERROR_MESSAGE_CLASS', $errormessageclass);
+$smarty->assign('ERROR_MESSAGE', $errormessage);
 $focus->preViewCheck($_REQUEST, $smarty);
 
 // Identify this module as custom module.
-$smarty->assign('CUSTOM_MODULE', false);
+$smarty->assign('CUSTOM_MODULE', $focus->IsCustomModule);
 
 $smarty->assign('APP', $app_strings);
 $smarty->assign('MOD', $mod_strings);
@@ -52,6 +58,7 @@ $smarty->assign('CATEGORY', $category);
 $smarty->assign('IMAGE_PATH', "themes/$theme/images/");
 $smarty->assign('THEME', $theme);
 $smarty->assign('ID', $focus->id);
+$smarty->assign('RECORDID', $focus->id);
 $smarty->assign('MODE', $focus->mode);
 
 $recordName = array_values(getEntityName($currentModule, $focus->id));
@@ -72,6 +79,9 @@ $validationArray = split_validationdataArray(getDBValidationData($focus->tab_nam
 $smarty->assign('VALIDATION_DATA_FIELDNAME',$validationArray['fieldname']);
 $smarty->assign('VALIDATION_DATA_FIELDDATATYPE',$validationArray['datatype']);
 $smarty->assign('VALIDATION_DATA_FIELDLABEL',$validationArray['fieldlabel']);
+$smarty->assign('TODO_PERMISSION',CheckFieldPermission('parent_id','Calendar'));
+$smarty->assign('EVENT_PERMISSION',CheckFieldPermission('parent_id','Events'));
+$smarty->assign('CONTACT_PERMISSION',CheckFieldPermission('contact_id','Calendar'));
 
 $smarty->assign('EDIT_PERMISSION', isPermitted($currentModule, 'EditView', $record));
 $smarty->assign('CHECK', $tool_buttons);
@@ -134,23 +144,32 @@ $sql = $adb->pquery('select accountid from vtiger_contactdetails where contactid
 $accountid = $adb->query_result($sql,0,'accountid');
 if($accountid == 0) $accountid='';
 $smarty->assign('accountid',$accountid);
-$smarty->assign('TODO_PERMISSION',CheckFieldPermission('parent_id','Calendar'));
-$smarty->assign('CONTACT_PERMISSION',CheckFieldPermission('contact_id','Calendar'));
-$smarty->assign('EVENT_PERMISSION',CheckFieldPermission('parent_id','Events'));
 $smarty->assign('IS_REL_LIST', isPresentRelatedLists($currentModule));
+$isPresentRelatedListBlock = isPresentRelatedListBlock($currentModule);
+$smarty->assign('IS_RELBLOCK_LIST', $isPresentRelatedListBlock);
+$singlepane_view = GlobalVariable::getVariable('Application_Single_Pane_View', 0, $currentModule);
+$singlepane_view = empty($singlepane_view) ? 'false' : 'true';
 $smarty->assign('SinglePane_View', $singlepane_view);
-
-if($singlepane_view == 'true') {
+$smarty->assign('HASRELATEDPANES', 'false');
+if($singlepane_view == 'true' or $isPresentRelatedListBlock) {
 	$related_array = getRelatedLists($currentModule,$focus);
 	$smarty->assign("RELATEDLISTS", $related_array);
 
 	require_once('include/ListView/RelatedListViewSession.php');
 	if(!empty($_REQUEST['selected_header']) && !empty($_REQUEST['relation_id'])) {
-		RelatedListViewSession::addRelatedModuleToSession(vtlib_purify($_REQUEST['relation_id']),
-				vtlib_purify($_REQUEST['selected_header']));
+		RelatedListViewSession::addRelatedModuleToSession(vtlib_purify($_REQUEST['relation_id']),vtlib_purify($_REQUEST['selected_header']));
 	}
 	$open_related_modules = RelatedListViewSession::getRelatedModulesFromSession();
 	$smarty->assign("SELECTEDHEADERS", $open_related_modules);
+} else {
+	$bmapname = $currentModule.'RelatedPanes';
+	$cbMapid = GlobalVariable::getVariable('BusinessMapping_'.$bmapname, cbMap::getMapIdByName($bmapname));
+	if ($cbMapid) {
+		$cbMap = cbMap::getMapByID($cbMapid);
+		$rltabs = $cbMap->RelatedPanes($focus->id);
+		$smarty->assign('RLTabs', $rltabs['panes']);
+		$smarty->assign('HASRELATEDPANES', 'true');
+	}
 }
 
 if(isPermitted($currentModule, 'CreateView', $record) == 'yes')
@@ -159,8 +178,8 @@ if(isPermitted($currentModule, 'Delete', $record) == 'yes')
 	$smarty->assign('DELETE', 'permitted');
 
 $blocks = getBlocks($currentModule,'detail_view','',$focus->column_fields);
-$custom_blocks = getCustomBlocks($currentModule,'detail_view');
 $smarty->assign('BLOCKS', $blocks);
+$custom_blocks = getCustomBlocks($currentModule,'detail_view');
 $smarty->assign('CUSTOMBLOCKS', $custom_blocks);
 $smarty->assign('FIELDS',$focus->column_fields);
 $smarty->assign("BLOCKINITIALSTATUS",$_SESSION['BLOCKINITIALSTATUS']);
@@ -168,6 +187,24 @@ $smarty->assign("BLOCKINITIALSTATUS",$_SESSION['BLOCKINITIALSTATUS']);
 include_once('vtlib/Vtiger/Link.php');
 $customlink_params = Array('MODULE'=>$currentModule, 'RECORD'=>$focus->id, 'ACTION'=>vtlib_purify($_REQUEST['action']));
 $smarty->assign('CUSTOM_LINKS', Vtiger_Link::getAllByType(getTabid($currentModule), Array('DETAILVIEWBASIC','DETAILVIEW','DETAILVIEWWIDGET'), $customlink_params));
+if($isPresentRelatedListBlock) {
+	$related_list_block = array();
+	foreach ($blocks as $blabel => $binfo) {
+		if (!empty($binfo['relatedlist'])) {
+			foreach ($related_array as $rlabel => $rinfo) {
+				if ($rinfo['relationId']==$binfo['relatedlist']) {
+					$related_list_block[$binfo['relatedlist']] = array($rlabel=>$rinfo);
+					break;
+				}
+			}
+		}
+	}
+	$smarty->assign('RELATEDLISTBLOCK', $related_list_block);
+}
+
+// Hide Action Panel
+$DEFAULT_ACTION_PANEL_STATUS = GlobalVariable::getVariable('Application_Action_Panel_Open',1);
+$smarty->assign('DEFAULT_ACTION_PANEL_STATUS',($DEFAULT_ACTION_PANEL_STATUS ? '' : 'display:none'));
 
 // Record Change Notification
 $focus->markAsViewed($current_user->id);
