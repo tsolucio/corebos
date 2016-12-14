839b6570db157341a6f55f35e49df6c8e85e2a12
Lorida Cito <loridacito@gmail.com>
2016-12-14 13:53:32 +0100

    feat(Calendar4You) get dynamic tabid + change sequence of links

--- build/changeSets/addlinksforcalendar.php
+++ build/changeSets/addlinksforcalendar.php
@@ -7,8 +7,12 @@ class addlinksforcalendar extends cbupdaterWorker {
 			$this->sendMsg('Changeset '.get_class($this).' already applied!');
 		} else {
 			global $adb;
-			$link = Vtiger_Link::addlink(55, 'HEADERSCRIPT', 'Calendar4You_HeaderScript3', 'modules/Calendar4You/fullcalendar/lib/moment.min.js');
-                       	$link2 = Vtiger_Link::addlink(55, 'HEADERCSS', 'Calendar4You_HeaderStyle2', 'modules/Calendar4You/fullcalendar/themes/cupertino/jquery-ui.min.css');
+                        $query=$adb->query("select tabid from vtiger_tab where name='Calendar4You'");
+                        $tabid=$adb->query_result($query,0,0);
+			$link = Vtiger_Link::addlink($tabid, 'HEADERSCRIPT', 'Calendar4You_HeaderScript3', 'modules/Calendar4You/fullcalendar/lib/moment.min.js');
+                       	$link2 = Vtiger_Link::addlink($tabid, 'HEADERCSS', 'Calendar4You_HeaderStyle2', 'modules/Calendar4You/fullcalendar/themes/cupertino/jquery-ui.min.css');
+                        $adb->query("UPDATE vtiger_links SET sequence = '1' WHERE  linkurl='modules/Calendar4You/fullcalendar/fullcalendar.js'");
+                        $adb->query("UPDATE vtiger_links SET sequence = '2' WHERE  linkurl='modules/Calendar4You/Calendar4You.js'");
                         $this->sendMsg('Changeset '.get_class($this).' applied!');
 			$this->markApplied();
 		}
