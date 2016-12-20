ce596fbe547ee19ed3644606afb8914214e6ff7c
Joe Bordes <joe@tsolucio.com>
2016-12-21 00:35:13 +0100

    fix(EmailTemplates) mysql strict error saving template

--- modules/Settings/saveemailtemplate.php
+++ modules/Settings/saveemailtemplate.php
@@ -32,7 +32,7 @@ if(isset($templateid) && $templateid !='')
 else
 {
 	$templateid = $db->getUniqueID('vtiger_emailtemplates');
-	$sql = "insert into vtiger_emailtemplates values (?,?,?,?,?,?,?,?)";
+	$sql = "insert into vtiger_emailtemplates (foldername, templatename, subject, description, body, deleted, templateid, frommail) values (?,?,?,?,?,?,?,?)";
 	$params = array($folderName, $templateName, $subject, $description, $body, 0, $templateid, $emailfrom);
 	$adb->pquery($sql, $params);
 
