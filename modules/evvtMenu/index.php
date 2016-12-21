c18aa8a507123fafc46bb4da075b75187afeaa3d
Joe Bordes <joe@tsolucio.com>
2016-12-21 12:06:59 +0100

    fix(Menu) permit authorized non-admin users to manage menu and eliminate obsolete kendoui reference

--- modules/evvtMenu/index.php
+++ modules/evvtMenu/index.php
@@ -16,30 +16,8 @@
 *  Version      : 1.0
 *  Author       : JPL TSolucio, S. L.
 *************************************************************************************************/
-
-global $app_strings, $mod_strings, $current_language, $currentModule, $theme,$current_user;
+global $app_strings, $mod_strings, $current_language, $currentModule, $theme, $current_user;
 require('user_privileges/user_privileges_'.$current_user->id.'.php');
-
-if(!$is_admin) {
-	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
-	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
-
-		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
-		<tbody><tr>
-		<td rowspan='2' width='11%'><img src= " . vtiger_imageurl('denied.gif', $theme) . " ></td>
-		<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'>
-			<span class='genHeaderSmall'>$app_strings[LBL_PERMISSION]</span></td>
-		</tr>
-		<tr>
-		<td class='small' align='right' nowrap='nowrap'>
-		<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br>
-		</td>
-		</tr>
-		</tbody></table>
-		</div>";
-	echo "</td></tr></table>";
-	exit;
-}
 
 require_once('Smarty_setup.php');
 require_once('modules/evvtMenu/evvtMenu.inc');
@@ -66,9 +44,6 @@ $smarty->assign('MODULE', $currentModule);
 $smarty->assign('CATEGORY', $category);
 $smarty->assign('IMAGE_PATH', "themes/$theme/images/");
 $smarty->assign('MODE',$mode);
-global $KENDOUI_URL;
-$smarty->assign('KUIDIR', rtrim($KENDOUI_URL,'/').'/');
 
 $smarty->display(vtlib_getModuleTemplate($currentModule,'index.tpl'));
-
 ?>
