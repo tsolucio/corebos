d35f4a9eba7e375957fd2be8de483c0d3209652d
Joe Bordes <joe@tsolucio.com>
2016-12-21 11:07:29 +0100

    fix(Menu) eliminate loading style on inactive modules which was breaking the UL menu element

--- index.php
+++ index.php
@@ -639,7 +639,6 @@ if($display == "no")
 }
 // vtlib customization: Check if module has been de-activated
 else if(!vtlib_isModuleActive($currentModule) and !($currentModule=='Tooltip' and $action==$module."Ajax" and $_REQUEST['file']=='ComputeTooltip')) {
-	echo "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";
 	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
 	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 85%; position: relative; z-index: 10000000;'>
 		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
