30734f8bda61e1319d03dc611dafa20a1faef053
Joe Bordes <joe@tsolucio.com>
2016-12-22 01:22:31 +0100

    fix(Tooltip/GlobalVariable) permit access to tooltip and globalvariable calculation for all users even if the module is not accessible in the profile

--- index.php
+++ index.php
@@ -606,8 +606,10 @@ if(!$skipSecurityCheck && $use_current_login)
 	$seclog->debug('########### Pemitted ---> yes  ##############');
 }
 
-if($display == "no")
-{
+if($display == "no"
+		and !(($currentModule=='Tooltip' and $action==$module."Ajax" and $_REQUEST['file']=='ComputeTooltip')
+			or ($currentModule=='GlobalVariable' and $action==$module."Ajax" and $_REQUEST['file']=='SearchGlobalVar'))
+	) {
 	echo "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";
 	if ($action==$module."Ajax") {
 	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%'><tr><td align='center'>";
@@ -638,7 +640,10 @@ if($display == "no")
 	}
 }
 // vtlib customization: Check if module has been de-activated
-else if(!vtlib_isModuleActive($currentModule) and !($currentModule=='Tooltip' and $action==$module."Ajax" and $_REQUEST['file']=='ComputeTooltip')) {
+else if(!vtlib_isModuleActive($currentModule)
+		and !(($currentModule=='Tooltip' and $action==$module."Ajax" and $_REQUEST['file']=='ComputeTooltip')
+			or ($currentModule=='GlobalVariable' and $action==$module."Ajax" and $_REQUEST['file']=='SearchGlobalVar'))
+	) {
 	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
 	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 85%; position: relative; z-index: 10;'>
 		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
