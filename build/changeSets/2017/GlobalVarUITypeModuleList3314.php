5d620d55d65c4bc4932c3edca55b71f216579e77
Joe Bordes <joe@tsolucio.com>
2017-01-03 01:42:11 +0100

    fix(GlobalVar) change modulelist field to uitype 3314 to support non-entity modules/extensions

---
+++ build/changeSets/2017/GlobalVarUITypeModuleList3314.php
@@ -0,0 +1,36 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+* Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+* file except in compliance with the License. You can redistribute it and/or modify it
+* under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+* granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+* the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+* applicable law or agreed to in writing, software distributed under the License is
+* distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied. See the License for the specific language governing
+* permissions and limitations under the License. You may obtain a copy of the License
+* at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+*************************************************************************************************/
+
+class GlobalVarUITypeModuleList3314 extends cbupdaterWorker {
+
+	function applyChange() {
+		if ($this->hasError()) $this->sendError();
+		if ($this->isApplied()) {
+			$this->sendMsg('Changeset '.get_class($this).' already applied!');
+		} else {
+			$moduleInstance = Vtiger_Module::getInstance('GlobalVariable');
+			// change uitype
+			$field = Vtiger_Field::getInstance('module_list',$moduleInstance);
+			if ($field) {
+				$this->ExecuteQuery("update vtiger_field set uitype=3314 where fieldid=".$field->id);
+			}
+			$this->sendMsg('Changeset '.get_class($this).' applied!');
+			$this->markApplied();
+		}
+		$this->finishExecution();
+	}
+
+}
