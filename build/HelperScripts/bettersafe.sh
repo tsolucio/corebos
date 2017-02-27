61316addf30a9742b7431f740eca3c127051fcbc
Joe Bordes <joe@tsolucio.com>
2017-02-27 16:27:11 +0100

    feat(HelperSctipts) add more information and options to bettersafe script

--- build/HelperScripts/bettersafe.sh
+++ build/HelperScripts/bettersafe.sh
@@ -25,7 +25,15 @@ done
 
 echo
 echo "Deactivate all modules you are not using. Besides being more secure the application will be faster."
-echo "Optionally you can deactivate and eliminate (completely uninstall) these modules:"
+echo "Optionally you can"
+echo
+echo "=> Deactivate and eliminate (completely uninstall) these modules:"
 echo " - FieldFormulas  this module is totally obsolete > move any rules you have there to workflows"
 echo " - evvtApps  as far as I know nobody is using this"
 echo
+echo "=> Eliminate the SOAP interface extensions you are not using:"
+echo " - soap/customerportal.php  (note: there is a global variable to deactivate this one)"
+echo " - soap/vtigerolservice.php  (I don't think this one is used at all, it is all done with webservice now)"
+echo " - soap/wordplugin.php  (I doubt this even works anymore: use OpenOffice integration)"
+
+echo "=> Deactivate webservice access with the Webservice_Enabled global variable if you are not using this interface."
