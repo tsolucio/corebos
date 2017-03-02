1a459d1e8ca060903ba5c1964e28eec645bd4a10
albana <a.celepija@studiosynthesis.biz>
2017-03-02 10:52:45 +0100

    first changes for GoogleContacts

---
+++ modules/WSAPP/language/hu_hu.lang.php
@@ -0,0 +1,14 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$mod_strings = array(
+'WSAPP' => 'WSAPP',
+'WSAPPInfo' => 'This extension enhances the application with a set of internal functions to sync with Outlook. It has no visible functionality so you can eliminate it from the menu.',
+);
+?>
