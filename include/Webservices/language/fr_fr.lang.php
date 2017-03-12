e9d88d5c894f0343c48522ee7dfa955a5bca99d0
polanskiman <lefevre_paul@yahoo.fr>
2017-03-12 13:53:33 +0700

    i18n(fr_fr) update FR translation

---
+++ include/Webservices/language/fr_fr.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*********************************************************************************
+ ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+  * ("License"); You may not use this file except in compliance with the License
+  * The Original Code is:  vtiger CRM Open Source
+  * The Initial Developer of the Original Code is vtiger.
+  * Portions created by vtiger are Copyright (C) vtiger.
+  * All Rights Reserved.
+ *
+  ********************************************************************************/
+$app_strings = array (
+	'Groups'=>'Groupes',
+	'DocumentFolders'=>'Dossiers Documents',
+	'Currency'=>'Devises',
+	'SINGLE_Groups'=>'Groupe',
+	'SINGLE_DocumentFolders'=>'Dossiers Document',
+	'SINGLE_Currency'=>'Devise',
+);
+
+$webservice_strings = array(
+	'LBL_INVALID_OLD_PASSWORD' => 'Le mot de passe antérieur est invalide.',
+	'LBL_NEW_PASSWORD_MISMATCH' => "Le Nouveau mot de passe et la confirmation ne correspondent pas.",
+	'LBL_DATABASE_QUERY_ERROR' => 'Erreur: l\'opération a générée une erreur dans la base de donnée.',
+	'LBL_CHANGE_PASSWORD_FAILURE' => 'La modification du mot de passe à échouée.',
+);
+
+?>
