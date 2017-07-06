908894f4c15019c473e39d8c6e88ed0a44941a05
Joe Bordes <joe@tsolucio.com>
2017-07-07 01:19:07 +0200

    feat(Translation) translation infrastructure module

---
+++ modules/cbtranslation/DetailView.php
@@ -0,0 +1,19 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+require_once('Smarty_setup.php');
+
+global $mod_strings, $app_strings, $currentModule, $current_user, $theme, $log;
+
+$smarty = new vtigerCRM_Smarty();
+
+require_once 'modules/Vtiger/DetailView.php';
+
+$smarty->display('DetailView.tpl');
+?>
