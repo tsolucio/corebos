3c9e6e911796d9022e8e3a41bf17c21e2c52e20b
Joe Bordes <joe@tsolucio.com>
2017-08-15 18:44:38 +0200

    security(Announcement) XSS in announcment

--- include/utils/CommonUtils.php
+++ include/utils/CommonUtils.php
@@ -2288,7 +2288,8 @@ function get_announcements() {
 	global $default_charset, $currentModule;
 	$announcement = GlobalVariable::getVariable('Application_Announcement','',$currentModule);
 	if ($announcement != '') {
-			$announcement = html_entity_decode($announcement, ENT_QUOTES, $default_charset);
+		$announcement = html_entity_decode($announcement, ENT_QUOTES, $default_charset);
+		$announcement = vtlib_purify($announcement);
 	}
 	return $announcement;
 }
