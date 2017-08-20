7fe02c34d0be31a2aacc2ef550bc57cb1f08cf6d
Joe Bordes <joe@tsolucio.com>
2017-08-20 18:20:57 +0200

    chore(Users) eliminate unused code to change password which was for before we had the change password button

--- modules/Users/Save.php
+++ modules/Users/Save.php
@@ -131,17 +131,6 @@ if(empty($_REQUEST['changepassword']) || $_REQUEST['changepassword'] != 'true')
 
 	$return_id = $focus->id;
 
-	if (isset($_POST['user_name']) && isset($_POST['new_password'])) {
-		$new_pass = $_POST['new_password'];
-		$new_passwd = $_POST['new_password'];
-		$new_pass = md5($new_pass);
-		$uname = $_POST['user_name'];
-		if (!$focus->change_password($_POST['confirm_new_password'], $_POST['new_password'])) {
-			header("Location: index.php?action=DetailView&module=Users&record=".$focus->id."&error_string=".urlencode($focus->error_string));
-			exit;
-		}
-	}
-
 	if(isset($focus->id) && $focus->id != '')
 	{
 		if(isset($_POST['group_name']) && $_POST['group_name'] != '') {
