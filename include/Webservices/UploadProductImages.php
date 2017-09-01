76687b8f6e5f978dc12bd649da583817796b948c
Joe Bordes <joe@tsolucio.com>
2017-09-01 10:48:02 +0200

    fix(Webservice) add support for mass upload of images into product multi-image field. related to 835b088d9b6493d8df3b5cf9fb64131524ff5a98. Thanks John

--- include/Webservices/UploadProductImages.php
+++ include/Webservices/UploadProductImages.php
@@ -1,142 +1,100 @@
 <?php
-/*+***********************************************************************************
- * The contents of this file are subject to the vtiger CRM Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is:  vtiger CRM Open Source
- * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
- * All Rights Reserved.
- *************************************************************************************/
+/*************************************************************************************************
+ * Copyright 2017 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
 
-//include 'vtlib/Vtiger/Module.php';
-function cbws_uploadProductImages($recordID, $fileData, $user)
-{
+function cbws_uploadProductImages($recordID, $fileData, $user) {
+	global $log, $adb, $root_directory;
 
-    global $log, $adb, $root_directory;
-	require_once 'include/Webservices/getRecordImages.php';
-	
+	$idList = vtws_getIdComponents($recordID);
+	$webserviceObject = VtigerWebserviceObject::fromId($adb, $recordID);
+	$handlerPath = $webserviceObject->getHandlerPath();
+	$handlerClass = $webserviceObject->getHandlerClass();
 
+	require_once $handlerPath;
 
-    // How do we separate the product id from 4x 41838 ?
-    // Separates into $wsid (module) and $crmid (crmID)
-    list($wsid, $crmid) = explode('x', $recordID);
+	$handler = new $handlerClass($webserviceObject, $user, $adb, $log);
+	$meta = $handler->getMeta();
+	$entityName = $meta->getObjectEntityName($recordID);
+	$types = vtws_listtypes(null, $user);
+	if ($entityName != 'Products' || !in_array($entityName, $types['types'])) {
+		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to perform the operation is denied");
+	}
 
-    // Check for maximum of 6 images? Not sure how best to do this
-    // use function cbws_getrecordimageinfo($id, $user)
-    
-	// this breaks....
-	 //$check = cbws_getrecordimageinfo($recordID, $user); 
-	
-	$log->debug("Entering into add uploadProductImages($wsid,$crmid) method.");
-	
-    // Another is in Utils.php
-    // Separates into $idList['0'] (module) and $idList['1'] (crmID)
-    $idList = vtws_getIdComponents($recordID); // does the same as above
-    //    $idList = vtws_getIdComponents($element['id']);
-    //    $webserviceObject = VtigerWebserviceObject::fromId($adb, $idList[0]);
-    $webserviceObject = VtigerWebserviceObject::fromId($adb, $idList[0]);
-    $handlerPath = $webserviceObject->getHandlerPath();
-    $handlerClass = $webserviceObject->getHandlerClass();
+	if ($entityName !== $webserviceObject->getEntityName()) {
+		throw new WebServiceException(WebServiceErrorCode::$INVALIDID, "Id specified is incorrect");
+	}
 
-    require_once $handlerPath;
+	if (!$meta->hasPermission(EntityMeta::$UPDATE, $recordID)) {
+		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to read given object is denied");
+	}
 
-    $handler = new $handlerClass($webserviceObject, $user, $adb, $log);
-    $meta = $handler->getMeta();
-    $entityName = $meta->getObjectEntityName($idList[0]);
-    $product = CRMEntity::getInstance('Products');
-    $product->retrieve_entity_info($idList[1], 'Products');
+	if (!$meta->exists($idList[1])) {
+		throw new WebServiceException(WebServiceErrorCode::$RECORDNOTFOUND, "Record you are trying to access is not found");
+	}
 
-    $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    foreach ($fileData as $fieldname => $imageDetail) {
-        $_FILES = array();
-        //$imagename = $imageDetail['filename'];
-        //$image = $imageDetail['filedata'];
-        $filepath = $root_directory . 'storage/' . $imageDetail['filename'];
-        file_put_contents($filepath, base64_decode($imageDetail['filedata']));
+	if ($meta->hasWriteAccess() !== true) {
+		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to write is denied");
+	}
 
-        //s$imagename = basename($image);
-        $_FILES[$fieldname] = array(
-            'name' => $imageDetail['filename'],
-            'type' => finfo_file($finfo, $filepath),
-            'tmp_name' => $filepath,
-            'error' => 0,
-            'size' => filesize($filepath),
-        );
-        $product->insertIntoAttachment($crmid, 'Products', true);
-		
-		// Somehow we need to catch the result here to return it.
-		$result = "something or other";
-        }
+	$crmid = $idList[1];
+	// Check for maximum number of images
+	$maximages = GlobalVariable::getVariable('Product_Maximum_Number_Images', 6, 'Products');
+	$rsimg = $adb->pquery('select count(*) as cnt
+		from vtiger_attachments
+		inner join vtiger_crmentity on vtiger_crmentity.crmid = vtiger_attachments.attachmentsid
+		inner join vtiger_seattachmentsrel on vtiger_attachments.attachmentsid=vtiger_seattachmentsrel.attachmentsid
+		where deleted=0 and vtiger_crmentity.setype LIKE "Products Image" and vtiger_seattachmentsrel.crmid=?', array($crmid));
+	$numimages = $adb->query_result($rsimg, 0, 'cnt');
+	if ($numimages >= $maximages) {
+		throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Maximum number of images has been reached");
+	}
 
-	// here on down is guesswork
-	
-	// I think here down is error checking but not sure why it is after the event ?
-	// Should the insert come after this ?
-	
-    $types = vtws_listtypes(null, $user);
-    if (!in_array($entityName, $types['types'])) {
-        throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to perform the operation is denied");
-    }
+	$product = CRMEntity::getInstance('Products');
+	$product->retrieve_entity_info($idList[1], 'Products');
 
-    if ($entityName !== $webserviceObject->getEntityName()) {
-        throw new WebServiceException(WebServiceErrorCode::$INVALIDID, "Id specified is incorrect");
-    }
+	$log->debug("Entering into add uploadProductImages($recordID) method.");
 
-    if (!$meta->hasPermission(EntityMeta::$UPDATE, $element['id'])) {
-        throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to read given object is denied");
-    }
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$newnumimages = $numimages;
+	foreach ($fileData as $imageDetail) {
+		$_FILES = array();
+		$filepath = $root_directory . 'cache/' . basename($imageDetail['name']);
+		file_put_contents($filepath, base64_decode($imageDetail['content']));
 
-    if (!$meta->exists($idList[1])) {
-        throw new WebServiceException(WebServiceErrorCode::$RECORDNOTFOUND, "Record you are trying to access is not found");
-    }
+		$_FILES['imagename'] = array(
+			'name' => $imageDetail['name'],
+			'type' => finfo_file($finfo, $filepath),
+			'tmp_name' => $filepath,
+			'error' => 0,
+			'size' => filesize($filepath),
+		);
+		$product->insertIntoAttachment($crmid, 'Products', true);
+		unlink($filepath);
+		$newnumimages++;
+		if ($newnumimages >= $maximages) {
+			break;
+		}
+	}
+	$rsimg = $adb->pquery('select count(*) as cnt
+		from vtiger_attachments
+		inner join vtiger_crmentity on vtiger_crmentity.crmid = vtiger_attachments.attachmentsid
+		inner join vtiger_seattachmentsrel on vtiger_attachments.attachmentsid=vtiger_seattachmentsrel.attachmentsid
+		where deleted=0 and vtiger_crmentity.setype LIKE "Products Image" and vtiger_seattachmentsrel.crmid=?', array($crmid));
+	$finalnumimages = $adb->query_result($rsimg, 0, 'cnt');
 
-    if ($meta->hasWriteAccess() !== true) {
-        throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to write is denied");
-    }
-
-    $referenceFields = $meta->getReferenceFieldDetails();
-    foreach ($referenceFields as $fieldName => $details) {
-        if (isset($element[$fieldName]) && strlen($element[$fieldName]) > 0) {
-            $ids = vtws_getIdComponents($element[$fieldName]);
-            $elemTypeId = $ids[0];
-            $elemId = $ids[1];
-            $referenceObject = VtigerWebserviceObject::fromId($adb, $elemTypeId);
-            if (!in_array($referenceObject->getEntityName(), $details)) {
-                throw new WebServiceException(WebServiceErrorCode::$REFERENCEINVALID, "Invalid reference specified for $fieldName");
-            }
-            if ($referenceObject->getEntityName() == 'Users') {
-                if (!$meta->hasAssignPrivilege($element[$fieldName])) {
-                    throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Cannot assign record to the given user");
-                }
-            }
-            if (!in_array($referenceObject->getEntityName(), $types['types']) && $referenceObject->getEntityName() != 'Users') {
-                throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Permission to access reference type is denied " . $referenceObject->getEntityName());
-            }
-        } else if (isset($element[$fieldName]) and $element[$fieldName] !== NULL) {
-            unset($element[$fieldName]);
-        }
-    }
-
-    $meta->hasMandatoryFields($element);
-
-    $ownerFields = $meta->getOwnerFields();
-    if (is_array($ownerFields) && sizeof($ownerFields) > 0) {
-        foreach ($ownerFields as $ownerField) {
-            if (isset($element[$ownerField]) && $element[$ownerField] !== null && !$meta->hasAssignPrivilege($element[$ownerField])) {
-                throw new WebServiceException(WebServiceErrorCode::$ACCESSDENIED, "Cannot assign record to the given user");
-            }
-        }
-    }
-
-    VTWS_PreserveGlobal::flush();
-    if (!empty($_FILES)) {
-        foreach ($_FILES as $field => $file) {
-            unlink($file['tmp_name']);
-        }
-    }
-
-    // return a result ?
-    return $entity;
+	return ($finalnumimages == ($numimages + count($fileData)));
 }
 
 ?>
