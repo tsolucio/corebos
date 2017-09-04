3e130b729e8dab43559cc6afb5a4683da52b2c57
Joe Bordes <joe@tsolucio.com>
2017-09-04 13:23:39 +0200

    fix(QuickCreate/Popup) filter the newly created field on the entityfield of the module, not on uitype4 which may not be present

--- Popup.php
+++ Popup.php
@@ -62,7 +62,7 @@ $smarty->assign("POPUP", str_replace('&','-a;',$suri).'-a;popqc=true');
 if (!empty($_REQUEST['popqc']) and $_REQUEST['popqc'] = 'true' and empty($_REQUEST['advft_criteria']) and !empty($_REQUEST['record'])) {
 	$fldrs = $adb->pquery('SELECT vtiger_field.fieldlabel,vtiger_field.tablename,vtiger_field.columnname,vtiger_field.fieldname,vtiger_entityname.entityidfield
 			FROM vtiger_field
-			INNER JOIN vtiger_entityname on vtiger_field.tabid=vtiger_entityname.tabid and modulename=? WHERE uitype=4',array($currentModule));
+			INNER JOIN vtiger_entityname on vtiger_field.tabid=vtiger_entityname.tabid and modulename=? WHERE vtiger_field.fieldname=vtiger_entityname.fieldname',array($currentModule));
 	$row = $adb->fetch_array($fldrs);
 	$fieldLabelEscaped = str_replace(" ","_",$row['fieldlabel']);
 	$optionvalue = $row['tablename'].":".$row['columnname'].":".$row['fieldname'].":".$currentModule."_".$fieldLabelEscaped.":V";
