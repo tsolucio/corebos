580f95151b0b420ef7ce8f0475fb82b734be4bdc
endritkane <endritkane95@gmail.com>
2017-09-19 10:07:47 +0200

    minor fix from merge conflict

--- modules/Campaigns/Campaigns.php
+++ modules/Campaigns/Campaigns.php
@@ -160,7 +160,7 @@ class Campaigns extends CRMEntity {
 		if ($actions) {
 			if (is_string($actions)) {
 				$actions = explode(',', strtoupper($actions));
-
+			}
 			if(in_array('SELECT', $actions) && isPermitted($related_module,4, '') == 'yes')
 			{
 				$button .= "<input title='".getTranslatedString('LBL_SELECT')." ". getTranslatedString($related_module). "' class='slds-button slds-button--small slds-button--brand' type='button' onclick=\"return window.open('index.php?module=$related_module&return_module=$currentModule&action=Popup&popuptype=detailview&select=enable&form=EditView&form_submit=false&recordid=$id&parenttab=$parenttab','test','width=640,height=602,resizable=0,scrollbars=0');\" value='". getTranslatedString('LBL_SELECT'). " " . getTranslatedString($related_module) ."'>";
