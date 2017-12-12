624c5ad012ed5183991890eda11e3ee88a888ef7
Joe Bordes <joe@tsolucio.com>
2017-12-12 01:50:12 +0100

    i18n(pt_br) missing files. Thanks Ranieri

---
+++ modules/CronTasks/language/pt_br.lang.php
@@ -0,0 +1,42 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Contributor(s): Ranieri/Translate terms eCRM web | 11/03/2017 |Sao Paulo/SP/Brasil.|www.ecrmweb.com.br
+ ************************************************************************************/
+$mod_strings = array (
+	'LBL_ACTIVE' => 'Ativo',
+	'LBL_INACTIVE' => 'Inativo',
+	'LBL_STATUS' => 'Status',
+	'LBL_SCHEDULER' => 'Scheduler',
+	'LBL_FREQUENCY'=> 'Frequencia',
+	'LBL_HOURMIN' => '(H:M)',
+	'LAST_START'=>'Último Scan Começou às',
+	'LAST_END'=>'Último Scan Terminou às',
+	'LBL_SEQUENCE'=>'Sequencia',
+	'LBL_TOOLS' =>'Ferramentas',
+	'LBL_RUNNING'=>'Rodando',
+	'LBL_MINIMUM_FREQUENCY'=>'A frequencia de qualquer tarefa agendada (cron) deve ser maior do que',
+	'LBL_SECONDS'=>'Segundos',
+	'LBL_SECONDS_AGO'=>'seg(s) atrás',
+	'LBL_MINUTES'=>'Minutos',
+	'LBL_MINUTES_AGO'=>'min(s) atrás',
+	'LBL_HOURS'=>'Horas',
+	'LBL_DAILY'=>'Dias',
+	'LBL_HOURS_AGO'=>'hr(s) atrás',
+	'LBL_DAYS'=>'Dias',
+	'LBL_DAYS_AGO'=>'dia(s) atrás',
+	'LBL_MONTHS'=>'Meses',
+	'LBL_MONTHS_AGO'=>'mes(es) atrás',
+	'LBL_YEARS'=>'Anos',
+	'LBL_YEARS_AGO'=>'ano(s) atrás',
+	'Allows you to Configure Cron Task' => 'Configurar tarefas Agendadas (Cron) ',
+	'Scheduler' => 'Scheduler',
+	'Cron Job' => 'Tarefa Agendada',
+	'cronWatcherService' => 'Serviço Monitoria do Cron',
+);
+?>
