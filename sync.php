921b8c77868ac34406b794368fec8b3680d223e6
Lorida Cito <loridacito@gmail.com>
2017-12-12 14:00:39 +0100

    fix(Calendar4You) add corebos session for tokens and fix clear tokens problem

--- sync.php
+++ sync.php
@@ -42,7 +42,7 @@ $client->setScopes(array("https://www.googleapis.com/auth/calendar","https://www
 if (isset($_GET['code'])) {
   $q=$client->authenticate($_GET['code']);
   coreBOS_Session::set('token', $client->getAccessToken());
-  $token=json_decode($_SESSION['token']);
+  $token=json_decode(coreBOS_Session::get('token'));
   if($token->refresh_token!='' && $token->refresh_token!=null)
   $adb->pquery("update its4you_googlesync4you_access set refresh_token=? where userid=?",array($token->refresh_token,$_SESSION['authenticated_user_id']));
   header("Location: $site_URL/index.php?module=Calendar4You&action=index");
