d228591069fdfc680210f285d5f0518fd0905034
Joe Bordes <joe@tsolucio.com>
2018-01-13 01:50:43 +0100

    cbSR(Reports)

--- modules/Reports/CreateCSV.php
+++ modules/Reports/CreateCSV.php
@@ -19,39 +19,38 @@
 global $php_max_execution_time;
 set_time_limit($php_max_execution_time);
 
-require_once("modules/Reports/ReportRun.php");
-require_once("modules/Reports/Reports.php");
+require_once('modules/Reports/ReportRun.php');
+require_once('modules/Reports/Reports.php');
 
 global $tmp_dir, $root_directory;
-$fname = tempnam($root_directory.$tmp_dir, "merge2.csv");
+$fname = tempnam($root_directory.$tmp_dir, 'merge2.csv');
 
 # Write out the data
-$reportid = vtlib_purify($_REQUEST["record"]);
+$reportid = vtlib_purify($_REQUEST['record']);
 $oReportRun = new ReportRun($reportid);
 if (empty($_REQUEST['advft_criteria'])) {
 	$advft_criteria = '';
 } else {
 	$advft_criteria = $_REQUEST['advft_criteria'];
-	$advft_criteria = json_decode($advft_criteria,true);
+	$advft_criteria = json_decode($advft_criteria, true);
 }
 if (empty($_REQUEST['advft_criteria_groups'])) {
 	$advft_criteria_groups = '';
 } else {
 	$advft_criteria_groups = $_REQUEST['advft_criteria_groups'];
-	$advft_criteria_groups = json_decode($advft_criteria_groups,true);
+	$advft_criteria_groups = json_decode($advft_criteria_groups, true);
 }
-$filterlist = $oReportRun->RunTimeAdvFilter($advft_criteria,$advft_criteria_groups);
+$filterlist = $oReportRun->RunTimeAdvFilter($advft_criteria, $advft_criteria_groups);
 $oReportRun->writeReportToCSVFile($fname, $filterlist);
 
-if(isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'],'MSIE'))
-{
-	header("Pragma: public");
-	header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+if (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
+	header('Pragma: public');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 }
-header("Content-Type: application/csv;charset=utf-8");
-header("Content-Length: ".@filesize($fname));
-header('Content-disposition: attachment; filename="Reports.csv"');
-$fh=fopen($fname, "rb");
+header('Content-Type: application/csv;charset=utf-8');
+header('Content-Length: '.@filesize($fname));
+header("Content-disposition: attachment; filename='Reports.csv'");
+$fh=fopen($fname, 'rb');
 fpassthru($fh);
 exit();
 ?>
