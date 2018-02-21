f28b8dee8f25bd19a227115ad5da821337946b4e
Joe Bordes <joe@tsolucio.com>
2018-02-21 12:11:30 +0100

    cbSR(ProjectTask)

--- modules/ProjectTask/language/es_es.lang.php
+++ modules/ProjectTask/language/es_es.lang.php
@@ -6,7 +6,6 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- * 
  * ***********************************************************
 *  Module       : ProjectTask
 *  Language     : ES Spanish
@@ -15,43 +14,41 @@
 *  Author       : JPL TSolucio, S.L.
 *  License      : MPL 1.1
  ************************************************************************************/
-$mod_strings = Array(
-
+$mod_strings = array(
 	'LBL_MODULE_NAME'=>'Actividades',
-  	'LBL_MODULE_TITLE'=>'Actividades',
+	'LBL_MODULE_TITLE'=>'Actividades',
 	'LBL_NEW_PROJECTTASK'=>'Nueva Actividad',
-    'LBL_PROJECTTASKS'=>'Actividades',
-    'LBL_PROJECTTASK'=>'Actividad',
-    'Project Task ID'=>'ID Actividad',
-    'projectasks' => 'actividades',
-    'ProjectTask'=>'Actividades',
-    'SINGLE_ProjectTask' => 'Actividad',
+	'LBL_PROJECTTASKS'=>'Actividades',
+	'LBL_PROJECTTASK'=>'Actividad',
+	'Project Task ID'=>'ID Actividad',
+	'projectasks' => 'actividades',
+	'ProjectTask'=>'Actividades',
+	'SINGLE_ProjectTask' => 'Actividad',
 	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
-    'LBL_PROJECT_TASK_INFORMATION' => 'Información Actividad',
-    'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
-    'Assigned To' => 'Asignado a',
-    'Priority' => 'Prioridad',
-    'Progress' => 'Progreso',
+	'LBL_PROJECT_TASK_INFORMATION' => 'Información Actividad',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+	'Assigned To' => 'Asignado a',
+	'Priority' => 'Prioridad',
+	'Progress' => 'Progreso',
 	'Open' => 'Abierta',
 	'Cancelled'=>'Cancelada',
-    'Type'=>'Tipo',
-    'Worked Hours'=>'Horas Trabajadas',
-    'Start Date'=>'Fecha inicio',
-    'End Date'=>'Fecha fin',
-    'Related to' => 'Proyecto',
-   	'administrative' => 'administrativo',
-    'operative' => 'operativo',
-    'other' => 'otro',
-    'low' => 'baja',
-    'normal' => 'normal',
-    'high' => 'alta',
-    //added for related lists
-    'Project Task Name' => 'Nombre Actividad',
-    'Project Task No' => 'Nº Actividad',
-    'Project Task Number' => 'Código Actividad',
+	'Type'=>'Tipo',
+	'Worked Hours'=>'Horas Trabajadas',
+	'Start Date'=>'Fecha inicio',
+	'End Date'=>'Fecha fin',
+	'Related to' => 'Proyecto',
+	'administrative' => 'administrativo',
+	'operative' => 'operativo',
+	'other' => 'otro',
+	'low' => 'baja',
+	'normal' => 'normal',
+	'high' => 'alta',
+	//added for related lists
+	'Project Task Name' => 'Nombre Actividad',
+	'Project Task No' => 'Nº Actividad',
+	'Project Task Number' => 'Código Actividad',
 	'ProjectTask ID'=>'Id Actividad',
 	'Add Note' => 'Añadir Documento',
 	'--none--' => '--ninguno--',
 );
-
 ?>
