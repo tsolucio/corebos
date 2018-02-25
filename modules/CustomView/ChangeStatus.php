3ad257c6d8e84f6e39e734bc0453289c47399609
Joe Bordes <joe@tsolucio.com>
2018-02-25 22:02:32 +0100

    feat(CustomView) audit approval/revoke view

--- modules/CustomView/ChangeStatus.php
+++ modules/CustomView/ChangeStatus.php
@@ -10,7 +10,7 @@
 require_once 'include/logging.php';
 require_once 'include/database/PearDatabase.php';
 require_once 'modules/CustomView/CustomView.php';
-global $adb, $log;
+global $adb, $log, $current_user;
 
 $cvid = vtlib_purify($_REQUEST['record']);
 $status = vtlib_purify($_REQUEST['status']);
@@ -19,12 +19,25 @@ $now_action = vtlib_purify($_REQUEST['action']);
 if (isset($cvid) && $cvid != '') {
 	$oCustomView = new CustomView($module);
 	if ($oCustomView->isPermittedCustomView($cvid, $now_action, $oCustomView->customviewmodule) == 'yes') {
+		if ($status==2) {
+			$action = 'Revoke View (';
+		} else {
+			$action = 'Approve View (';
+		}
 		$updateStatusSql = 'update vtiger_customview set status=? where cvid=? and entitytype=?';
 		$updateresult = $adb->pquery($updateStatusSql, array($status, $cvid, $module));
 		if (!$updateresult) {
+			$action .= 'NOK)';
 			echo ':#:FAILURE:#:';
 		} else {
+			$action .= 'OK)';
 			echo ':#:SUCCESS:#:';
+		}
+		include 'user_privileges/audit_trail.php';
+		if ($audit_trail == 'true') {
+			$query = 'insert into vtiger_audit_trial values(?,?,?,?,?,?)';
+			$qparams = array($adb->getUniqueID('vtiger_audit_trial'), $current_user->id, 'CustomView', $action, $cvid, date('Y-m-d H:i:s'));
+			$adb->pquery($query, $qparams);
 		}
 	} else {
 		global $app_strings;
