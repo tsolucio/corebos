51980751856732917bf3fa49816bcc41d69981fa
Denald <denalddh@gmail.com>
2018-02-28 10:41:42 +0100

    feat(Reports) opening external url to new tab

--- modules/Reports/Save.php
+++ modules/Reports/Save.php
@@ -315,16 +315,14 @@ if ($reportid == '' || ($reportid!='' && isset($_REQUEST['saveashidden']) && $_R
 			echo $errormessage;
 			die;
 		}
-		$var = unserialize($minfo);
 
-		$var_one = $var[url];
-
-		echo ' 
-			<script>
-				window.opener.location.href = "'.$var_one.'";
-				window.open("index.php?module=Reports&action=ListView", "_blank");
-				self.close();
-		 	</script>';
+	$var = unserialize($minfo);
+	$var_one = $var[url];
+	echo '<script>
+			window.opener.location.href = "'.$var_one.'";
+			window.open("index.php?module=Reports&action=ListView", "_blank");
+			self.close();
+		 </script>';
 	}
 } else {
 	if ($reportid != "") {
