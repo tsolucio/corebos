b11329edae7e5c7e295b1c01c76bf97c02dc8d5e
Joe Bordes <joe@tsolucio.com>
2018-02-28 12:49:04 +0100

    chore(Install) eliminate unused code

--- install/CreateTables.php
+++ install/CreateTables.php
@@ -39,7 +39,6 @@ if (isset($_SESSION['installation_info']['currency_code'])) $currency_code = $_S
 if (isset($_SESSION['installation_info']['currency_symbol'])) $currency_symbol = $_SESSION['installation_info']['currency_symbol'];
 if (isset($_SESSION['installation_info']['selected_optional_modules'])) $selected_optional_modules = $_SESSION['installation_info']['selected_optional_modules'];
 
-//require_once('install/CreateTables.inc.php');
 include 'install/InitSchema.php';
 global $adb;
 $adb = PearDatabase::getInstance();
