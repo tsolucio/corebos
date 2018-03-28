942c5db10065d31726eb0b2cd80d144cae6d6953
Joe Bordes <joe@tsolucio.com>
2018-03-28 22:57:32 +0200

    cbSR(InvPDF/Docs) eliminate warning

--- modules/Documents/ListViewCalculations.php
+++ modules/Documents/ListViewCalculations.php
@@ -7,42 +7,44 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-$dbQuery = "select * from vtiger_attachmentsfolder";
-$result = $adb->pquery($dbQuery,array());
+$result = $adb->pquery('select * from vtiger_attachmentsfolder', array());
 $foldercount = $adb->num_rows($result);
-$folders = Array();
-$emptyfolders = Array();
-if($foldercount > 0 ) {
-	for($i=0;$i<$foldercount;$i++) 	{
+$folders = array();
+$emptyfolders = array();
+if ($foldercount > 0) {
+	for ($i=0; $i<$foldercount; $i++) {
 		$query = '';
 		$displayFolder='';
-		$query = substr($list_query, 0, stripos($list_query,' WHERE '));
-		$folder_id = $adb->query_result($result,$i,"folderid");
+		$query = substr($list_query, 0, stripos($list_query, ' WHERE '));
+		$folder_id = $adb->query_result($result, $i, 'folderid');
 		$query .= " where vtiger_crmentity.deleted=0 and vtiger_notes.folderid = $folder_id";
 		//Retreiving the no of rows
 		$count_result = $adb->query(mkCountQuery($query));
-		$num_records = $adb->query_result($count_result,0,"count");
-		if($num_records > 0){
+		$num_records = $adb->query_result($count_result, 0, 'count');
+		if ($num_records > 0) {
 			$displayFolder=true;
 		}
-		$folder_details=Array();
-		$folderid = $adb->query_result($result,$i,"folderid");
+		$folder_details=array();
+		$folderid = $adb->query_result($result, $i, 'folderid');
 		$folder_details['folderid']=$folderid;
-		$folder_details['foldername']=$adb->query_result($result,$i,"foldername");
+		$folder_details['foldername']=$adb->query_result($result, $i, 'foldername');
 		$foldername = $folder_details['foldername'];
-		$folder_details['description']=$adb->query_result($result,$i,"description");
+		$folder_details['description']=$adb->query_result($result, $i, 'description');
 		if ($displayFolder == true) {
 			$folders[$foldername] = $folder_details;
-		} else{
+		} else {
 			$emptyfolders[$foldername] = $folder_details;
 		}
-		if ($folderid == 1) $default_folder_details = $folder_details;
+		if ($folderid == 1) {
+			$default_folder_details = $folder_details;
+		}
 	}
-	if (count($folders) == 0) $folders[$default_folder_details['foldername']] = $default_folder_details;
+	if (count($folders) == 0) {
+		$folders[$default_folder_details['foldername']] = $default_folder_details;
+	}
 }
-
-$smarty->assign("NO_OF_FOLDERS",$foldercount);
-$smarty->assign("FOLDERS", $folders);
-$smarty->assign("EMPTY_FOLDERS", $emptyfolders);
-$smarty->assign("ALL_FOLDERS", array_merge($folders, $emptyfolders));
+$smarty->assign('NO_OF_FOLDERS', $foldercount);
+$smarty->assign('FOLDERS', $folders);
+$smarty->assign('EMPTY_FOLDERS', $emptyfolders);
+$smarty->assign('ALL_FOLDERS', array_merge($folders, $emptyfolders));
 ?>
