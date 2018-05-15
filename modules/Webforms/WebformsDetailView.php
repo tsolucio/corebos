fd119c9aa32dfc179b30fe7d194b1d4f18d3eb83
Joe Bordes <joe@tsolucio.com>
2018-05-15 19:08:11 +0200

    cbSR(Report/Webform) eliminate warning and format files

--- modules/Webforms/WebformsDetailView.php
+++ modules/Webforms/WebformsDetailView.php
@@ -9,16 +9,15 @@
  ********************************************************************************/
 global $app_strings, $mod_strings, $current_language, $currentModule, $theme,$current_user,$adb,$log;
 
-require_once('Smarty_setup.php');
-require_once('modules/Webforms/Webforms.php');
-require_once('modules/Webforms/model/WebformsModel.php');
-require_once('modules/Webforms/model/WebformsFieldModel.php');
-require_once('include/utils/CommonUtils.php');
+require_once 'Smarty_setup.php';
+require_once 'modules/Webforms/Webforms.php';
+require_once 'modules/Webforms/model/WebformsModel.php';
+require_once 'modules/Webforms/model/WebformsFieldModel.php';
+require_once 'include/utils/CommonUtils.php';
 
 Webforms::checkAdminAccess($current_user);
 
-if(isset($_REQUEST['id'])){
-
+if (isset($_REQUEST['id'])) {
 	$webformModel=Webforms_Model::retrieveWithId($_REQUEST['id']);
 	$webform=new Webforms();
 	$smarty = new vtigerCRM_Smarty();
@@ -29,24 +28,23 @@ if(isset($_REQUEST['id'])){
 		list($username) = getGroupName($webformModel->getOwnerId());
 	}
 
-	$smarty->assign('WEBFORMMODEL',$webformModel);
-	$smarty->assign('WEBFORM',$webform);
-	$smarty->assign('OWNER',$username);
+	$smarty->assign('WEBFORMMODEL', $webformModel);
+	$smarty->assign('WEBFORM', $webform);
+	$smarty->assign('OWNER', $username);
 	$smarty->assign('THEME', $theme);
 	$smarty->assign('MOD', $mod_strings);
 	$smarty->assign('APP', $app_strings);
 	$smarty->assign('MODULE', $currentModule);
 	$smarty->assign('SINGLE_MOD', 'SINGLE_'.$currentModule);
-	if(isset($tool_buttons)==false) {
+	if (isset($tool_buttons)==false) {
 		$tool_buttons = Button_Check($currentModule);
 	}
 	$smarty->assign('CHECK', $tool_buttons);
 	$smarty->assign('CATEGORY', $category);
 	$smarty->assign('IMAGE_PATH', "themes/$theme/images/");
 	$smarty->assign('WEBFORMFIELDS', Webforms::getFieldInfos($webformModel->getTargetModule()));
-	$smarty->assign('ACTIONPATH',$site_URL.'/modules/Webforms/capture.php');
-	$smarty->assign('LANGUAGE',$current_language);
-	$smarty->display(vtlib_getModuleTemplate($currentModule,'DetailView.tpl'));
+	$smarty->assign('ACTIONPATH', $site_URL.'/modules/Webforms/capture.php');
+	$smarty->assign('LANGUAGE', $current_language);
+	$smarty->display(vtlib_getModuleTemplate($currentModule, 'DetailView.tpl'));
 }
-
 ?>
