50e5fc73cc994e726f6c089912699079e43aadb0
Joe Bordes <joe@tsolucio.com>
2018-06-01 16:05:50 +0200

    i18n(Import) delete obsolete labels

--- modules/Import/language/it_it.lang.php
+++ modules/Import/language/it_it.lang.php
@@ -8,11 +8,7 @@
  * All Rights Reserved.
  *************************************************************************************/
 
-$mod_strings = array (
-  'LBL_IMPORT_MODULE_NO_DIRECTORY' => 'La cartella',
-  'LBL_IMPORT_MODULE_NO_DIRECTORY_END' => 'non esiste o non &egrave; scrivibile',
-  'LBL_IMPORT_MODULE_ERROR_NO_UPLOAD' => 'Il file non &egrave; stato caricato con successo, prova di nuovo',
-  'LBL_IMPORT_MODULE_ERROR_LARGE_FILE' => 'Il file &egrave; troppo grande. Massimo:',
+$mod_strings = array(
   'LBL_MODULE_NAME' => 'Importa',
   'LBL_TRY_AGAIN' => 'Prova di nuovo',
   'LBL_ERROR' => 'Errore:',
