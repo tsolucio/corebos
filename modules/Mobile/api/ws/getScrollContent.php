4b3272c44ae6ddffaeaa1abac179112344d35fc7
Omar Llorens <omar@tsolucio.com>
2018-06-18 11:10:27 +0200

    cbSR(Mobile)

--- modules/Mobile/api/ws/getScrollContent.php
+++ modules/Mobile/api/ws/getScrollContent.php
@@ -10,34 +10,32 @@
 include_once __DIR__ . '/models/SearchFilter.php';
 
 class crmtogo_WS_getScrollContent extends crmtogo_WS_Controller {
-	
+
 	function process(crmtogo_API_Request $request) {
 		return $this->getContent($request);
 	}
-	
+
 	function getContent(crmtogo_API_Request $request) {
 		global $currentModule;
 		$db = PearDatabase::getInstance();
-		$current_user = $this->getActiveUser();	
-		
+		$current_user = $this->getActiveUser();
+
 		$module = $request->get('module');
 		$limit = $request->get('number');
 		$offset = $request->get('offset');
 		$search =  trim($request->get('src_str'));
 		$customView = new CustomView($module);
-		
-		if(!empty($_REQUEST['view'])) {
+
+		if (!empty($_REQUEST['view'])) {
 			$viewid=$_REQUEST['view'];
-		}
-		else {
+		} else {
 			$viewid = $customView->getViewId($module);
 		}
-		
+
 		$queryGenerator = new QueryGenerator($module, $current_user);
 		if ($viewid != "0") {
 			$queryGenerator->initForCustomViewById($viewid);
-		} 				
-		else {
+		} else {
 			$queryGenerator->initForDefaultCustomView();
 		}
 		$currentModule = $module;
@@ -51,28 +49,24 @@ class crmtogo_WS_getScrollContent extends crmtogo_WS_Controller {
 		$ws_entity=$db->pquery($entity_sql, array($module));
 		$fieldname= $db->query_result($ws_entity,0,'fieldname');
 		$tablename= $db->query_result($ws_entity,0,'tablename');
-		
+
 		//set the list and content order
 		if ($module =='Contacts' || $module =='Leads') {
 			$list_query .= " AND (lastname LIKE '%$search%' OR firstname LIKE '%$search%') ORDER BY lastname";
-		}
-		elseif ($module != 'cbCalendar') {
+		} elseif ($module != 'cbCalendar') {
 			$list_query .= " AND ".$tablename.".".$fieldname." LIKE '%$search%' ORDER BY ".$tablename.".".$fieldname;
-		}
-		//special handling for calendar (currently display tasks only)
-		elseif ($module == 'cbCalendar') {
+		} elseif ($module == 'cbCalendar') { //special handling for calendar (currently display tasks only)
 			$list_query .= " AND vtiger_activity.activitytype!='Emails'";
 			$list_query .= " AND subject LIKE '%$search%' ORDER BY date_start DESC";
-		}
-		else  {
+		} else {
 			$list_query .= " AND ".$tablename.".".$fieldname." LIKE '%$search%' ORDER BY ".$tablename.".".$fieldname;
 		}
 		$list_query .= " LIMIT $offset, $limit;";
 		$listview_entries = $db->pquery($list_query ,array());
-		
+
 		$response = new crmtogo_API_Response();
 		$response->setResult(array('records'=>$listview_entries, 'module'=>$module));
-		
+
 		return $response;
 	}
 }
