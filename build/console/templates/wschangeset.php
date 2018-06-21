0d93628ad6ef79760c455b90826ccd572ad004bc
Albert Xhani <albert.xhani@outlook.com>
2018-06-21 11:42:25 +0200

    fix(Console) entitymethod:create added missing file for WS creation

---
+++ build/console/templates/wschangeset.php
@@ -0,0 +1,11 @@
+<?php
+$operationInfo = array(
+	 'name'    => 'WSNAME',
+	 'include' => 'include/Webservices/WSNAME.php',
+	 'handler' => 'WSNAME',
+	 'prelogin'=> 0,
+	 'type'    => 'METHODTYPE',
+	 'parameters' => array(
+		PARAMS
+	 )
+);
