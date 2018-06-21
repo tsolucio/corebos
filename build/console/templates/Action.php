956adb42dda4d58e3cc65998b1a6a63a929c16dd
Albert Xhani <albert.xhani@outlook.com>
2018-06-21 11:26:39 +0200

    feat(Console) action:create command to create a new corebos action

---
+++ build/console/templates/Action.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *	CoreBOS new action
+ */
+class DummyClass_Action {
+
+	/**
+	 * Action Constructor
+	 *
+	 * @param array $request
+	 * Request info sent by user
+	 */
+	public function __construct($request) {
+	}
+}
