fac28385b266fe0ac8181a6f522000677edf4b2f
besartmarku <besart.marku@gmail.com>
2018-10-04 15:56:39 +0200

    feat [Validate XML Maps against their XSD Schema]

--- modules/cbMap/cbMap.js
+++ modules/cbMap/cbMap.js
@@ -6,7 +6,60 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
+var sweetAlert = document.createElement('script');  
+sweetAlert.setAttribute('src','https://unpkg.com/sweetalert/dist/sweetalert.min.js');
+document.head.appendChild(sweetAlert);
+
+
 function showMapWindow(mapid) {
-	var url = 'index.php?module=cbMap&action=cbMapAjax&file=generateMap&mapid='+mapid;
-	window.open(url, 'Create Mapping', 'width=940,height=800,resizable=1,scrollbars=1');
+    var url = 'index.php?module=cbMap&action=cbMapAjax&file=generateMap&mapid='+mapid;
+    window.open(url, 'Create Mapping', 'width=940,height=800,resizable=1,scrollbars=1');
+}
+
+function validateMap(mapid) {
+    var url = "index.php?module=cbMap&action=cbMapAjax&file=validateMap";
+    var stringData = "mapid=" + mapid;
+    jQuery.ajax({
+        type: "POST",
+        url: url,
+        data: stringData,
+        cache: false,
+        async: false,
+        success: function(text) {
+            if (text=='VALIDATION_NOT_IMPLEMENTED_YET'){
+                swal("Notice!", "The validation for this map type hasn't been implemented yet.", "warning");
+            }
+
+            else if (text){
+                cleanText = text.replace(/<\/?[^>]+(>|$)/g, "");        
+                cleanText_final= cleanText.replace("Error: The document has no document element. on line -1", " ");
+                swal({
+                        title: "Oops!",
+                        text: "XML is NOT valid, please fix it",
+                        icon: "warning",
+                        dangerMode: true,
+                        buttons: {
+                            catch: {
+                                text: "Show Error",
+                                value: "catch",
+                            },
+                            Ok: {
+                                text: "Ok",
+                            },
+                        },
+                    })
+                    .then((value) => {
+                        switch (value) {
+                            case "catch":
+                                swal("Error Log", cleanText_final, "error");
+                                break;
+                        }
+                    });
+
+            } else {
+                swal("Good job!", "XML is valid", "success");
+            }
+        },
+
+    });
 }
---
+++ modules/cbMap/validateMap.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once('modules/cbMap/cbMap.php');
+require_once('data/CRMEntity.php');
+require_once('include/utils/utils.php');
+require_once('include/database/PearDatabase.php');
+require_once('include/utils/utils.php');
+
+$mapid = $_REQUEST["mapid"];
+$focus1 = CRMEntity::getInstance("cbMap");
+$focus1->retrieve_entity_info($mapid, "cbMap");
+$maptype = $focus1->column_fields["maptype"];
+$content = $focus1->column_fields["content"];
+
+function libxml_display_error($error)
+{
+    $return = "<br/>\n";
+    switch ($error->level) {
+        case LIBXML_ERR_WARNING:
+            $return .= "Warning: ";
+            break;
+        case LIBXML_ERR_ERROR:
+            $return .= "Error: ";
+            break;
+        case LIBXML_ERR_FATAL:
+            $return .= "Fatal Error: ";
+            break;
+    }
+    $return .= trim($error->message);
+    $return .= " on line <b>$error->line</b>\n";
+
+    return $return;
+}
+
+function libxml_display_errors()
+{
+    $errors = libxml_get_errors();
+    foreach ($errors as $error) {
+        print libxml_display_error($error);
+    }
+    libxml_clear_errors();
+}
+
+// Enable user error handling
+libxml_use_internal_errors(true);
+$content1 = htmlspecialchars_decode($content);
+$xml = new DOMDocument();
+$xml->loadXML($content1);
+
+if (!file_exists('modules/cbMap/XSD_schemas/' . $maptype . '.xsd')) {
+    echo "VALIDATION_NOT_IMPLEMENTED_YET";
+   
+} else {
+    if (!$xml->schemaValidate('modules/cbMap/XSD_schemas/' . $maptype . '.xsd')) {
+        libxml_display_errors();
+    }
+}
+
