dd5f468e21745981438bd4b7385fec250b65636f
Kliv Shapllo <k.shapllo@studiosynthesis.biz>
2018-10-04 14:29:28 +0200

    feat(BusinessAction) Get active links from new module

--- modules/BusinessActions/BusinessActions.php
+++ modules/BusinessActions/BusinessActions.php
@@ -9,6 +9,8 @@
  ************************************************************************************/
 require_once 'data/CRMEntity.php';
 require_once 'data/Tracker.php';
+include_once 'vtlib/Vtiger/Utils/StringTemplate.php';
+include_once 'vtlib/Vtiger/LinkData.php';
 
 class BusinessActions extends CRMEntity {
 	public $db;
@@ -21,6 +23,7 @@ class BusinessActions extends CRMEntity {
 	/** Indicator if this is a custom module or standard module */
 	public $IsCustomModule = true;
 	public $HasDirectImageField = false;
+
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
@@ -124,6 +127,26 @@ class BusinessActions extends CRMEntity {
 		}
 	}
 
+	private static function convertToObject($tabid, $valuemap) {
+
+		$link_obj = new Vtiger_Link();
+
+		$link_obj->tabid = $tabid;
+		$link_obj->linkid = $valuemap['businessactionsid'];
+		$link_obj->linktype       = $valuemap['elementtype_action'];
+		$link_obj->linklabel      = $valuemap['linklabel'];
+		$link_obj->linkurl        = decode_html($valuemap['linkurl']);
+		$link_obj->linkicon       = decode_html($valuemap['linkicon']);
+		$link_obj->sequence       = $valuemap['sequence'];
+		$link_obj->status         = (isset($valuemap['status']) ? $valuemap['status'] : false);
+		$link_obj->handler_path   = $valuemap['handler_path'];
+		$link_obj->handler_class  = $valuemap['handler_class'];
+		$link_obj->handler        = $valuemap['handler'];
+		$link_obj->onlyonmymodule = $valuemap['onlyonmymodule'];
+
+		return $link_obj;
+	}
+
 	/**
 	 * Invoked when special actions are performed on the module.
 	 * @param String Module name
@@ -144,6 +167,131 @@ class BusinessActions extends CRMEntity {
 		} elseif ($event_type == 'module.postupdate') {
 			// TODO Handle actions after this module is updated.
 		}
+	}
+
+	public static function getAllByType($tabid, $type = false, $parameters = false, $userid = null, $recordid = null) {
+		global $adb, $current_user;
+
+		$accumulator = array();
+		$module_name = getTabModuleName($tabid);
+		$multitype = false;
+
+		if ($userid == null) {
+			$userid = $current_user->id;
+		}
+
+		$type_sql = "";
+
+		if ($type) {
+			// Multiple link type selection
+			if (is_array($type)) {
+				$multitype = true;
+				$type_sql = $adb->convert2Sql(' AND elementtype_action IN ('.Vtiger_Utils::implodestr('?', count($type), ',') .') ', $adb->flatten_array($type));
+			} else {
+				$type_sql = $adb->convert2Sql(' AND elementtype_action = ?', array($type));
+			}
+		}
+
+		$query = 'SELECT businessactionsid, 
+                         elementtype_action,
+                         linklabel,
+                         linkurl,
+                         linkicon,
+                         sequence,
+                         handler_path,
+                         handler_class,
+                         handler,
+                         onlyonmymodule,
+                         brmap,
+                         mandatory
+                    FROM vtiger_businessactions INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = vtiger_businessactions.businessactionsid
+                   WHERE vtiger_crmentity.deleted = 0 
+                     AND active = 1 
+                     AND module_list LIKE "%'.$module_name.'%" '.$type_sql;
+
+		$orderby = ' ORDER BY elementtype_action, sequence';
+
+		$role_condition = "EXISTS(SELECT 1 
+                                    FROM vtiger_user2role 
+                                   WHERE vtiger_user2role.userid=? 
+                                     AND vtiger_businessactions.acrole LIKE CONCAT('%', vtiger_user2role.roleid, '%')
+                           )";
+		$role_condition = $adb->convert2Sql($role_condition, array($userid));
+
+		$user_condition = $adb->convert2sql('vtiger_crmentity.smownerid = ?', array($userid));
+
+		require_once 'include/utils/GetUserGroups.php';
+		$UserGroups = new GetUserGroups();
+		$UserGroups->getAllUserGroups($userid);
+
+		$group_condition = "";
+		if (count($UserGroups->user_groups)>0) {
+			$groups = implode(',', $UserGroups->user_groups);
+			$group_condition = 'OR vtiger_crmentity.smownerid IN ('.$groups.') ';
+		}
+
+		$where_ext = "AND (".$role_condition." OR ".$user_condition. " ".$group_condition.")";
+		$sql = $query.$where_ext.$orderby;
+
+		$business_actions = $adb->query($sql);
+
+		while ($row = $adb->fetch_array($business_actions)) {
+			$accumulator[] = $row;
+		}
+
+		$strtemplate = new Vtiger_StringTemplate();
+		if ($parameters) {
+			foreach ($parameters as $key => $value) {
+				$strtemplate->assign($key, $value);
+			}
+		}
+
+		$result = array();
+		if ($multitype) {
+			foreach ($type as $t) {
+				$result[$t] = array();
+			}
+		}
+
+		foreach ($accumulator as $row) {
+
+			/** Should the widget be shown */
+			$return = cbEventHandler::do_filter('corebos.filter.link.show', array($row, $type, $parameters));
+			if ($return == false) {
+				continue;
+			}
+
+			//Get Vtiger_Link object
+			$link = self::convertToObject($tabid, $row);
+
+			if (!empty($row['handler_path']) && isInsideApplication($row['handler_path'])) {
+				checkFileAccessForInclusion($row['handler_path']);
+				require_once $row['handler_path'];
+				$linkData = new Vtiger_LinkData($link, $current_user);
+				$ignore = call_user_func(array($row['handler_class'], $row['handler']), $linkData);
+				if (!$ignore) {
+					self::log("Ignoring Link ... ".var_export($row, true));
+					continue;
+				}
+			}
+
+			if (isset($recordid) && $row['brmap'] > 0 && $row['mandatory'] == 0 && !coreBOS_Rule::evaluate($row['brmap'], $recordid)) {
+				continue;
+			}
+
+			if ($parameters) {
+				$link->linkurl = $strtemplate->merge($link->linkurl);
+				$link->linkicon= $strtemplate->merge($link->linkicon);
+			}
+
+			if ($multitype) {
+				$result[$link->linktype][] = $link;
+			} else {
+				$result[$link->linktype] = $link;
+			}
+		}
+
+		return $result;
 	}
 
 	/**
