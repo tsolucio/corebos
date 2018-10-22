ae08d2ecdb68a947892a6034cb7769471fee42dc
Joe Bordes <joe@tsolucio.com>
2018-10-22 20:14:16 +0200

    fix(Validations) change deprecated PHP function

--- include/validation/validateCPFCNPJ.php
+++ include/validation/validateCPFCNPJ.php
@@ -21,7 +21,7 @@ function validaCPF($field, $cpf = null) {
 	}
 
 	// Elimina possivel mascara
-	$cpf = ereg_replace('[^0-9]', '', $cpf);
+	$cpf = preg_replace('/[^0-9]/', '', $cpf);
 	$cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
 
 	// Verifica se o numero de digitos informados é igual a 11
