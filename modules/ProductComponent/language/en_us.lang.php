002e5fe34e5495ce84c0921ee790b24f12526b5a
Jonida Sinaj <j.sinaj@evolutivo.it>
2018-11-19 11:35:22 +0100

    feat(Product Component)install of new module

---
+++ modules/ProductComponent/language/en_us.lang.php
@@ -0,0 +1,37 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'ProductComponent' => 'Product Component',
+	'SINGLE_ProductComponent' => 'Product Component',
+	'ProductComponent ID' => 'Product Component ID',
+	'frompdo' => 'From Product',
+	'topdo' => 'To Product',
+	'Relation Mode' => 'Relation Mode',
+	'Relation from' => 'Valid from',
+	'Relation Number' => 'Relation Number',
+	'Relation to' => 'Valid to',
+	'quantity' => 'Quantity',
+	'Quantity Rule' => 'Quantity Rule',
+	'instructions' => 'Instructions',
+
+	'Required' => 'Required',
+	'Marketing' => 'Marketing',
+	'Substitue' => 'Substitue',
+	'Complement' => 'Complement',
+	'Incompatible' => 'Incompatible',
+	'Obsolesence' => 'Obsolesence',
+
+	'LBL_PRODUCTCOMPONENT_INFORMATION' => 'Product Component Information',
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+);
+
+?>
---
+++ modules/ProductComponent/language/es_mx.lang.php
@@ -0,0 +1,37 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'ProductAssociation' => 'Relación Productos',
+	'SINGLE_ProductAssociation' => 'Relación Producto',
+	'ProductAssociation ID' => 'ID Relación Producto',
+	'frompdo' => 'Producto Principal',
+	'topdo' => 'Producto Asociado',
+	'Relation Mode' => 'Tipo Relación',
+	'Relation from' => 'Válido desde',
+	'Relation Number' => 'Nº Relación',
+	'Relation to' => 'Válido hasta',
+	'quantity' => 'Cantidad',
+	'Quantity Rule' => 'Regla Cantidad',
+	'instructions' => 'Instrucciones',
+
+	'Required' => 'Requerido',
+	'Marketing' => 'Marketing',
+	'Substitue' => 'Substituye',
+	'Complement' => 'Complementa',
+	'Incompatible' => 'Incompatible',
+	'Obsolesence' => 'Obsolescencia',
+
+	'LBL_PRODUCTASSOCIATION_INFORMATION' => 'Información Relación Producto',
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+);
+
+?>
