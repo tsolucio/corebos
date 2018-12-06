b8e0b42d7aba7f38688b74ebdd73ba9f36a0a276
glory moshi <glorysimon95@gmail.com>
2018-12-06 14:17:25 +0300

    feat(update in add block and field in cbQuestion)

--- build/changeSets/2018/addBlockandFieldincbQuestion.php
+++ build/changeSets/2018/addBlockandFieldincbQuestion.php
@@ -1,14 +1,20 @@
-* License. Roughly speaking, non-commercial users may share and modify this code, but must give credit
- * and share improvements. However, for proper details please read the full License, available at
- * http://vizsage.com/license/Vizsage-License-BY-NC-SA.html and the handy reference for understanding
- * the full license at http://vizsage.com/license/Vizsage-Deed-BY-NC-SA.html. Unless required by
- * applicable law or agreed to in writing, any software distributed under the License is distributed
- * on an  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and limitations under the
- * License terms of Creative Commons Attribution-NonCommercial-ShareAlike 3.0 (the License).
- *************************************************************************************************/
+<?php
+/*************************************************************************************************
+ * Copyright 2018 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+* Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+* file except in compliance with the License. You can redistribute it and/or modify it
+* under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+* granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+* the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+* applicable law or agreed to in writing, software distributed under the License is
+* distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+* either express or implied. See the License for the specific language governing
+* permissions and limitations under the License. You may obtain a copy of the License
+* at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+*************************************************************************************************/
 
-class addOrderInformation extends cbupdaterWorker {
+class addBlockandFieldincbQuestion extends cbupdaterWorker {
 
     public function applyChange() {
         global $adb;
@@ -32,7 +38,7 @@ class addOrderInformation extends cbupdaterWorker {
                             'label' => 'Order by Column',
                             'displaytype'=>'1',
                         ),
-                    )
+                    ),
                     'Grouping Information' => array(
                         'groupby' => array(
                             'columntype'=>'TEXT',
