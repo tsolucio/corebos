09b5a345d49923cc0d723abfa1dce3a4d7f93c3e
Mario Anthony <anthonyjeremiah1956@gmail.com>
2019-01-14 15:55:02 +0300

    Initial Commit

--- modules/com_vtiger_workflow/workflowlist.php
+++ modules/com_vtiger_workflow/workflowlist.php
@@ -13,6 +13,7 @@ require_once 'include/events/SqlResultIterator.inc';
 require_once 'VTWorkflowManager.inc';
 require_once 'VTWorkflowApplication.inc';
 require_once 'VTWorkflowUtils.php';
+require_once 'VTWorkflow.php';
 require_once 'vtlib/Vtiger/Cron.php';
 
 function vtDisplayWorkflowList($adb, $request, $requestUrl, $app_strings, $current_language) {
@@ -31,7 +32,10 @@ function vtDisplayWorkflowList($adb, $request, $requestUrl, $app_strings, $curre
 
 	$smarty = new vtigerCRM_Smarty();
 	$wfs = new VTWorkflowManager($adb);
+	$focus = new Workflow();
 	$smarty->assign('moduleNames', $util->vtGetModules($adb));
+	$smarty->assign('modulelist', $util->getModulesList($adb));
+	$smarty->assign('triggerlist', $util->getWorkflowExecutionConditionList());
 	$smarty->assign('returnUrl', $requestUrl);
 
 	$listModule = isset($request['list_module']) ? $request['list_module'] : '';
@@ -51,6 +55,8 @@ function vtDisplayWorkflowList($adb, $request, $requestUrl, $app_strings, $curre
 	$smarty->assign('PAGE_TITLE', $mod['LBL_AVAILABLE_WORKLIST_LIST']);
 	$smarty->assign('module', $module);
 	$smarty->assign('MODULE', $module->name);
+	$smarty->assign('LIST_HEADER', $focus->getWorkListHeader());
+	$smarty->assign('LIST_FIELDS', $focus->list_fields_name);
 	$smarty->assign('CRON_TASK', Vtiger_Cron::getInstance('Workflow'));
 	$smarty->display("{$module->name}/ListWorkflows.tpl");
 }
