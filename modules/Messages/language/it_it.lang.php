c85bf6d72055263a982556cf8c639df6dc1ce184
Lorida Cito <l.cito@studiosynthesis.biz>
2019-01-17 17:43:46 +0100

    feat(Messages) adding new module message

---
+++ modules/Messages/language/it_it.lang.php
@@ -0,0 +1,45 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Messages' => 'Messaggi',
+	'SINGLE_Messages' => 'Messaggi',
+	'Messages ID' => 'ID Messaggi',
+
+	'LBL_CUSTOM_INFORMATION' => 'Informazione Personalizzata',
+	'LBL_MESSAGES_INFORMATION' => 'Informazione Messaggio',
+	'LBL_DESCRIPTION_INFORMATION' => 'Informazione Descrizione',
+
+	'Messages No'=>'No Messaggio',
+        'Messages Name'=>'Nome Messaggio',
+        'Messages Type'=>'Tipo Messaggio',
+        'Related To'=>'Relazionato A',
+        'Assigned To'=>'Assegnato A',
+        'Created Time'=>'Orario Creazione',
+        'Modified Time'=>'Orario Modifica',
+        'Description'=>'Descrizione',
+        'lasturlclicked'=>'Ultimo URL Cliccato',
+        'no_mail'=>'No Mail',
+        'messagesuniqueid'=>'Messages UniqueId',
+        'Campaign'=>'Campaign',
+        'Account'=>'Account',
+        'Contact'=>'Contact',
+        'Lead'=>'Lead',
+        'Clicked'=>'Clicked',
+        'Dropped'=>'Dropped',
+        'Bounce'=>'Bounce',
+        'Open'=>'Open',
+        'Delivered'=>'Delivered',
+        'Unsubscribe'=>'Unsubscribe',
+        'Spam'=>'Spam',
+        'Status'=>'Status',         
+);
+
+?>
