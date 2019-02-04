c8a0e6ea7a237e1c60cc4c8a62bce7000855c0d8
Kliv Shapllo <k.shapllo@studiosynthesis.biz>
2019-02-04 16:37:22 +0100

    feat(BusinessAction) Business Action WS calls implemented with cbQuestion logic

---
+++ include/Webservices/executeBusinessAction.php
@@ -0,0 +1,8 @@
+<?php
+function executeBusinessAction($businessactionid) {
+	global $current_user;
+
+	$businessAction = (object) vtws_retrieve($businessactionid, $current_user);
+    return vtlib_process_widget($businessAction);
+
+}
