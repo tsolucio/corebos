1a8c5d04ac089817106b57b8f5437cec58f0e9a7
Joe Bordes <joe@tsolucio.com>
2019-02-10 13:44:32 +0100

    fix(cbCompany) avoid duplicating organization record on install

--- build/changeSets/2018/addModulecbCompany.php
+++ build/changeSets/2018/addModulecbCompany.php
@@ -39,32 +39,43 @@ class addModulecbCompany extends cbupdaterWorker {
 				require_once 'modules/Users/Users.php';
 				$query = $adb->query('SELECT * FROM vtiger_organizationdetails limit 1');
 				if ($query && $adb->num_rows($query) > 0) {
-					$userFocus = new Users();
-					$adminId = $userFocus->retrieve_user_id('admin');
-					$focus = new $module();
-					$focus->column_fields['assigned_user_id'] = $adminId;
-					$focus->column_fields['companyname'] = $adb->query_result($query, 0, 'organizationname');
-					$focus->column_fields['address'] = $adb->query_result($query, 0, 'address');
-					$focus->column_fields['city'] = $adb->query_result($query, 0, 'city');
-					$focus->column_fields['state'] = $adb->query_result($query, 0, 'state');
-					$focus->column_fields['country'] = $adb->query_result($query, 0, 'country');
-					$focus->column_fields['postalcode'] = $adb->query_result($query, 0, 'code');
-					$focus->column_fields['phone'] = $adb->query_result($query, 0, 'phone');
-					$focus->column_fields['fax'] = $adb->query_result($query, 0, 'fax');
-					$focus->column_fields['website'] = $adb->query_result($query, 0, 'website');
-					$focus->column_fields['defaultcompany'] = 1;
-					$imagesArray = array('companylogo'=>'logoname','applogo'=>'frontlogo','favicon'=>'faviconlogo');
-					foreach ($imagesArray as $index => $name) {
-						$filetyp =str_replace('.', '', strtolower(substr($adb->query_result($query, 0, $name), -4)));
-						$_FILES[$index] = array(
-							'name' => $adb->query_result($query, 0, $name),
-							'type' => 'image/'.$filetyp,
-							'tmp_name' => 'test/logo/'.$adb->query_result($query, 0, $name),
-							'error' => 0,
-							'size' => 1
-						);
+					$chk = $adb->pquery(
+						'select 1 from vtiger_cbcompany where companyname=? and address=? and phone=? and defaultcompany=?',
+						array(
+							$adb->query_result($query, 0, 'organizationname'),
+							$adb->query_result($query, 0, 'address'),
+							$adb->query_result($query, 0, 'phone'),
+							'1',
+						)
+					);
+					if ($chk && $adb->num_rows($chk) == 0) {
+						$userFocus = new Users();
+						$adminId = $userFocus->retrieve_user_id('admin');
+						$focus = new $module();
+						$focus->column_fields['assigned_user_id'] = $adminId;
+						$focus->column_fields['companyname'] = $adb->query_result($query, 0, 'organizationname');
+						$focus->column_fields['address'] = $adb->query_result($query, 0, 'address');
+						$focus->column_fields['city'] = $adb->query_result($query, 0, 'city');
+						$focus->column_fields['state'] = $adb->query_result($query, 0, 'state');
+						$focus->column_fields['country'] = $adb->query_result($query, 0, 'country');
+						$focus->column_fields['postalcode'] = $adb->query_result($query, 0, 'code');
+						$focus->column_fields['phone'] = $adb->query_result($query, 0, 'phone');
+						$focus->column_fields['fax'] = $adb->query_result($query, 0, 'fax');
+						$focus->column_fields['website'] = $adb->query_result($query, 0, 'website');
+						$focus->column_fields['defaultcompany'] = 1;
+						$imagesArray = array('companylogo'=>'logoname','applogo'=>'frontlogo','favicon'=>'faviconlogo');
+						foreach ($imagesArray as $index => $name) {
+							$filetyp =str_replace('.', '', strtolower(substr($adb->query_result($query, 0, $name), -4)));
+							$_FILES[$index] = array(
+								'name' => $adb->query_result($query, 0, $name),
+								'type' => 'image/'.$filetyp,
+								'tmp_name' => 'test/logo/'.$adb->query_result($query, 0, $name),
+								'error' => 0,
+								'size' => 1
+							);
+						}
+						$focus->save($module);
 					}
-					$focus->save($module);
 				}
 			}
 			$this->sendMsg('Changeset '.get_class($this).' applied!');
