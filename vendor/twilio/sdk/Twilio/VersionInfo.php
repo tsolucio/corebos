bd21db0a54c3e85ccc297277a0b364d14bc0efa7
Joe Bordes <joe@tsolucio.com>
2019-02-11 23:03:11 +0100

    feat(Twilio) library

---
+++ vendor/twilio/sdk/Twilio/VersionInfo.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Twilio;
+
+
+class VersionInfo {
+    const MAJOR = 5;
+    const MINOR = 28;
+    const PATCH = 1;
+
+    public static function string() {
+        return implode('.', array(self::MAJOR, self::MINOR, self::PATCH));
+    }
+}
