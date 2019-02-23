d23f07aae3f9ab93ab7af8d296300296f8975ec0
Joe Bordes <joe@tsolucio.com>
2019-02-23 18:33:29 +0100

    i18n(Payment) missing language files

---
+++ modules/CobroPago/language/en_gb.lang.php
@@ -0,0 +1,113 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+'CobroPago'=>'Payments',
+'CobroPago ID'=>'Payment ID',
+'SINGLE_CobroPago'=>'Payment',
+'LBL_MODULE_TITLE'=>'Payments: Home',
+'LBL_SEARCH_FORM_TITLE'=>'Payment Search',
+'LBL_LIST_FORM_TITLE'=>'Payment List',
+'LBL_COBROPAGO_NAME'=>'Payment Ref:',
+'LBL_COBROPAGO'=>'Payment:',
+'LBL_NAME'=>'Payment Ref',
+'LBL_INVITEE'=>'Contacts',
+'LBL_LIST_COBROPAGO_NAME'=>'Payment',
+'LBL_LIST_ACCOUNT_NAME'=>'Account Name',
+'LBL_PRODUCT_NAME'=>'Product Name',
+'LBL_LIST_AMOUNT'=>'Amount',
+
+'LBL_ACCOUNT_NAME'=>'Account Name:',
+'LBL_AMOUNT'=>'Amount:',
+'LBL_TYPE'=>'Type:',
+'LBL_PROBABILITY'=>'Probability (%):',
+'LBL_DESCRIPTION'=>'Description:',
+'LBL_COBROPAGO_INFORMATION'=>'Payment Information:',
+'LBL_DESCRIPTION_INFORMATION'=>'Description Information:',
+'LBL_CUSTOM_INFORMATION'=>'Custom Information',
+'SERVER_CONFIGURATION' => 'Payment Configuration',
+'SERVER_CONFIGURATION_DESCRIPTION' => 'Configure Payment Module Parameters',
+
+'LBL_NEW_FORM_TITLE'=>'New Payment',
+
+'ERR_DELETE_RECORD'=>'A record number must be specified to delete the payment.',
+'LBL_TOP_PAYMENTS'=>'My Top Open Payments',
+
+'NTC_NONE_SCHEDULED'=>'None scheduled.',
+
+'LBL_SELECT_COBROPAGO'=>'Select Payment',
+'LBL_GENERAL_INFORMATION'=>'General Information',
+
+'LBL_NEW_CobroPago'=>'New Payment',
+'LBL_CobroPago_TITLE'=>'Payments',
+
+'LBL_TOOL_FORM_TITLE'=>'Payment Tools',
+'CobroPago Name'=>'Payment Ref',
+'Reference'=>'Payment Ref',
+'Register'=>'Register Date',
+'Paid'=>'Paid',
+'Credit'=>'Credit',
+'Amount'=>'Amount',
+'Benefit'=>'Benefit',
+'Cost'=>'Cost',
+'Account Name'=>'Account Name',
+'Parent'=>'Account/Contact',
+'Currency'=>'Currency',
+'RelatedTo'=>'Related entity',
+'PaymentMode'=>'Payment Mode',
+'Category'=>'Payment Category',
+'CobroPago Type'=>'Payment Type',
+'Type'=>'Type',
+'Probability'=>'Probability (%)',
+'cobropagoid'=>'Payment Id',
+'DueDate'=>'Due Date',
+'Comercial'=>'Related user',
+'Recibo'=>'Receipt',
+'Historico de Cobros y Pagos'=>'Payment History',
+'Payment Paid'=>'Payment marked as PAID by ',
+'PaidOn'=>' on ',
+'RecieptOpen'=>'Receipt opened for printing by ',
+'CyP No'=>'Payment No',
+'PaymentDate'=>'Payment Date',
+
+//Added for Existing Picklist Strings
+
+'Cash'=>'Cash',
+'Check'=>'Check',
+'Credit card'=>'Credit card',
+'Infrastructure'=>'Infrastructure',
+'Stock'=>'Stock',
+'Sale'=>'Sale',
+
+'Other'=>'Other',
+
+'--None--'=>'--None--',
+'LBL_CHANGE'=>'Sales User',
+
+'LBL_CARD_INFORMATION' => 'Card Information',
+'CARD_ACCOUNT' => 'Card Number:',
+'CARD_CVV' => 'CVV2:',
+'CARD_EXPIRY_DATE' => 'Expiry Date (MMYY):',
+'CARD_SUBMIT' => 'Send',
+'ACH_ACCOUNT_TYPE' => 'Account Type:',
+'ACH_ACCOUNT_NUMBER' => 'Account Number:',
+'ACH_ROUTING_NUMBER' => 'Routing Number:',
+'ACH_SUBMIT' => 'Send',
+'ACH_ACCOUNT_TYPE_C' => 'Checking',
+'ACH_ACCOUNT_TYPE_S' => 'Savings',
+
+'Receive BluePay Payment'=>'Receive BluePay Payment',
+
+'ERR_ALREADY_PAID' => "This payment has been already paid.",
+'ERR_NO_ACCOUNT' => "There's no related account.",
+'ERR_NO_TOKEN' => "No token for this account.",
+);
+
+?>
