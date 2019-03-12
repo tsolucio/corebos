bb720bb4ef89d53d68f7c530087a074090fe6009
Joe Bordes <joe@tsolucio.com>
2019-03-12 10:12:45 +0100

    feat(Users) add full image information with same structure that we use in web service

--- modules/Users/Users.php
+++ modules/Users/Users.php
@@ -696,6 +696,7 @@ class Users extends CRMEntity {
 	 * @returns user info in $this->column_fields array:: Type array
 	 */
 	public function retrieveCurrentUserInfoFromFile($userid) {
+		global $adb, $site_URL;
 		checkFileAccessForInclusion('user_privileges/user_privileges_' . $userid . '.php');
 		require 'user_privileges/user_privileges_' . $userid . '.php';
 		foreach ($this->column_fields as $field => $value_iter) {
@@ -705,6 +706,28 @@ class Users extends CRMEntity {
 			}
 		}
 		$this->id = $userid;
+		$imageurl = '';
+		$image_name = $this->column_fields['imagename'];
+		if ($image_name != '') {
+			$sql = "select vtiger_attachments.*
+			from vtiger_attachments
+			inner join vtiger_crmentity on vtiger_crmentity.crmid = vtiger_attachments.attachmentsid
+			where vtiger_crmentity.setype='Users Attachment' and vtiger_attachments.name = ?";
+			$image_res = $adb->pquery($sql, array(str_replace(' ', '_', decode_html($image_name))));
+			if ($adb->num_rows($image_res)>0) {
+				$image_id = $adb->query_result($image_res, 0, 'attachmentsid');
+				$image_path = $adb->query_result($image_res, 0, 'path');
+				$image_name = decode_html($adb->query_result($image_res, 0, 'name'));
+				$imageurl = array(
+					'name' => $image_name,
+					'path' => $image_path . $image_id . '_' . urlencode($image_name),
+					'fullpath' => $site_URL.'/'.$image_path . $image_id . '_' . urlencode($image_name),
+					'type' => $adb->query_result($image_res, 0, 'type'),
+					'id' => $image_id,
+				);
+			}
+		}
+		$this->column_fields['imagenameimagenfo'] = $imageurl;
 		return $this;
 	}
 
