e0cbd72945ecc3b426b8456e255194ecf661ea6d
glory moshi <glorysimon95@gmail.com>
2019-03-20 18:23:32 +0300

    fix(changeSet)update dataoftype assets and calendardate fields

--- build/changeSets/2019/changetypeofdata.php
+++ build/changeSets/2019/changetypeofdata.php
@@ -23,18 +23,9 @@ class changetypeofdata extends cbupdaterWorker {
 		if ($this->isApplied()) {
 			$this->sendMsg('Changeset '.get_class($this).' already applied!');
 		} else {
-			$this->ExecuteQuery(
-				'update vtiger_field set typeofdata=? where tablename=? and columname=?',
-				array('D~M', 'vtiger_assets', 'datesold')
-			);
-			$this->ExecuteQuery(
-				'update vtiger_field set typeofdata=? where tablename=? and columname=?',
-				array('D~M~OTH~GE~datesold~Date Sold', 'vtiger_assets', 'dateinservice')
-			);
-			$this->ExecuteQuery(
-				'update vtiger_field set typeofdata=? where tablename=? and columname=?',
-				array('D~O~OTH~GE~date_start~Start Date & Time', 'vtiger_activity', 'due_date')
-			);
+			$this->ExecuteQuery("update vtiger_field set typeofdata=? where tablename=? and columnname=?", array('D~M', 'vtiger_assets', 'datesold'));
+			$this->ExecuteQuery("update vtiger_field set typeofdata=? where tablename=? and columnname=?", array('D~M~OTH~GE~datesold~Date Sold', 'vtiger_assets', 'dateinservice'));
+			$this->ExecuteQuery("update vtiger_field set typeofdata=? where tablename=? and columnname=?", array('D~O~OTH~GE~date_start~Start Date & Time', 'vtiger_activity', 'due_date'));
 			$this->sendMsg('Changeset '.get_class($this).' applied!');
 			$this->markApplied(false);
 		}
