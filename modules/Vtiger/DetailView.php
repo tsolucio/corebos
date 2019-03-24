c5146fd1204626b5da27ae96c10a0938ff9e6b00
Joe Bordes <joe@tsolucio.com>
2019-03-24 20:44:13 +0100

    cbSR(Product) eliminate warning

--- modules/Vtiger/DetailView.php
+++ modules/Vtiger/DetailView.php
@@ -153,7 +153,7 @@ if ($isPresentRelatedListBlock) {
 		$keys = array_keys($rl);
 		if (array_key_exists($keys[0], $_SESSION['BLOCKINITIALSTATUS']) && $_SESSION['BLOCKINITIALSTATUS'][$keys[0]] == 1) {
 			$open_related_modules[] = $keys[0];
-			$smarty->assign("SELECTEDHEADERS", $open_related_modules);
+			$smarty->assign('SELECTEDHEADERS', $open_related_modules);
 		}
 	}
 	$smarty->assign('RELATEDLISTBLOCK', $related_list_block);
