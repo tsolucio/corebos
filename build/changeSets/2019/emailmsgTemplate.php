5112fa543f7b307f9858c4545a2512e5380f6daa
shibe2018 <mohamed.staffs2018@gmail.com>
2019-04-24 15:50:08 +0300

    feat(MsgTemplate) update hard coded email into MsgTemplate

--- build/changeSets/2019/emailmsgTemplate.php
+++ build/changeSets/2019/emailmsgTemplate.php
@@ -16,57 +16,55 @@
 
 class emailmsgTemplate extends cbupdaterWorker {
 
-    public function applyChange() {
-        if ($this->hasError()) {
-            $this->sendError();
-        }
-        if ($this->isApplied()) {
-            $this->sendMsg('Changeset ' . get_class($this) . ' already applied!');
-        } else {
-            global $adb, $default_charset, $current_user, $mod_strings;
-            include_once 'modules/MsgTemplate/MsgTemplate.php';
+	public function applyChange() {
+		if ($this->hasError()) {
+			$this->sendError();
+		}
+		if ($this->isApplied()) {
+			$this->sendMsg('Changeset ' . get_class($this) . ' already applied!');
+		} else {
+			global $adb, $default_charset, $current_user, $mod_strings;
+			include_once 'modules/MsgTemplate/MsgTemplate.php';
 
-            $langs = array('en'=>'en_us','es'=>'es_es');
-            foreach ($langs as $shortcode => $longcode) {
-                
-                $mod_strings = return_module_language($longcode, 'HelpDesk');
-                $contents = $mod_strings['LBL_LOGIN_DETAILS'];
-                $contents .= "<br><br>".$mod_strings['LBL_USERNAME']." ".'$user_name$';
-                $contents .= "<br>".$mod_strings['LBL_PASSWORD']." ".'$user_password$';
-                $focus = new MsgTemplate();
-                $focus->id = '';
-                $focus->mode = '';
-                $focus->column_fields['reference'] = 'CustomerPortal_Mail_Password';
-                $focus->column_fields['msgt_type'] = 'Email';
-                $focus->column_fields['msgt_status'] = 'Active';
-                $focus->column_fields['msgt_language'] = $shortcode;
-                // $focus->column_fields['msgt_language'] = 'en';
-                $focus->column_fields['subject'] = $mod_strings['LBL_SUBJECT_PORTAL_LOGIN_DETAILS'];
-                $focus->column_fields['template'] = $contents;
-                $focus->column_fields['templateonlytext'] = $contents;
-                $focus->column_fields['tags'] = '';
-                $focus->column_fields['description'] = 'template that will be sent when customer portal user requests a new password';
-                $focus->column_fields['assigned_user_id'] = $current_user->id;
-                $focus->save('MsgTemplate');
+			$langs = array('en'=>'en_us','es'=>'es_es');
+			foreach ($langs as $shortcode => $longcode) {
+				$mod_strings = return_module_language($longcode, 'HelpDesk');
+				$contents = $mod_strings['LBL_LOGIN_DETAILS'];
+				$contents .= "<br><br>".$mod_strings['LBL_USERNAME']." ".'$user_name$';
+				$contents .= "<br>".$mod_strings['LBL_PASSWORD']." ".'$user_password$';
+				$focus = new MsgTemplate();
+				$focus->id = '';
+				$focus->mode = '';
+				$focus->column_fields['reference'] = 'CustomerPortal_Mail_Password';
+				$focus->column_fields['msgt_type'] = 'Email';
+				$focus->column_fields['msgt_status'] = 'Active';
+				$focus->column_fields['msgt_language'] = $shortcode;
+				// $focus->column_fields['msgt_language'] = 'en';
+				$focus->column_fields['subject'] = $mod_strings['LBL_SUBJECT_PORTAL_LOGIN_DETAILS'];
+				$focus->column_fields['template'] = $contents;
+				$focus->column_fields['templateonlytext'] = $contents;
+				$focus->column_fields['tags'] = '';
+				$focus->column_fields['description'] = 'template that will be sent when customer portal user requests a new password';
+				$focus->column_fields['assigned_user_id'] = $current_user->id;
+				$focus->save('MsgTemplate');
+			}
 
-            }
+			$mod = Vtiger_Module::getInstance('Contacts');
+			$block = Vtiger_Block::getInstance('LBL_CONTACT_INFORMATION', $mod);
+			$field = new Vtiger_Field();
+			$field->name = 'template_language';
+			$field->label = 'Template Language';
+			$field->columntype = 'varchar(6)';
+			$field->uitype = 15;
+			$field->displaytype = 1;
+			$field->typeofdata = 'V~O';
+			$block->addField($field);
+			// Adding picklist on module Contacts
+			$field->setPicklistValues($langs);
 
-            $mod = Vtiger_Module::getInstance('Contacts');
-            $block = Vtiger_Block::getInstance('LBL_CONTACT_INFORMATION', $mod);
-            $field = new Vtiger_Field();
-            $field->name = 'template_language';
-            $field->label = 'Template Language';
-            $field->columntype = 'varchar(6)';
-            $field->uitype = 15;
-            $field->displaytype = 1;
-            $field->typeofdata = 'V~O';
-            $block->addField($field);
-            // Adding picklist on module Contacts
-            $field->setPicklistValues($langs);
-
-            $this->sendMsg('Changeset ' . get_class($this) . ' applied!');
-            $this->markApplied();
-        }
-        $this->finishExecution();
-    }
+			$this->sendMsg('Changeset ' . get_class($this) . ' applied!');
+			$this->markApplied();
+		}
+		$this->finishExecution();
+	}
 }
--- include/utils/UserInfoUtil.php
+++ include/utils/UserInfoUtil.php
@@ -426,27 +426,27 @@ function fetchWordTemplateList($module) {
  * @returns Type:: resultset
  */
 function fetchEmailTemplateInfo($templateName, $desired_lang = null, $default_lang = null) {
-    require_once 'modules/cbtranslation/cbtranslation.php';
-    global $log, $adb, $current_user, $default_language;
-    $log->debug('> fetchEmailTemplateInfo '.$templateName);
-    if (!$desired_lang) {
-        $desired_lang = $current_user->language;
-        $desired_lang = cbtranslation::getShortLanguageName($desired_lang);
-    }
-    if (!$default_lang) {
-        $default_lang = $default_language;
-        $default_lang = cbtranslation::getShortLanguageName($default_lang);
-    }
-    $result = $adb->pquery('select * from vtiger_msgtemplate where reference=? and msgt_language=?', array($templateName, $desired_lang));
-    if (!$result) {
-        $result = $adb->pquery('select * from vtiger_msgtemplate where reference=? and msgt_language=?', array($templateName, $default_lang));
-    }
-    if (!$result) {
-        $result = $adb->pquery('select * from vtiger_msgtemplate where reference=?', array($templateName));
-    }
+	require_once 'modules/cbtranslation/cbtranslation.php';
+	global $log, $adb, $current_user, $default_language;
+	$log->debug('> fetchEmailTemplateInfo '.$templateName);
+	if (!$desired_lang) {
+		$desired_lang = $current_user->language;
+		$desired_lang = cbtranslation::getShortLanguageName($desired_lang);
+	}
+	if (!$default_lang) {
+		$default_lang = $default_language;
+		$default_lang = cbtranslation::getShortLanguageName($default_lang);
+	}
+	$result = $adb->pquery('select * from vtiger_msgtemplate where reference=? and msgt_language=?', array($templateName, $desired_lang));
+	if (!$result) {
+		$result = $adb->pquery('select * from vtiger_msgtemplate where reference=? and msgt_language=?', array($templateName, $default_lang));
+	}
+	if (!$result) {
+		$result = $adb->pquery('select * from vtiger_msgtemplate where reference=?', array($templateName));
+	}
 
-    $log->debug('< fetchEmailTemplateInfo');
-    return $result;
+	$log->debug('< fetchEmailTemplateInfo');
+	return $result;
 }
 
 /** Function to substitute the tokens in the specified file
