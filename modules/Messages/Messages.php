d751c0baaf5c0f78c6175a797d4e4746325afb7e
Joe Bordes <joe@tsolucio.com>
2019-04-25 09:40:47 +0200

    fix(Message) add icon

--- modules/Messages/Messages.php
+++ modules/Messages/Messages.php
@@ -21,6 +21,8 @@ class Messages extends CRMEntity {
 	/** Indicator if this is a custom module or standard module */
 	public $IsCustomModule = true;
 	public $HasDirectImageField = false;
+	public $moduleIcon = array('library' => 'standard', 'containerClass' => 'slds-icon_container slds-icon-standard-account', 'class' => 'slds-icon', 'icon'=>'messaging_conversation');
+
 	/**
 	 * Mandatory table for supporting custom fields.
 	 */
