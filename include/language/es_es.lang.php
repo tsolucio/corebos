9d4839242b63d22bae60632d1983e91476e59e5f
jonasinaj <j.sinaj@evolutivo.it>
2019-05-03 12:00:24 +0200

    feat(Tags) migrate mass tag functionality from cbcrm

--- include/language/es_es.lang.php
+++ include/language/es_es.lang.php
@@ -1284,6 +1284,10 @@ $app_strings = array (
 	//added for creating a custom lead map row in UI
 	'LBL_ADD_MAPPING'=> 'Añadir Vinculación',
 	'LBL_FIELD_MAPPING' => 'Vinculación Campos',
+	'LBL_MASSTAG_FORM_HEADER' => 'Mass Tag Operations',
+	'LBL_REMOVE_TAG' => 'Remove Tag',
+	'LBL_EXECUTE_MASSTAG' => 'Execute',
+	'LBL_CANCEL_MASSTAG' => 'Cancel',
 
 	'LBL_MANDATORY_FIELD_MISSING'=>'Falta rellenar un campo obligatorio',
 	'ERROR_GETTING_FILTER' => 'ERROR Aplicando Filtro. Por favor elimínalo y crealo de nuevo.',
