e5106a2078151fea97d7dc6ee480ffea75de57fb
Omar Llorens <omar@tsolucio.com>
2019-06-28 15:47:19 +0200

    fix(evvtgendoc): token cbCompany now is supported

--- modules/evvtgendoc/compile.php
+++ modules/evvtgendoc/compile.php
@@ -424,7 +424,7 @@ if (file_exists('modules/evvtgendoc/commands_'. OpenDocument::$compile_language 
 				}
 			} elseif (areModulesRelated($token_pair[0], $module)) {
 				$reemplazo = retrieve_from_db($marcador, $focus->column_fields[$related_module[$module][$token_pair[0]]], $token_pair[0]);
-			} elseif ($token_pair[0] == 'Organization') {
+			} elseif ($token_pair[0] == 'Organization' || $token_pair[0] == 'cbCompany') {
 				$res = $adb->query('SELECT * FROM vtiger_cbcompany WHERE defaultcompany=1');
 				$org_fields = $adb->getFieldsArray($res);
 				if (in_array($token_pair[1], $org_fields)) {
