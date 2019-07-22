9b630f270ef2f5405d7b9fac1fcf8012df6d0478
Joe Bordes <joe@tsolucio.com>
2019-07-22 17:05:44 +0200

    i18n(ro_ro)

---
+++ modules/PBXManager/language/ro_ro.lang.php
@@ -0,0 +1,42 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2019 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
+
+$mod_strings = array(
+	'PBX Manager' => 'Manager PBX',
+	'PBXManager' => 'Manager PBX',
+	'SINGLE_PBXManager' => 'Manager PBX',
+	'Asterisk' => 'Asterisk',
+	'LBL_ASTERISK_INFORMATION' => 'Informatii Asterisk',
+	'LBL_CALL_INFORMATION' => 'Informatii apel',
+	'Call From' => 'Apel De la',
+	'Call To' => 'Apel Catre',
+	'Time Of Call' => 'Timpul Apelului',
+	'PBXManager ID' => 'ID PBX Manager',
+	'ERR_Authenticate' => 'EROARE: authenticateUser. Eroare la conectarea cu serverul.',
+	'ERR_Numbers' => 'Extensia De LA sau numarul Destinatie goale. Nu se poate procesa apelul.',
+	'ERR_Call' => 'ERROR: createcall. Eroare la conectarea cu serverul.',
+	'outgoing' => 'Iesire',
+	'outgoing-missed' => 'Iesire - Pierdut',
+	'outgoing-received' => 'Iesire - Raspuns',
+	'incoming' => 'Intrare',
+	'incoming-missed' => 'Intrare - Pierdut',
+	'incoming-received' => 'Intrare - Primit',
+	'missed' => 'Pierdut',
+	'received' => 'Primit',
+	'OutgoingCall' => 'Apel iesire de la',
+	'Status' => 'Status',
+);
+?>
---
+++ modules/Project/language/ro_ro.lang.php
@@ -0,0 +1,75 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2019 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
+
+$mod_strings = array(
+	'LBL_MODULE_NAME' => 'Proiecte',
+	'LBL_MODULE_TITLE' => 'Proiecte',
+	'LBL_NEW_PROJECT' => 'Proiect Nou',
+	'LBL_PROJECTS' => 'Proiecte',
+	'LBL_PROJECT' => 'Proiect',
+	'projectname' => 'Nume proiect',
+	'Project' => 'Proiecte',
+	'SINGLE_Project' => 'Proiect',
+	'LBL_CUSTOM_INFORMATION' => 'Informatii Custom',
+	'LBL_PROJECT_INFORMATION' => 'Informatii Proiect',
+	'LBL_DESCRIPTION_INFORMATION' => 'Informatii Descriere',
+	'Project ID' => 'ID Proiect',
+	'Assigned To' => 'Asignat Lui',
+	'Start Date' => 'Data Start',
+	'Target End Date' => 'Data Sfarsit Previzionata',
+	'Actual End Date' => 'Data Sfarsit Efectiva',
+	'Status' => 'Status',
+	'Type' => 'Tip',
+	'Target Budget' => 'Buget Tinta',
+	'Project Url' => 'Url',
+	'Priority' => 'Prioritate',
+	'Progress' => 'Progres',
+	'Related To' => 'Relationat Cu',
+	'administrative' => 'administrativ',
+	'operative' => 'operativ',
+	'other' => 'altul',
+	'low' => 'mica',
+	'normal' => 'normala',
+	'high' => 'mare',
+	'prospecting' => 'Prospectare',
+	'in progress' => 'In Progres',
+	'initiated' => 'Initiat',
+	'on hold' => 'In Asteptare',
+	'archived' => 'Arhivat',
+	'completed' => 'Completat',
+	'delivered' => 'Livrat',
+	'--none--' => '--nimic--',
+	'Project Name' => 'Nume Proiect',
+	'LBL_RELATED_PROJECT_TASKS' => 'Sarcini Proiect',
+	'LBL_RELATED_PROJECT_MILESTONES' => 'Etape Proiect',
+	'Charts' => 'Harti',
+	'LBL_PROGRESS_CHART' => 'Harta Progres',
+	'Project No' => 'Nr Proiect',
+	'Project Milestones' => 'Etape Proiect',
+	'Project Tasks' => 'Sarcini Proiect',
+	'Project Team' => 'Echipa Proiect',
+	'Related to' => 'Relationat cu',
+	'Created Time' => 'Ora Creare',
+	'Modified Time' => 'Ora Modificare',
+	'Last Modified By' => 'Ultima Modificare De',
+	'Description' => 'Descriere',
+	'Email' => 'Email',
+	'Created By' => 'Creat De',
+	'Documents' => 'Documente',
+	'Trouble Tickets' => 'Tickete Probleme',
+	'Emails' => 'Email-uri',
+);
+?>
