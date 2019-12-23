6365e62eed984710950165951d977203ca266d92
joebordes <joe@tsolucio.com>
2019-12-23 23:55:03 +0100

    feat(RecordVersion) change activation to a more visible layout

--- Smarty/templates/modules/Utilities/recordversioning.tpl
+++ Smarty/templates/modules/Utilities/recordversioning.tpl
@@ -11,53 +11,37 @@
 			<div class="slds-media slds-no-space slds-grow">
 				<div class="slds-media__figure">
 					<svg aria-hidden="true" class="slds-icon slds-icon-standard-user">
-						<use xlink:href="include/LD/assets/icons/action-sprite/svg/symbols.svg#password_unlock"></use>
+						<use xlink:href="include/LD/assets/icons/action-sprite/svg/symbols.svg#change_record_type"></use>
 					</svg>
 				</div>
 				<div class="slds-media__body">
-					<h1 class="slds-page-header__title slds-m-right_small slds-align-middle slds-truncate"
-						title="{$TITLE_MESSAGE}">{$TITLE_MESSAGE}</h1>
+					<h1 class="slds-page-header__title slds-m-right_small slds-align-middle slds-truncate" title="{$TITLE_MESSAGE}">{$TITLE_MESSAGE}</h1>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <br>
-<form role="form" style="margin:0 100px;" name="faform">
+<form role="form" name="faform">
 <input type="hidden" name="module" value="Utilities">
 <input type="hidden" name="action" value="integration">
 <input type="hidden" name="_op" id="_op" value="setconfigrecordversioning">
-<header>
-	<div class="slds-form-element slds-lookup" data-select="single" style="width: 400px; margin-bottom: 6px;">
-		<label class="slds-form-element__label" for="lookup-339">{'LBL_MODULE'|getTranslatedString:'LBL_MODULE'}</label>
-		<div class="slds-form-element__control slds-grid slds-box_border">
-			<div class="slds-dropdown_trigger slds-dropdown-trigger_click slds-align-middle slds-m-left_xx-small slds-shrink-none">
-				<svg aria-hidden="true" class="slds-icon slds-icon-standard-account slds-icon_small">
-					<use xlink:href="include/LD/assets/icons/standard-sprite/svg/symbols.svg#user"></use>
-				</svg>
-			</div>
-			<div class="slds-input-has-icon slds-input-has-icon_right slds-grow">
-				<svg aria-hidden="true" class="slds-input__icon">
-					<use xlink:href="include/LD/assets/icons/utility-sprite/svg/symbols.svg#search"></use>
-				</svg>
-				<select name="module_list" id="module_list" class="slds-lookup__search-input slds-input_bare" type="search"
-					onChange="document.getElementById('_op').value='getconfigrecordversioning';document.faform.submit();"
-					aria-owns="module_list" role="combobox" aria-activedescendent="" aria-expanded="false" aria-autocomplete="list">
-					<option value="none" selected="true">{$APP.LBL_NONE}</option>
-					{$MODULELIST}
-				</select>
-			</div>
-		</div>
+<input type="hidden" name="tabid" id="tabid" value="">
+<input type="hidden" name="onoroff" id="onoroff" value="">
+<div class="slds-grid slds-wrap slds-p-around_x-small">
+	{foreach item=modinfo from=$MODULELIST}
+	<div class="slds-col slds-medium-size_1-of-2 slds-large-size_1-of-3 slds-size_1-of-1 slds-form-element slds-m-top_x-small slds-utility-bar__action">
+		<label class="slds-checkbox_toggle slds-grid">
+			<span class="slds-form-element__label slds-m-bottom_none slds-col slds-size_2-of-3">&nbsp;{$modinfo['name']}</span>
+			<input type="checkbox" name="tabid_{$modinfo['tabid']}" value="{$modinfo['tabid']}" aria-describedby="{$modinfo['name']}" {if $modinfo['visible']}checked{/if}
+				onChange="VtigerJS_DialogBox.block();document.getElementById('tabid').value=this.value;document.getElementById('onoroff').value=this.checked;document.faform.submit();" />
+			<span id="tabid_{$modinfo['tabid']}" class="slds-checkbox_faux_container slds-col slds-size_1-of-3" aria-live="assertive">
+				<span class="slds-checkbox_faux"></span>
+				<span class="slds-checkbox_on"></span>
+				<span class="slds-checkbox_off"></span>
+			</span>
+		</label>
 	</div>
-</header>
-<div class="slds-form-element">
-	<label class="slds-checkbox_toggle slds-grid">
-	<input type="checkbox" name="rvactive" aria-describedby="toggle-desc" {if $isActive}checked{/if} onChange="document.faform.submit();"/>
-	<span id="toggle-desc" class="slds-checkbox_faux_container" aria-live="assertive">
-		<span class="slds-checkbox_faux"></span>
-		<span class="slds-checkbox_on">{'LBL_ENABLED'|@getTranslatedString:'Settings'}</span>
-		<span class="slds-checkbox_off">{'LBL_DISABLED'|@getTranslatedString:'Settings'}</span>
-	</span>
-	</label>
+	{/foreach}
 </div>
 </form>
--- include/integrations/recordversioning/settings.php
+++ include/integrations/recordversioning/settings.php
@@ -17,184 +17,25 @@
  *  Author    : JPL TSolucio, S. L.
  *************************************************************************************************/
 global $current_user, $adb;
-include_once 'include/Webservices/Create.php';
-include_once 'vtlib/Vtiger/Module.php';
-require_once 'include/events/include.inc';
-require_once 'modules/com_vtiger_workflow/VTEntityMethodManager.inc';
+require_once 'include/integrations/recordversioning/RecordVersionUtils.php';
 
 $smarty = new vtigerCRM_Smarty();
 
-$moduleid = isset($_REQUEST['module_list']) ? vtlib_purify($_REQUEST['module_list']) : '';
-//check for global variable
-$recexists = $adb->pquery(
-	'select globalvariableid,module_list from vtiger_globalvariable inner join vtiger_crmentity on crmid=globalvariableid where deleted=0 and gvname=?',
-	array('RecordVersioningModules')
-);
-$count = $adb->num_rows($recexists);
-if ($count > 0) {
-	$module_list = explode(' |##| ', $adb->query_result($recexists, 0, 1));
-	$gvid = $adb->query_result($recexists, 0, 0);
-} else {
-	$module_list = array();
-	$gvid = 0;
-}
-$isAppActive = false;
+$tabid = isset($_REQUEST['tabid']) ? vtlib_purify($_REQUEST['tabid']) : 0;
+$moduleid = getTabModuleName($tabid);
+$rvutil = new RecordVersionUtils($moduleid);
+
 if (!empty($moduleid) && $_REQUEST['_op']=='setconfigrecordversioning') {
-	$isFormActive = ((empty($_REQUEST['rvactive']) || $_REQUEST['rvactive']!='on') ? '0' : '1');
-	//check for business action
-	$ba = $adb->pquery(
-		'select businessactionsid,module_list from vtiger_businessactions join vtiger_crmentity on crmid=businessactionsid where deleted=0 and active=? and linklabel=?',
-		array('1', 'Revisiones')
-	);
-	$bacount = $adb->num_rows($ba);
-	if ($ba && $bacount>0) {
-		$baid = $adb->query_result($ba, 0, 0);
-		$module_listba = $adb->query_result($ba, 0, 1);
-	} else {
-		$baid = 0;
-		$module_listba = '';
-	}
-	//check for workflow
-	$wfquery = $adb->pquery("select workflow_id from com_vtiger_workflows where module_name=? and summary='Update Revision Fields'", array($moduleid));
-	$wfcount = $adb->num_rows($wfquery);
-
+	$isFormActive = ((empty($_REQUEST['onoroff']) || $_REQUEST['onoroff']!='true') ? '0' : '1');
 	if ($isFormActive=='1') {
-		if ($count > 0 && !in_array($moduleid, $module_list)) {
-			$adb->pquery("update vtiger_globalvariable set module_list=CONCAT(module_list,' |##| $moduleid') where globalvariableid=?", array($gvid));
-		} elseif ($count==0) {
-			vtws_create('GlobalVariable', array(
-				'gvname' => 'RecordVersioningModules',
-				'default_check' => '0',
-				'value' => '1',
-				'mandatory' => '0',
-				'blocked' => '0',
-				'module_list' => $moduleid,
-				'category' => 'System',
-				'in_module_list' => '1',
-				'assigned_user_id' => vtws_getEntityId('Users').'x'.$current_user->id,
-			), $current_user);
-		}
-
-		if ($bacount > 0 && !in_array($moduleid, $module_list)) {
-			$adb->pquery("update vtiger_businessactions set module_list=CONCAT(module_list,' |##| $moduleid') where businessactionsid=?", array($baid));
-		} elseif ($bacount==0) {
-			vtws_create('BusinessActions', array(
-				'linklabel' => 'Revisiones',
-				'active' => '1',
-				'linktype' => 'DETAILVIEWWIDGET',
-				'linkurl' => 'module=Utilities&action=UtilitiesAjax&file=revisionblock&record=$RECORD$&currmodule=$MODULE$',
-				'mandatory' => '1',
-				'module_list' => $moduleid,
-				'assigned_user_id' => vtws_getEntityId('Users').'x'.$current_user->id,
-			), $current_user);
-		}
-
-		//create fields
-		$blockquery = $adb->pquery('select blockid from vtiger_blocks where visible=0 and tabid=? limit 1', array(getTabid($moduleid)));
-		$blockid = $adb->query_result($blockquery, 0, 0);
-		$block = Vtiger_Block::getInstance($blockid);
-
-		$mod = Vtiger_Module::getInstance($moduleid);
-		$fld = Vtiger_Field::getInstance('revision', $mod);
-		if (!$fld) {
-			$field1 = new Vtiger_Field();
-			$field1->name = 'revision';
-			$field1->label= 'Revision';
-			$field1->column = 'revision';
-			$field1->columntype = 'VARCHAR(100)';
-			$field1->uitype = 1;
-			$field1->typeofdata = 'V~O';
-			$field1->displaytype = 4;
-			$field1->presence = 0;
-			$block->addField($field1);
-			$adb->query('update '.$mod->basetable.' set revision=?', array('1'));
-		}
-		$fld2 = Vtiger_Field::getInstance('revisionactiva', $mod);
-		if (!$fld2) {
-			$field2 = new Vtiger_Field();
-			$field2->name = 'revisionactiva';
-			$field2->label= 'Active Revision';
-			$field2->column = 'revisionactiva';
-			$field2->columntype = 'VARCHAR(3)';
-			$field2->uitype = 56;
-			$field2->typeofdata = 'C~O';
-			$field2->displaytype = 4;
-			$field2->presence = 0;
-			$block->addField($field2);
-			$adb->query('update '.$mod->basetable.' set revisionactiva=?', array('1'));
-		}
-		//create event handler
-		$evhandler = $adb->pquery("select is_active,eventhandler_id from vtiger_eventhandlers where handler_class='UtilitiesEventsHandler'", array());
-		$counteh = $adb->num_rows($evhandler);
-		if ($counteh > 0) {
-			$isactive = $adb->query_result($evhandler, 0, 0);
-			$ehid = $adb->query_result($evhandler, 0, 1);
-			if ($isactive != 1) {
-				$adb->pquery('update vtiger_eventhandlers set is_active=1 where eventhandler_id=?', array($ehid));
-			}
-		} else {
-			$em = new VTEventsManager($adb);
-			$em->registerHandler('corebos.filter.listview.querygenerator.before', 'modules/Utilities/UtilitiesHandler.php', 'UtilitiesEventsHandler');
-		}
-		//create workflow
-		if ($wfcount == 0) {
-			require_once 'modules/com_vtiger_workflow/VTWorkflowManager.inc';
-			require_once 'modules/com_vtiger_workflow/VTTaskManager.inc';
-			require_once 'modules/com_vtiger_workflow/VTWorkflowApplication.inc';
-			require_once 'include/events/SqlResultIterator.inc';
-			$wm = new VTWorkflowManager($adb);
-			$wf = $wm->newWorkflow($moduleid);
-			$wf->description = 'Update Revision Fields';
-			$wf->purpose = 'Initialize revision fields on first save';
-			$wf->test = '';
-			$wf->executionConditionAsLabel('ON_FIRST_SAVE');
-			$wm->save($wf);
-
-			$tm = new VTTaskManager($adb);
-			$task = $tm->createTask('VTUpdateFieldsTask', $wf->id);
-			$task->summary = 'Update Revision Fields';
-			$task->active=true;
-			$task->field_value_mapping = '[{"fieldname":"revision","valuetype":"rawtext","value":"1"},{"fieldname":"revisionactiva","valuetype":"rawtext","value":"true:boolean"}]';
-			$tm->saveTask($task);
-		}
-		$isAppActive = true;
+		$rvutil->enableRecordVersionForModule();
 	} else {
-		$wfid = $adb->query_result($wfquery, 0, 0);
-		$wm = new VTWorkflowManager($adb);
-		$wm->delete($wfid);
-
-		$index = array_search($moduleid, $module_list);
-		unset($module_list[$index]);
-		if (count($module_list)>0) {
-			$module_del = implode(' |##| ', $module_list);
-		} else {
-			$module_del = '';
-		}
-		$adb->pquery("update vtiger_globalvariable set module_list='$module_del' where globalvariableid=?", array($gvid));
-		$adb->pquery("update vtiger_businessactions set module_list='$module_del' where businessactionsid=?", array($baid));
-		$isAppActive = false;
-	}
-} else {
-	if ($count>0 && in_array($moduleid, $module_list)) {
-		$isAppActive = true;
+		$rvutil->disableRecordVersionForModule();
 	}
 }
-
-$entitymodules = getAllowedPicklistModules(0);
-$opt = '';
-foreach ($entitymodules as $module) {
-	if ($moduleid == $module) {
-		$selected='selected';
-	} else {
-		$selected = '';
-	}
-	$opt.="<option value='$module' $selected>".getTranslatedString($module, $module).'</option>';
-}
-
-$smarty->assign('isActive', $isAppActive);
 
 $smarty->assign('TITLE_MESSAGE', getTranslatedString('Record Versioning', $currentModule));
-$smarty->assign('MODULELIST', $opt);
+$smarty->assign('MODULELIST', $rvutil::recverGetModuleinfo());
 $smarty->assign('APP', $app_strings);
 $smarty->assign('MOD', $mod_strings);
 $smarty->assign('MODULE', $currentModule);
