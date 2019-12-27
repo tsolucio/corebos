48c250ef64fa65e8ace549ba3677f4e029572871
joebordes <joe@tsolucio.com>
2019-12-27 16:41:46 +0100

    feat(Workflow:DuplicateRecord) refactor update revision fields code into function so it can be called also when duplicating from workflow

--- modules/Utilities/createrevision.php
+++ modules/Utilities/createrevision.php
@@ -18,35 +18,9 @@
 require_once 'include/utils/duplicate.php';
 
 function dq_createRevision($crmid, $module) {
-	global $adb;
 	$map = $module.'_DuplicateRelations';
 	$new_record_id = duplicaterec($module, $crmid, $map);
-	include_once "modules/$module/$module.php";
-	$focus = new $module;
-	$entityidfield = $focus->table_index;
-	$table_name = $focus->table_name;
-	$queryfield = $adb->pquery(
-		'select columnname from vtiger_field join vtiger_tab on vtiger_field.tabid=vtiger_tab.tabid where uitype=4 and name=?',
-		array($module)
-	);
-	if ($adb->num_rows($queryfield)==0) {
-		$uniquefield = $focus->list_link_field;
-	} else {
-		$uniquefield = $adb->query_result($queryfield, 0, 0);
-	}
-
-	$seqnors = $adb->pquery("select $uniquefield from $table_name where $entityidfield=?", array($crmid));
-	$seqno = $adb->query_result($seqnors, 0, 0);
-	$revisiones=$adb->pquery(
-		"select count($entityidfield) as num_revisiones
-		from $table_name
-		INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid = $table_name.$entityidfield
-		where deleted=0 and $uniquefield=? order by revision",
-		array($seqno)
-	);
-	$new_num_revision=intval($adb->query_result($revisiones, '0', 'num_revisiones')) + 1;
-	$adb->pquery("update $table_name set revision=?,$uniquefield=?,revisionactiva=1 where $entityidfield=?", array($new_num_revision,$seqno,$new_record_id));
-	$adb->pquery("update $table_name set revisionactiva=0 where $entityidfield!=? and $uniquefield=?", array($new_record_id, $seqno));
+	dq_updateRevisionFields($module, $crmid, $new_record_id);
 	return $new_record_id;
 }
 
