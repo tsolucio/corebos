c68b4a836e2a8076d4905cc5c5cac5fefa7f22c6
Joe Bordes <joe@tsolucio.com>
2020-01-25 19:51:34 +0100

    feat(Workflow) add exists and existsrelated functions to expression language

--- modules/com_vtiger_workflow/expression_functions/logicalops.php
+++ modules/com_vtiger_workflow/expression_functions/logicalops.php
@@ -28,3 +28,72 @@ function __cb_or($arr) {
 function __cb_and($arr) {
 	return $arr[0] && $arr[1];
 }
+
+function __cb_exists($arr) {
+	global $current_user, $adb;
+	$env = $arr[2];
+	$data = $env->getData();
+	$recordid = $data['id'];
+	$qg = new QueryGenerator($env->getModuleName(), $current_user);
+	$qg->addCondition($arr[0], $arr[1], 'e');
+	list($mid, $crmid) = explode('x', $recordid);
+	$qg->addCondition('id', $crmid, 'n', $qg::$AND);
+	$qe = 'SELECT EXISTS(SELECT 1 '.$qg->getFromClause().$qg->getWhereClause().')';
+	$rs = $adb->query($qe);
+	if ($rs) {
+		$ex = $adb->query_result($rs, 0, 0);
+		return ($ex == '1');
+	} else {
+		return false;
+	}
+}
+
+function __cb_existsrelated($params) {
+	global $adb;
+	$existsrelated = false;
+	$relatedmodule = $params[0];
+	$env = $params[3];
+	$data = $env->getData();
+	$recordid = $data['id'];
+	$module = $env->getModuleName();
+	if (!empty($relatedmodule) && !empty($recordid) && !empty($module)) {
+		list($mid, $crmid) = explode('x', $recordid);
+		$moduleId = getTabid($module);
+		$relatedModuleId = getTabid($relatedmodule);
+		$moduleInstance = CRMEntity::getInstance($module);
+		$relationResult = $adb->pquery(
+			'SELECT * FROM vtiger_relatedlists WHERE tabid=? AND related_tabid=?',
+			array($moduleId, $relatedModuleId)
+		);
+
+		if (!$relationResult || !$adb->num_rows($relationResult)) {
+			// MODULES_NOT_RELATED
+			return false;
+		}
+
+		$relationInfo = $adb->fetch_array($relationResult);
+		$relfunp = array($crmid, $moduleId, $relatedModuleId);
+		global $GetRelatedList_ReturnOnlyQuery;
+		$holdValue = $GetRelatedList_ReturnOnlyQuery;
+		$GetRelatedList_ReturnOnlyQuery = true;
+		$relationData = call_user_func_array(array($moduleInstance, $relationInfo['name']), $relfunp);
+		if (!isset($relationData['query'])) {
+			// OPERATIONNOTSUPPORTED
+			return false;
+		}
+		$GetRelatedList_ReturnOnlyQuery = $holdValue;
+		$relmod = Vtiger_Module::getInstance($relatedmodule);
+		$fld = Vtiger_Field::getInstance($params[1], $relmod);
+		if (!$fld) {
+			// FIELD NOT FOUND
+			return false;
+		}
+		$query = mkXQuery($relationData['query'], '1');
+		$query = stripTailCommandsFromQuery($query).' AND '.$fld->table.'.'.$fld->column.'=? LIMIT 1';
+		$result = $adb->pquery($query, array($params[2]));
+		if ($result) {
+			$existsrelated = ($adb->num_rows($result) > 0);
+		}
+	}
+	return $existsrelated;
+}
