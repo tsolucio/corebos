f92c846250b45ebafb851917ac285f90defe7fac
Denald Hushi <denalddh@gmail.com>
2020-04-08 23:41:16 +0200

    feat(DataTable) checkbox render

--- Smarty/templates/Components/ComponentsJS.tpl
+++ Smarty/templates/Components/ComponentsJS.tpl
@@ -4,3 +4,4 @@
 <script src="./include/components/toast-ui/pagination/tui-pagination.js"></script>
 <script src="./include/components/toast-ui/tui-date-picker/tui-date-picker.js"></script>
 <script src="./include/components/toast-ui/grid/tui-grid.js"></script>
+<script src="./include/components/checkboxrenderer.js"></script>
---
+++ include/components/checkboxrenderer.js
@@ -0,0 +1,60 @@
+class RowNumberRenderer {
+  constructor(props) {
+    const el = document.createElement('span');
+    el.innerHTML = `${props.formattedValue}`;
+    this.el = el;
+  }
+
+  getElement() {
+    return this.el;
+  }
+
+  render(props) {
+    this.el.innerHTML = `${props.formattedValue}`;
+  }
+}
+
+class CheckboxRenderer {
+  constructor(props) {
+    const { grid, rowKey } = props;
+
+    const label = document.createElement('label');
+    label.className = 'checkbox';
+    label.setAttribute('for', String(rowKey));
+
+    const hiddenInput = document.createElement('input');
+    hiddenInput.className = 'hidden-input';
+    hiddenInput.name = 'deleteFields';
+    hiddenInput.id = String(rowKey);
+
+    const customInput = document.createElement('span');
+    customInput.className = 'custom-input';
+
+    label.appendChild(hiddenInput);
+    label.appendChild(customInput);
+
+    hiddenInput.type = 'checkbox';
+    hiddenInput.addEventListener('change', () => {
+      if (hiddenInput.checked) {
+        grid.check(rowKey);
+      } else {
+        grid.uncheck(rowKey);
+      }
+    });
+
+    this.el = label;
+
+    this.render(props);
+  }
+
+  getElement() {
+    return this.el;
+  }
+
+  render(props) {
+    const hiddenInput = this.el.querySelector('.hidden-input');
+    const checked = Boolean(props.value);
+
+    hiddenInput.checked = checked;
+  }
+}
