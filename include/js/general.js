f35bd20c07c1a854e7f0ccf41151c577dd86bba3
joebordes <joe@tsolucio.com>
2020-04-28 00:14:33 +0200

    fix(Calendar) deactivate calendar check if module access is not permitted

--- include/js/general.js
+++ include/js/general.js
@@ -3304,6 +3304,12 @@ var ActivityReminder_Deactivated = 0;
 GlobalVariable_getVariable('Debug_ActivityReminder_Deactivated', 0, 'cbCalendar', '').then(function (response) {
 	var obj = JSON.parse(response);
 	ActivityReminder_Deactivated = obj.Debug_ActivityReminder_Deactivated;
+	ExecuteFunctions('ispermitted', 'checkmodule=Calendar&checkaction=index').then(function (response) {
+		var obj = JSON.parse(response);
+		if (obj.isPermitted == false) {
+			ActivityReminder_Deactivated = 1;
+		}
+	});
 }, function (error) {
 	ActivityReminder_Deactivated = 0;
 });
