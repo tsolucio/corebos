ef86bc16a4593ef96750e000c1b900443de5cd61
Lorida Cito <loridacito@gmail.com>
2020-05-02 22:43:17 +0200

    feat(Socket.io) adding submodule

--- socketlockit/ssl-config.js
+++
@@ -1,4 +0,0 @@
-var path = require('path'),
-fs = require("fs");
-exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privkey.pem')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, './private/cert.pem')).toString();
