4f6e6aecdc20b73bdf95267acb6c7ada6bf14d40
joebordes <joe@tsolucio.com>
2020-07-09 02:54:44 +0200

    fix(PurchaseOrder) fill vendor field when converting from Product

--- modules/PurchaseOrder/EditView.php
+++ modules/PurchaseOrder/EditView.php
@@ -173,8 +173,10 @@ if (!empty($_REQUEST['parent_id']) && !empty($_REQUEST['return_module'])) {
 }
 
 if (!empty($_REQUEST['vendor_id']) && $_REQUEST['record']=='') {
+	$vendid = vtlib_purify($_REQUEST['vendor_id']);
 	$vend_focus = CRMEntity::getInstance('Vendors');
-	$vend_focus->retrieve_entity_info($_REQUEST['vendor_id'], 'Vendors');
+	$vend_focus->retrieve_entity_info($vendid, 'Vendors');
+	$focus->column_fields['vendor_id']=$vendid;
 	$focus->column_fields['bill_city']=$vend_focus->column_fields['city'];
 	$focus->column_fields['ship_city']=$vend_focus->column_fields['city'];
 	$focus->column_fields['bill_street']=$vend_focus->column_fields['street'];
