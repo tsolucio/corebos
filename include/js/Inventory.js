129ebec760f52b348295205000319e0b80673ab9
Luke1982 <gcx3@hotmail.com>
2020-08-04 17:01:47 +0200

    feat(Inventory) include ldsCheckbox

--- include/js/Inventory.js
+++ include/js/Inventory.js
@@ -1990,7 +1990,7 @@ window.addEventListener('load', function () {
 					me.fields[field.getFieldName()] = field;
 				}
 			}
-			
+
 			ldsCheckbox.setUnique();
 		}
 		construct(this);
@@ -2287,6 +2287,52 @@ window.addEventListener('load', function () {
 	* Export
 	*/
 	return InventoryLine;
+});
+
+/****
+* ldsCheckbox
+* @author: MajorLabel <info@majorlabel.nl>
+* @license GNU
+*/
+(function ldscheckboxModule(factory){
+
+	if (typeof define === "function" && define.amd) {
+		define(factory);
+	} else if (typeof module != "undefined" && typeof module.exports != "undefined") {
+		module.exports = factory();
+	} else {
+		window["ldsCheckbox"] = factory();
+	}
+
+})(function ldscheckboxFactory(){
+
+	/**
+	 * @class ldsCheckbox
+	 */
+	function ldsCheckbox() {
+		/* Public attributes */
+	}
+
+	/*
+		* Static methods
+		*/
+	ldsCheckbox.setUnique = function() {
+		var ldsCheckboxes = document.getElementsByClassName("slds-checkbox");
+		for (var i = 0; i < ldsCheckboxes.length; i++) {
+			ldsCheckboxes[i].getElementsByTagName("input")[0].id = "slds-checkbox-" + i,
+			ldsCheckboxes[i].getElementsByClassName("slds-checkbox__label")[0].setAttribute("for", "slds-checkbox-" + i);
+		}
+	}
+
+	ldsCheckbox.prototype = {
+		constructor: ldsCheckbox,
+	}
+
+	/*
+		* Export
+		*/
+	return ldsCheckbox;
+
 });
 
 /****
