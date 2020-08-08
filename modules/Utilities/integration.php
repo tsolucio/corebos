9f3d1f3894beb6066a44c5866bad0a64d97c622e
joebordes <joe@tsolucio.com>
2020-08-08 16:49:03 +0200

    feat(Webservice) road runner web service Login System Synchronization

--- modules/Utilities/integration.php
+++ modules/Utilities/integration.php
@@ -98,6 +98,10 @@ switch ($_REQUEST['_op']) {
 	case 'setconfiglogall':
 		include_once 'include/integrations/logall/settings.php';
 		break;
+	case 'getconfigloginsync':
+	case 'setconfigloginsync':
+		include_once 'include/integrations/loginsync/settings.php';
+		break;
 	default:
 		$smarty = new vtigerCRM_Smarty();
 		$titlemessage = getTranslatedString('Available Integrations', $currentModule);
@@ -169,6 +173,14 @@ switch ($_REQUEST['_op']) {
 				'url' => 'index.php?action=integration&module=Utilities&_op=getconfiglogall',
 			),
 		);
+		if (file_exists('build/wsChanges/LoginSession.php')) {
+			$intgr[] = array(
+				'abbr' => 'LS',
+				'title' => getTranslatedString('Login Sync Activation', 'Utilities'),
+				'desc' => getTranslatedString('Login_Synic Desc', 'Utilities'),
+				'url' => 'index.php?action=integration&module=Utilities&_op=getconfigloginsync',
+			);
+		}
 		$smarty->assign('integrations', $intgr);
 		$smarty->assign('TITLE_MESSAGE', $titlemessage);
 		$smarty->assign('APP', $app_strings);
