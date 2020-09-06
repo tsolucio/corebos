a3d1a9fb16714951988d20ea833dace46d4bc3d9
joebordes <joe@tsolucio.com>
2020-09-06 13:04:03 +0200

    cbSR(Calendar) iCal

--- modules/Calendar/iCal/bennu.inc.php
+++ modules/Calendar/iCal/bennu.inc.php
@@ -8,16 +8,16 @@
  *
  *  See http://bennu.sourceforge.net/ for more information and downloads.
  *
- * @author Ioannis Papaioannou 
+ * @author Ioannis Papaioannou
  * @version $Id$
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
 
-if(!defined('_BENNU_VERSION')) {
-    define('_BENNU_VERSION', '0.1');
-    include('bennu.class.php');
-    include('iCalendar_rfc2445.php');
-    include('iCalendar_components.php');
-    include('iCalendar_properties.php');
-    include('iCalendar_parameters.php');
+if (!defined('_BENNU_VERSION')) {
+	define('_BENNU_VERSION', '0.1');
+	include 'bennu.class.php';
+	include 'iCalendar_rfc2445.php';
+	include 'iCalendar_components.php';
+	include 'iCalendar_properties.php';
+	include 'iCalendar_parameters.php';
 }
