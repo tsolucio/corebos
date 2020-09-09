d69b45a8f34132fa0983c7a874dd637afd79c3a8
Mario Anthony <anthonyjeremiah1956@gmail.com>
2020-09-09 17:57:50 +0300

    feat(Emails) Debug SendEmail Function with Inbucket

--- modules/GlobalVariable/language/en_us.gvdefs.php
+++ modules/GlobalVariable/language/en_us.gvdefs.php
@@ -111,6 +111,13 @@ $GlobalVariableDefinitons = array(
 	'values' => '0 | 1',
 	'definition' => 'If set to 1 a call stack will be output before the "Sorry! Attempt to access restricted file." error message.',
 ),
+'Debug_Email_Send_To_Inbucket'=> array(
+	'status' => 'Implemented',
+	'valuetype' => 'Boolean',
+	'category' => 'Debug',
+	'values' => '0 | 1',
+	'definition' => 'If set to 1 will sent email to inBucket ',
+),
 'Accounts_BlockDuplicateName' => array(
 	'status' => 'Implemented',
 	'valuetype' => 'Boolean',
