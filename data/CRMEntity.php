tsolucio/corebos

commit 4903f7cc3b9f81bba3fa222b6a11121d80646786

fix(MysqlStrict) set empty time field (uitype 14) to null to avoid mariadb strict mode error

data/CRMEntity.php
@@ -759,6 +759,8 @@ class CRMEntity {
 					} else {
 						$fldvalue = $this->column_fields[$fieldname];
 					}
+				} elseif ($uitype == 14 && empty($fldvalue)) {
+					$fldvalue = null;
 				} elseif ($uitype == 50) {
 					$timefmt = '';
 					if (!empty($this->column_fields[$fieldname]) && strlen($this->column_fields[$fieldname])>16) {

commit 5937548443051ce48bedafc029b7287ef948c33c

fix(TimeField) save issue introduced in 4903f7cc3b9f81bba3fa222b6a11121d80646786

data/CRMEntity.php
@@ -756,7 +756,7 @@ class CRMEntity {
 					} else {
 						$fldvalue = $this->column_fields[$fieldname];
 					}
-				} elseif ($uitype == 14 && empty($fldvalue)) {
+				} elseif ($uitype == 14 && empty($this->column_fields[$fieldname])) {
 					$fldvalue = null;
 				} elseif ($uitype == 50) {
 					$timefmt = '';