17c1cd9c481c3cbeca3917d783d4fc6e1637fa17
george1806 <fortinatus.george1806@gmail.com>
2020-10-15 22:08:43 +0300

    feat(workflow) support iCalendar invitation attachment in email task

--- modules/com_vtiger_workflow/language/en_us.lang.php
+++ modules/com_vtiger_workflow/language/en_us.lang.php
@@ -243,5 +243,7 @@ $mod_strings = array(
 'LBL_SELECT_CREDENTIAL' => 'Select Credential',
 'LBL_SEND_FILE' => 'Send file to',
 'FILENAME' => 'Destination filename',
+'LBL_ENABLED' => 'Enabled',
+'LBL_DISABLED' => 'Disabled',
 );
 ?>
