4539c071f4e297eec2b946628b0c53e38a8fbc41
Timothy Anthony <anthony.timothy90@gmail.com>
2020-10-22 13:40:33 +0300

    fix(Webservices) Validating user with status and deleted values

--- include/Webservices/Create.php
+++ include/Webservices/Create.php
@@ -103,8 +103,9 @@ function vtws_create($elementType, $element, $user) {
 				}
 			}
 		}
+		// Validate assigned_user_id
 		list($void, $assigned_user_id) = explode('x', $element['assigned_user_id']);
-		$result = $adb->pquery('select first_name from vtiger_users where id=?', array($assigned_user_id));
+		$result = $adb->pquery('select first_name from vtiger_users where id=? and status=? and deleted=?', array($assigned_user_id, 'Active', 0));
 		if (!$result || $adb->num_rows($result)==0) {
 			throw new WebServiceException(WebServiceErrorCode::$REFERENCEINVALID, "Invalid reference specified for assigned_user_id");
 		}
