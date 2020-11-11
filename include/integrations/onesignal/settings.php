0a2b17f7c19f693586fbaa0f63680c04f487fd41
Mario Anthony <anthonyjeremiah1956@gmail.com>
2020-11-11 15:20:09 +0300

    fix(Utilities)Update the Copyright year

--- include/integrations/onesignal/settings.php
+++ include/integrations/onesignal/settings.php
@@ -1,6 +1,6 @@
 <?php
 /*************************************************************************************************
- * Copyright 2019 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS customizations.
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS customizations.
  * You can copy, adapt and distribute the work under the "Attribution-NonCommercial-ShareAlike"
  * Vizsage Public License (the "License"). You may not use this file except in compliance with the
  * License. Roughly speaking, non-commercial users may share and modify this code, but must give credit
