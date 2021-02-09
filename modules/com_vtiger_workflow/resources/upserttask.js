0b87b90a32e390fa136fc1e4d2ef88943dd63efd
denald <denalddh@gmail.com>
2021-02-09 11:33:01 +0100

    feat(Workflow) add upsert task

---
+++ modules/com_vtiger_workflow/resources/upserttask.js
@@ -0,0 +1,201 @@
+/*************************************************************************************************
+ * Copyright 2020 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************
+ *  Author       : JPL TSolucio, S. L.
+ *************************************************************************************************/
+//addition to stringify json function
+(function () {
+	// Convert array to object
+	var convArrToObj = function (array) {
+		var thisEleObj = new Object();
+		if (typeof array == 'object') {
+			for (var i in array) {
+				var thisEle = convArrToObj(array[i]);
+				thisEleObj[i] = thisEle;
+			}
+		} else {
+			thisEleObj = array;
+		}
+		return thisEleObj;
+	};
+	var oldJSONStringify = JSON.stringify;
+	JSON.stringify = function (input) {
+		if (oldJSONStringify(input) == '[]') {
+			return oldJSONStringify(convArrToObj(input));
+		} else {
+			return oldJSONStringify(input);
+		}
+	};
+})();
+
+function CBUpsertTask($) {
+	var vtinst = new VtigerWebservices('webservice.php');
+	var desc = null;
+	var format = fn.format;
+
+	function errorDialog(message) {
+		alert(message);
+	}
+
+	function handleError(fn) {
+		return function (status, result) {
+			if (status) {
+				fn(result);
+			} else {
+				errorDialog('Failure:'+result);
+			}
+		};
+	}
+
+	function implode(sep, arr) {
+		var out = '';
+		$.each(arr, function (i, v) {
+			out+=v;
+			if (i<arr.length-1) {
+				out+=sep;
+			}
+		});
+		return out;
+	}
+
+	function fillOptions(el, options, empt) {
+		if (empt==0) {
+			el.empty();
+		}
+		$.each(options, function (k, v) {
+			if (v.indexOf('----')>-1) {
+				var dis='disabled';
+			} else {
+				dis='';
+			}
+			el.append('<option value="'+k+'" '+dis+'>'+v+'</option>');
+		});
+	}
+
+	function map(fn, list) {
+		var out = [];
+		$.each(list, function (i, v) {
+			out[out.length]=fn(v);
+		});
+		return out;
+	}
+
+	function reduceR(fn, list, start) {
+		var acc = start;
+		$.each(list, function (i, v) {
+			acc = fn(acc, v);
+		});
+		return acc;
+	}
+
+	function dict(list) {
+		var out = {};
+		$.each(list, function (i, v) {
+			out[v[0]] = v[1];
+		});
+		return out;
+	}
+
+	function filter(pred, list) {
+		var out = [];
+		$.each(list, function (i, v) {
+			if (pred(v)) {
+				out[out.length]=v;
+			}
+		});
+		return out;
+	}
+
+	function contains(list, value) {
+		var ans = false;
+		$.each(list, function (i, v) {
+			if (v==value) {
+				ans = true;
+				return false;
+			}
+		});
+		return ans;
+	}
+
+	function diff(reflist, list) {
+		var out = [];
+		$.each(list, function (i, v) {
+			if (contains(reflist, v)) {
+				out.push(v);
+			}
+		});
+		return out;
+	}
+
+	function concat(lista, listb) {
+		return lista.concat(listb);
+	}
+
+	//Get an array containing the the description of a module and all modules
+	//refered to by it. This is passed to callback.
+	function getDescribeObjects(accessibleModules, moduleName, callback) {
+		vtinst.describeObject(moduleName, handleError(function (result) {
+			var parent = result;
+			var fields = parent['fields'];
+			var referenceFields = filter(function (e) {
+				return e['type']['name']=='reference';
+			},
+			fields);
+			var referenceFieldModules =
+			map(
+				function (e) {
+					return e['type']['refersTo'];
+				},
+				referenceFields
+			);
+			function union(a, b) {
+				var newfields = filter(function (e) {
+					return !contains(a, e);
+				}, b);
+				return a.concat(newfields);
+			}
+			var relatedModules = reduceR(union, referenceFieldModules, [parent['name']]);
+
+			// Remove modules that is no longer accessible
+			relatedModules = diff(accessibleModules, relatedModules);
+
+			function executer(parameters) {
+				var failures = filter(function (e) {
+					return e[0]==false;
+				}, parameters);
+				if (failures.length!=0) {
+					var firstFailure = failures[0];
+					callback(false, firstFailure[1]);
+				} else {
+					var moduleDescriptions = map(function (e) {
+						return e[1];
+					},
+					parameters);
+					var modules = dict(map(function (e) {
+						return [e['name'], e];
+					},
+					moduleDescriptions));
+					callback(true, modules);
+				}
+			}
+			var p = parallelExecuter(executer, relatedModules.length);
+			$.each(relatedModules, function (i, v) {
+				p(function (callback) {
+					vtinst.describeObject(v, callback);
+				});
+			});
+		}));
+	}
+}
+CBUpsertTask = CBUpsertTask(jQuery);
---
+++ modules/com_vtiger_workflow/tasks/CBUpsertTask.php
@@ -0,0 +1,146 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2021 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************
+ *  Author       : JPL TSolucio, S. L.
+ *************************************************************************************************/
+require_once 'modules/com_vtiger_workflow/VTEntityCache.inc';
+require_once 'modules/com_vtiger_workflow/VTWorkflowUtils.php';
+require_once 'modules/com_vtiger_workflow/VTTaskQueue.inc';
+require_once 'modules/cbMap/cbMap.php';
+require_once 'include/events/include.inc';
+
+class CBUpsertTask extends VTTask {
+	public $executeImmediately = true;
+
+	public function getFieldNames() {
+		return array('bmapid', 'bmapid_display');
+	}
+
+	public function doTask(&$entity) {
+		global $adb, $current_user, $logbg, $from_wf, $currentModule;
+		$from_wf = true;
+		$logbg->debug('> CBUpsertTask');
+		$util = new VTWorkflowUtils();
+		$util->adminUser();
+		$isqueue=$entity->isqueue;
+		$taskQueue = new VTTaskQueue($adb);
+		$moduleName = $entity->getModuleName();
+		$context = $entity->WorkflowContext;
+		if (empty($currentModule) || $currentModule!=$moduleName) {
+			$currentModule = $moduleName;
+		}
+		$entityId = $entity->getId();
+		$recordId = vtws_getIdComponents($entityId);
+		$recordId = $recordId[1];
+		$logbg->debug("Module: $moduleName, Record: $entityId");
+		if ($context != '') {
+			$context_data = json_decode($context['context'], true);
+			$ws_map_response = $context['ws_map_response'];
+			foreach ($ws_map_response as $key => $value) {
+				if ($key != 'message' && $key != 'data') {
+					$field_map = $value['field'];
+					$context_map = str_replace('wsctx_', '', $value['context']);
+					if (strpos($field_map, '.') !== false) {
+						$related_modulename = explode('.', $field_map)[0];
+						$fieldname = explode('.', $field_map)[1];
+						$fields_data = $adb->pquery('SELECT * FROM vtiger_field WHERE columnname=?', array($fieldname));
+						$tabId = $adb->query_result($fields_data, 0, 'tabid');
+						$uitype = $adb->query_result($fields_data, 0, 'uitype');
+						$module = vtlib_getModuleNameById($tabId);
+						if (!array_key_exists($module, $map_fields)) {
+							$map_fields[$module] = array();
+						}
+						array_push($map_fields[$module], array(
+							'field' => $fieldname,
+							'context' => $context_map,
+							'uitype' => $uitype,
+							'related_modulename' => $uitype == '10' ? $related_modulename : ''
+						));
+					}
+				}
+			}
+			//match fields from map with values in ws response
+			foreach ($context_data as $key) {
+				$map_records = array();
+				foreach ($map_fields as $module => $fld) {
+					$crmid = coreBOS_Rule::evaluate($this->bmapid, $key);
+					$flds = array_map(function ($k) use ($module, $key) {
+						$field = $k['field'];
+						$context_field = $k['context'];
+						$uitype = $k['uitype'];
+						$related_modulename = $k['related_modulename'];
+						return array(
+							$module,
+							$field,
+							$key[$context_field],
+							$uitype,
+							$related_modulename,
+						);
+					}, $fld);
+					$records = $this->groupArrayByKey($flds, 0);
+					if (empty($crmid)) {
+						$this->upsertData($records, 'doCreate');
+					} else {
+						$this->upsertData($records, 'doUpdate', $crmid);
+					}
+				}
+			}
+		}
+		$util->revertUser();
+		$from_wf = false;
+		$logbg->debug('< CBUpsertTask');
+		die;
+	}
+
+	public function upsertData($data, $action, $crmid = 0) {
+		global $logbg, $adb, $current_user;
+		$logbg->debug('> upsertData');
+		foreach ($data as $module => $val) {
+			include_once "modules/$module/$module.php";
+			$moduleHandler = vtws_getModuleHandlerFromName($module, $current_user);
+			$handlerMeta = $moduleHandler->getMeta();
+			$focus = new $module();
+			if ($action == 'doCreate') {
+				$focus->modue = '';
+			} elseif ($action == 'doUpdate') {
+				$focus->retrieve_entity_info($crmid, $module);
+				$focus->id = $crmid;
+				$focus->modue = 'edit';
+			}
+			for ($i=0; $i<count($val); $i++) {
+				$field = $val[$i][1];
+				$value = $val[$i][2];
+				$uitype = $val[$i][3];
+				$relMod = $val[$i][4];
+				$focus->column_fields[$field] = $value;
+			}
+			$focus->column_fields = DataTransform::sanitizeRetrieveEntityInfo($focus->column_fields, $handlerMeta);
+			$focus->save($module);
+		}
+		$logbg->debug('< upsertData');
+	}
+
+	public function groupArrayByKey($array, $key) {
+		global $logbg;
+		$logbg->debug('> groupArrayByKey');
+		$result = array();
+		foreach ($array as $element) {
+			$result[$element[$key]][] = $element;
+		}
+		$logbg->debug('< groupArrayByKey');
+		return $result;
+	}
+}
+?>
