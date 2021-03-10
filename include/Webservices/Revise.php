a119efd4051bb2f496378690a6c97102af2f90bd
joebordes <joe@tsolucio.com>
2021-03-10 23:02:00 +0100

    fix(WebService) avoid access to reference field if user does not have permission on it

--- include/Webservices/Revise.php
+++ include/Webservices/Revise.php
@@ -115,7 +115,7 @@ function vtws_revise($element, $user) {
 	if (count($listofrelfields)>0) {
 		$deref = unserialize(vtws_getReferenceValue(serialize($listofrelfields), $user));
 		foreach ($r as $relfield => $mods) {
-			if (!empty($entity[$relfield])) {
+			if (!empty($entity[$relfield]) && !empty($deref[$entity[$relfield]])) {
 				$entity[$relfield.'ename'] = $deref[$entity[$relfield]];
 			}
 		}
