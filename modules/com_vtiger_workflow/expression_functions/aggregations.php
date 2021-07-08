f8be27074cf468759c8da0bf34f6a6bf9d8d2e37
joebordes <joe@tsolucio.com>
2021-07-08 18:06:46 +0200

    feat(Workflow) aggregation group_concat on uitype10 fields now can return the name instead of the ID concatenating 'name' after the separator

--- modules/com_vtiger_workflow/expression_functions/aggregations.php
+++ modules/com_vtiger_workflow/expression_functions/aggregations.php
@@ -88,9 +88,14 @@ function __cb_aggregation_getQuery($arr, $userdefinedoperation = true) {
 		return 0;
 	}
 	$sep = '';
+	$useNamesForUIType10 = false;
 	if ($operation=='group_concat') { // to support 'separator'
 		if (stripos($arr[2], 'separator')!==false) {
 			$sep = ' '.trim(substr($arr[2], strpos($arr[2], ' ')));
+			if (strtolower(substr($sep, -4))=='name') {
+				$useNamesForUIType10 = true;
+				$sep = ' '.trim(substr($sep, 0, strlen($sep)-4));
+			}
 			$arr[2] = trim(substr($arr[2], 0, strpos($arr[2], ' ')));
 		} else {
 			$arr[2] = trim($arr[2]);
@@ -154,7 +159,14 @@ function __cb_aggregation_getQuery($arr, $userdefinedoperation = true) {
 	if ($userdefinedoperation) {
 		$query = 'select '.$operation.'('.$relfields_operation.') as aggop '.$qfrom;
 	} else {
-		$query = 'select '.$operation.'('.$rfield->table.'.'.$rfield->column.$sep.') as aggop '.$qfrom;
+		if ($useNamesForUIType10 && $rfield->uitype==Field_Metadata::UITYPE_RECORD_RELATION) {
+			$secLevelRel = getFirstModule($relmodule, $rfield->name);
+			$einfo = getEntityField($secLevelRel);
+			$fname = $einfo['tablename'].'.'.$einfo['fieldname'];
+		} else {
+			$fname = $rfield->table.'.'.$rfield->column;
+		}
+		$query = 'select '.$operation.'('.$fname.$sep.') as aggop '.$qfrom;
 	}
 	$logbg->debug("Agg query: $query");
 	return $query;
