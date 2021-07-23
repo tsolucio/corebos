77c685637cddff036ca4a382d2864078be0eb6d1
joebordes <joe@tsolucio.com>
2021-07-24 01:21:55 +0200

    feat(Comments) set related list with documents

--- build/changeSets/2021/setDocRelComments.php
+++ build/changeSets/2021/setDocRelComments.php
@@ -1,6 +1,6 @@
 <?php
 /*************************************************************************************************
- * Copyright 2014 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Copyright 2021 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
 * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
 * file except in compliance with the License. You can redistribute it and/or modify it
 * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
@@ -14,7 +14,7 @@
 * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
 *************************************************************************************************/
 
-class setDocRelVendor extends cbupdaterWorker {
+class setDocRelComments extends cbupdaterWorker {
 
 	public function applyChange() {
 		if ($this->hasError()) {
@@ -23,7 +23,7 @@ class setDocRelVendor extends cbupdaterWorker {
 		if ($this->isApplied()) {
 			$this->sendMsg('Changeset '.get_class($this).' already applied!');
 		} else {
-			$module = 'Vendors';
+			$module = 'ModComments';
 			$relmodule = 'Documents';
 			$mod = Vtiger_Module::getInstance($module);
 			$relmod = Vtiger_Module::getInstance($relmodule);
@@ -39,7 +39,7 @@ class setDocRelVendor extends cbupdaterWorker {
 			$this->sendError();
 		}
 		if ($this->isApplied()) {
-			$module = 'Vendors';
+			$module = 'ModComments';
 			$relmodule = 'Documents';
 			$mod = Vtiger_Module::getInstance($module);
 			$relmod = Vtiger_Module::getInstance($relmodule);
