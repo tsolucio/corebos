ddbfcdc187d5c8996183e4762be72cc0a431c64b
joebordes <joe@tsolucio.com>
2021-07-29 17:58:48 +0200

    opt(ListSearch) delete duplicate SQL condition

--- include/Webservices/CustomerPortalWS.php
+++ include/Webservices/CustomerPortalWS.php
@@ -670,7 +670,6 @@ function getSearchingListViewEntries($focus, $module, $list_result, $navigation_
 				INNER JOIN vtiger_def_org_field ON vtiger_def_org_field.fieldid = vtiger_field.fieldid
 				WHERE vtiger_field.tabid=? and vtiger_field.presence in (0,2)
 				AND vtiger_profile2field.visible = 0
-				AND vtiger_profile2field.visible = 0
 				AND vtiger_def_org_field.visible = 0
 				AND vtiger_profile2field.profileid IN ('. generateQuestionMarks($profileList) .')
 				AND vtiger_field.fieldname IN ('. generateQuestionMarks($field_list) .')';
