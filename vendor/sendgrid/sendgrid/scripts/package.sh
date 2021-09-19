cab1e4bc56eeca975e1e8574a9b571a5e94f3ca8
Joe Bordes <joe@tsolucio.com>
2021-09-19 19:33:15 +0200

    CI(Sendgrid) delete executable files

--- vendor/sendgrid/sendgrid/scripts/package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf vendor composer.lock
-composer install --no-dev
-printf "<?php\nrequire __DIR__ . '/vendor/autoload.php';\n?>" > sendgrid-php.php
-cd ..
-zip -r sendgrid-php.zip sendgrid-php -x \*.git\* \*composer.json\* \*scripts\* \*test\* \*.travis.yml\* \*prism\*
-
-exit 0
