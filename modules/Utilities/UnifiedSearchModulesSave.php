49a8f13e6080aaef731f286d7a3af6a15861896d
joebordes <joe@tsolucio.com>
2021-10-15 01:26:53 +0200

    fix(GlobalSearch) unselect all modules

--- modules/Utilities/UnifiedSearchModulesSave.php
+++ modules/Utilities/UnifiedSearchModulesSave.php
@@ -34,39 +34,37 @@ if (isset($_REQUEST['search_onlyin'])) {
 	}
 	// Save the selection for future use (UnifiedSearchModules.php)
 	coreBOS_Session::set('__UnifiedSearch_SelectedModules__', $search_onlyin);
-	if (count($search_onlyin)>0) {
-		// we save this users preferences in a global variable
-		global $current_user, $adb;
-		include_once 'include/Webservices/Create.php';
-		$crmEntityTable = CRMEntity::getcrmEntityTableAlias('GlobalVariable');
-		$dnjoin = 'INNER JOIN '.$crmEntityTable.' ON vtiger_crmentity.crmid=vtiger_globalvariable.globalvariableid';
-		$checkrs = $adb->pquery(
-			'select globalvariableid from vtiger_globalvariable '.$dnjoin.' where vtiger_crmentity.deleted=0 and gvname=? and vtiger_crmentity.smownerid=?',
-			array('Application_Global_Search_SelectedModules',$current_user->id)
+	// we save this users preferences in a global variable
+	global $current_user, $adb;
+	include_once 'include/Webservices/Create.php';
+	$crmEntityTable = CRMEntity::getcrmEntityTableAlias('GlobalVariable');
+	$dnjoin = 'INNER JOIN '.$crmEntityTable.' ON vtiger_crmentity.crmid=vtiger_globalvariable.globalvariableid';
+	$checkrs = $adb->pquery(
+		'select globalvariableid from vtiger_globalvariable '.$dnjoin.' where vtiger_crmentity.deleted=0 and gvname=? and vtiger_crmentity.smownerid=?',
+		array('Application_Global_Search_SelectedModules',$current_user->id)
+	);
+	if ($adb->num_rows($checkrs)>0) {
+		$gvid = $adb->query_result($checkrs, 0, 0);
+		$adb->pquery(
+			'update vtiger_globalvariable set value=? where globalvariableid=?',
+			array(implode(',', $search_onlyin),$gvid)
 		);
-		if ($adb->num_rows($checkrs)>0) {
-			$gvid = $adb->query_result($checkrs, 0, 0);
-			$adb->pquery(
-				'update vtiger_globalvariable set value=? where globalvariableid=?',
-				array(implode(',', $search_onlyin),$gvid)
-			);
-		} else {
-			$wsrs=$adb->pquery('select id from vtiger_ws_entity where name=?', array('Users'));
-			if ($wsrs && $adb->num_rows($wsrs)==1) {
-				$usrwsid = $adb->query_result($wsrs, 0, 0).'x';
-			}
-			vtws_create('GlobalVariable', array(
-				'gvname' => 'Application_Global_Search_SelectedModules',
-				'default_check' => '0',
-				'value' => implode(',', $search_onlyin),
-				'mandatory' => '0',
-				'blocked' => '0',
-				'module_list' => '',
-				'category' => 'System',
-				'in_module_list' => '',
-				'assigned_user_id' => $usrwsid.$current_user->id,
-			), $current_user);
+	} else {
+		$wsrs=$adb->pquery('select id from vtiger_ws_entity where name=?', array('Users'));
+		if ($wsrs && $adb->num_rows($wsrs)==1) {
+			$usrwsid = $adb->query_result($wsrs, 0, 0).'x';
 		}
+		vtws_create('GlobalVariable', array(
+			'gvname' => 'Application_Global_Search_SelectedModules',
+			'default_check' => '0',
+			'value' => implode(',', $search_onlyin),
+			'mandatory' => '0',
+			'blocked' => '0',
+			'module_list' => '',
+			'category' => 'System',
+			'in_module_list' => '',
+			'assigned_user_id' => $usrwsid.$current_user->id,
+		), $current_user);
 	}
 }
 ?>
