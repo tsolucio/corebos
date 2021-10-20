3cccefc8b013207dde9a217c491e2373fc736682
joebordes <joe@tsolucio.com>
2021-10-20 21:57:15 +0200

    opt(Mail) reduce SQL columns

--- modules/Emails/mail.php
+++ modules/Emails/mail.php
@@ -121,7 +121,7 @@ function send_mail(
 		//setting from _email to the defined email address in the outgoing server configuration
 		$from_email = $from_email_field;
 	}
-	$user_mail_config = $adb->pquery('select * from vtiger_mail_accounts where user_id=? AND og_server_status=1', array($current_user->id));
+	$user_mail_config = $adb->pquery('select og_server_username from vtiger_mail_accounts where user_id=? AND og_server_status=1', array($current_user->id));
 	if ($user_mail_config && $adb->num_rows($user_mail_config)>0) {
 		$from_email = $adb->query_result($user_mail_config, 0, 'og_server_username');
 	}
