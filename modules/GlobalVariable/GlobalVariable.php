e0bd54309551b8dc3f0986140cacc8ec4adcaa2f
joebordes <joe@tsolucio.com>
2021-11-07 01:00:24 +0100

    doc(BusinessAction) function headers

--- modules/GlobalVariable/GlobalVariable.php
+++ modules/GlobalVariable/GlobalVariable.php
@@ -194,8 +194,8 @@ class GlobalVariable extends CRMEntity {
 
 	/**
 	 * Invoked when special actions are performed on the module.
-	 * @param String Module name
-	 * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
+	 * @param string Module name
+	 * @param string Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
 	 */
 	public function vtlib_handler($modulename, $event_type) {
 		if ($event_type == 'module.postinstall') {
