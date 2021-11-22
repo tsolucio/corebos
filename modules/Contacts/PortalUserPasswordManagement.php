c2667bbc477a7d84000da68d1bda0793249fe1c0
Joe Bordes <joe@tsolucio.com>
2021-11-23 00:27:22 +0100

    feat(Contacts) add actions buttons and password check to Password detail view widget

--- modules/Contacts/PortalUserPasswordManagement.php
+++ modules/Contacts/PortalUserPasswordManagement.php
@@ -37,21 +37,45 @@ if (!empty($crmid) && isPermitted($mod, 'Save', $crmid)=='yes') {
 	}
 	$smarty->display('applicationmessage.tpl');
 	?>
+<div class="slds-notify slds-notify_alert slds-theme_alert-texture slds-theme_error" id="show-err_msg" style="margin-bottom: 20px;display: none">
+	<span class="slds-assistive-text">error</span>
+	<h2 id="err_msg"></h2>
+	<div class="slds-notify__close">
+		<button class="slds-button slds-button_icon slds-button_icon-small slds-button_icon-inverse" onclick="document.getElementById('show-err_msg').style.display='none';">
+			<svg class="slds-button__icon" aria-hidden="true">
+				<use xlink:href="include/LD/assets/icons/utility-sprite/svg/symbols.svg#close"></use>
+			</svg>
+		</button>
+	</div>
+</div>
 <div class="slds-grid">
 <div class="slds-col">
-	<button class="slds-truncate slds-button slds-button_neutral" title="<?php echo getTranslatedString('LBL_RESET_PASSWORD'); ?>" onclick="doppSave()">
+	<button class="slds-truncate slds-button slds-button_neutral" style="border:0;" title="<?php echo getTranslatedString('LBL_RESET_PASSWORD'); ?>" onclick="doppSave()">
 		<svg class="slds-icon slds-icon_x-small slds-icon-text-default slds-m-right_x-small" aria-hidden="true">
-			<use xlink:href="include/LD/assets/icons/action-sprite/svg/symbols.svg#reset_password"></use>
+			<use xlink:href="include/LD/assets/icons/utility-sprite/svg/symbols.svg#save"></use>
 		</svg>
-		<?php echo getTranslatedString('LBL_RESET_PASSWORD'); ?>
 	</button>
 </div>
 <div class="slds-col">
-	<button class="slds-truncate slds-button slds-button_text-destructive" title="<?php echo getTranslatedString('LBL_DELETE'); ?>" onclick="doppDelete()">
+	<button class="slds-truncate slds-button slds-button_text-destructive" style="border:0;" title="<?php echo getTranslatedString('LBL_DELETE'); ?>" onclick="doppDelete()">
 		<svg class="slds-icon slds-icon_x-small slds-icon-text-default slds-m-right_x-small" aria-hidden="true">
 			<use xlink:href="include/LD/assets/icons/action-sprite/svg/symbols.svg#delete"></use>
 		</svg>
-		<?php echo getTranslatedString('LBL_DELETE'); ?>
+	</button>
+</div>
+<div class="slds-col">
+	<button class="slds-truncate slds-button slds-button_neutral" style="border:0;" title="<?php echo getTranslatedString('Generate password', 'Users'); ?>" onclick="doppGenerate()">
+		<svg class="slds-icon slds-icon_x-small slds-icon-text-default slds-m-right_x-small" aria-hidden="true">
+			<use xlink:href="include/LD/assets/icons/utility-sprite/svg/symbols.svg#password"></use>
+		</svg>
+	</button>
+</div>
+<div class="slds-col">
+	<button class="slds-truncate slds-button slds-button_neutral" style="border:0;" title="<?php echo getTranslatedString('Copy', 'EtiquetasOO'); ?>"
+	id="ppcopybtn" onclick="this.dataset.clipboardText = document.getElementById('ppasswd').value;">
+		<svg class="slds-icon slds-icon_x-small slds-icon-text-default slds-m-right_x-small" aria-hidden="true">
+			<use xlink:href="include/LD/assets/icons/utility-sprite/svg/symbols.svg#copy"></use>
+		</svg>
 	</button>
 </div>
 </div>
@@ -69,6 +93,9 @@ if (!empty($crmid) && isPermitted($mod, 'Save', $crmid)=='yes') {
 function sendPPJavascript() {
 	?>
 	<script>
+	window.doppGenerate = function () {
+		document.getElementById('ppasswd').innerHTML = corebos_Password.getPassword(12, true, true, true, true, false, true, true, true, false);
+	}
 	window.doppDelete = function () {
 		var params = `&${csrfMagicName}=${csrfMagicToken}`;
 		return fetch(
@@ -87,23 +114,44 @@ function sendPPJavascript() {
 		});
 	}
 	window.doppSave = function () {
-		var params = `&${csrfMagicName}=${csrfMagicToken}`;
-		params += '&ppasswd='+encodeURIComponent(document.getElementById('ppasswd').value);
-		return fetch(
-			'index.php?module=Contacts&action=ContactsAjax&file=PortalUserPasswordManagement&ppaction=sav&recordid='+document.getElementById('record').value,
-			{
-				method: 'post',
-				headers: {
-					'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-				},
-				credentials: 'same-origin',
-				body: params
-			}
-		).then(response => response.text())
-		.then(answer => {
-			document.getElementById('ppinfo').innerHTML = answer;
-		});
+		let err_msg = '';
+		let new_password = document.getElementById('ppasswd').value;
+		if (new_password == '') {
+			err_msg += alert_arr['ERR_ENTER_NEW_PASSWORD'];
+		}
+		if (err_msg != '') {
+			document.getElementById('show-err_msg').style.display = 'block';
+			document.getElementById('err_msg').innerHTML = err_msg;
+			return;
+		}
+		new_password = new_password.substring(0, 1024);
+		let password = corebos_Password.passwordChecker(new_password);
+		if (!password) {
+			err_msg = alert_arr['PASSWORD REQUIREMENTS NOT MET'];
+			document.getElementById('show-err_msg').style.display = 'block';
+			document.getElementById('err_msg').innerHTML = err_msg;
+		} else {
+			var params = `&${csrfMagicName}=${csrfMagicToken}`;
+			params += '&ppasswd='+encodeURIComponent(document.getElementById('ppasswd').value);
+			return fetch(
+				'index.php?module=Contacts&action=ContactsAjax&file=PortalUserPasswordManagement&ppaction=sav&recordid='+document.getElementById('record').value,
+				{
+					method: 'post',
+					headers: {
+						'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+					},
+					credentials: 'same-origin',
+					body: params
+				}
+			).then(response => response.text())
+			.then(answer => {
+				document.getElementById('ppinfo').innerHTML = answer;
+			});
+		}
 	}
+	var ppclipcopyobject = new ClipboardJS('#ppcopybtn');
+	ppclipcopyobject.on('success', function(e) { clipcopyclicked = false; });
+	ppclipcopyobject.on('error', function(e) { clipcopyclicked = false; });
 	</script>
 	<?php
 }
