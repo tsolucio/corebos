5657d150f79365050d5a3226186e503cff168fdc
denald <denalddh@gmail.com>
2022-03-29 16:01:08 +0200

    feat(DocumentFolders) create new folders method

--- modules/DocumentFolders/DocumentFolders.php
+++ modules/DocumentFolders/DocumentFolders.php
@@ -149,5 +149,21 @@ class DocumentFolders extends CRMEntity {
 			// Handle actions after this module is updated.
 		}
 	}
+
+	public static function createFolder($fname, $fid = 0) {
+		global $adb, $current_user;
+		$dbQuery = 'select 1 from vtiger_documentfolders inner join vtiger_crmentity on crmid=documentfoldersid where foldername=? and deleted=0';
+		$rs = $adb->pquery($dbQuery, array($fname));
+		if ($rs && $adb->num_rows($rs)==0) {
+			$focus = new DocumentFolders();
+			$focus->column_fields['foldername'] = $fname;
+			if ($fid > 0) {
+				$focus->column_fields['parentfolder'] = $fid;
+			}
+			$focus->saveentity('DocumentFolders');
+			return true;
+		}
+		return false;
+	}
 }
 ?>
