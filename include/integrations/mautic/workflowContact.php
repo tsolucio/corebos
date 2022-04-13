83b3e98ce7fa88ef8d75d967fbcd705a2647982a
Timothy <anthony.timothy90@gmail.com>
2022-04-13 13:36:24 +0300

    fix(mautic) Validating workflowContact Business maps

--- include/integrations/mautic/workflowContact.php
+++ include/integrations/mautic/workflowContact.php
@@ -26,51 +26,60 @@ function mauticContactCreate($entity) {
 		return;
 	}
 	if ($entity->data['mautic_id'] == '') {
+		$send2mautic = array();
 		$bmapname = 'ContactsToMautic';
 		$cbMapid = GlobalVariable::getVariable('BusinessMapping_'.$bmapname, cbMap::getMapIdByName($bmapname));
 		if ($cbMapid) {
-			$send2mautic = array();
 			$cbMap = cbMap::getMapByID($cbMapid);
 			$send2mautic = $cbMap->Mapping($entity->data, $send2mautic);
-			$send2mautic['ipAddress'] = $_SERVER['REMOTE_ADDR'];
-			$send2mautic['overwriteWithBlank'] = true;
+		} else {
+			$send2mautic['firstname'] = $entity->data['firstname'];
+			$send2mautic['lastname'] = $entity->data['lastname'];
+			$send2mautic['email'] = $entity->data['email'];
+			$send2mautic['corebos_id'] = $entity->data['id'];
+		}
+		$send2mautic['ipAddress'] = $_SERVER['REMOTE_ADDR'];
+		$send2mautic['overwriteWithBlank'] = true;
 
-			$auth = $mautic->authenticate();
-			if ($auth) {
-				$apiUrl     = $mautic->getSettings('baseUrl');
-				$api        = new MauticApi();
-				$contactApi = $api->newApi('contacts', $auth, $apiUrl);
-				$response = $contactApi->create($send2mautic);
-				$contact  = $response[$contactApi->itemName()];
-				if (!empty($contact['id'])) {
-					$core_fields = $contact['fields']['core'];
-					$social_fields = $contact['fields']['social'];
-					$fields = array_merge($core_fields, $social_fields);
+		$auth = $mautic->authenticate();
+		if ($auth) {
+			$apiUrl     = $mautic->getSettings('baseUrl');
+			$api        = new MauticApi();
+			$contactApi = $api->newApi('contacts', $auth, $apiUrl);
+			$response = $contactApi->create($send2mautic);
+			$contact  = $response[$contactApi->itemName()];
+			if (!empty($contact['id'])) {
+				$core_fields = $contact['fields']['core'];
+				$social_fields = $contact['fields']['social'];
+				$fields = array_merge($core_fields, $social_fields);
 
-					$mauticdata = array();
-					foreach ($fields as $field) {
-						$mauticdata[$field['alias']] = $field['value'];
-					}
-					$usrwsid = vtws_getEntityId('Users').'x'.$current_user->id;
+				$mauticdata = array();
+				foreach ($fields as $field) {
+					$mauticdata[$field['alias']] = $field['value'];
+				}
+				$usrwsid = vtws_getEntityId('Users').'x'.$current_user->id;
 
-					$bmapname = 'MauticToContacts';
-					$cbMapid = GlobalVariable::getVariable('BusinessMapping_'.$bmapname, cbMap::getMapIdByName($bmapname));
-					if ($cbMapid) {
-						$send2cb = array();
-						$cbMap = cbMap::getMapByID($cbMapid);
-						$send2cb = $cbMap->Mapping($mauticdata, $send2cb);
-						$send2cb['mautic_id'] = $contact['id'];
-						$send2cb['assigned_user_id'] = $usrwsid;
-						$send2cb['id'] = $entity->data['id'];
-						$send2cb['from_externalsource'] = 'mautic';
+				$send2cb = array();
+				$bmapname = 'MauticToContacts';
+				$cbMapid = GlobalVariable::getVariable('BusinessMapping_'.$bmapname, cbMap::getMapIdByName($bmapname));
+				if ($cbMapid) {
+					$cbMap = cbMap::getMapByID($cbMapid);
+					$send2cb = $cbMap->Mapping($mauticdata, $send2cb);
+				} else {
+					$send2cb['firstname'] = $mauticdata['firstname'];
+					$send2cb['lastname'] = $mauticdata['lastname'];
+					$send2cb['email'] = $mauticdata['email'];
+				}
+				$send2cb['mautic_id'] = $contact['id'];
+				$send2cb['assigned_user_id'] = $usrwsid;
+				$send2cb['id'] = $entity->data['id'];
+				$send2cb['from_externalsource'] = 'mautic';
 
-						$record = vtws_update($send2cb, $current_user);
-						if ($record) {
-							// Reset from_externalsource
-							list($contact_tabid, $contact_crmid) = explode('x', $record['id']);
-							$adb->pquery('UPDATE vtiger_contactdetails SET from_externalsource=? where contactid=?', array('', $contact_crmid));
-						}
-					}
+				$record = vtws_update($send2cb, $current_user);
+				if ($record) {
+					// Reset from_externalsource
+					list($contact_tabid, $contact_crmid) = explode('x', $record['id']);
+					$adb->pquery('UPDATE vtiger_contactdetails SET from_externalsource=? where contactid=?', array('', $contact_crmid));
 				}
 			}
 		}
@@ -82,22 +91,26 @@ function mauticContactUpdate($entity) {
 	if (!$mautic->isActive()) {
 		return;
 	}
+	$send2mautic = array();
 	$bmapname = 'ContactsToMautic';
 	$cbMapid = GlobalVariable::getVariable('BusinessMapping_'.$bmapname, cbMap::getMapIdByName($bmapname));
 	if ($cbMapid) {
-		$send2mautic = array();
 		$cbMap = cbMap::getMapByID($cbMapid);
 		$send2mautic = $cbMap->Mapping($entity->data, $send2mautic);
-		$send2mautic['ipAddress'] = $_SERVER['REMOTE_ADDR'];
-		$send2mautic['overwriteWithBlank'] = true;
+	} else {
+		$send2mautic['firstname'] = $entity->data['firstname'];
+		$send2mautic['lastname'] = $entity->data['lastname'];
+		$send2mautic['email'] = $entity->data['email'];
+	}
+	$send2mautic['ipAddress'] = $_SERVER['REMOTE_ADDR'];
+	$send2mautic['overwriteWithBlank'] = true;
 
-		$auth = $mautic->authenticate();
-		if ($auth) {
-			$apiUrl     = $mautic->getSettings('baseUrl');
-			$api        = new MauticApi();
-			$contactApi = $api->newApi('contacts', $auth, $apiUrl);
-			$contactApi->edit($entity->data['mautic_id'], $send2mautic);
-		}
+	$auth = $mautic->authenticate();
+	if ($auth) {
+		$apiUrl     = $mautic->getSettings('baseUrl');
+		$api        = new MauticApi();
+		$contactApi = $api->newApi('contacts', $auth, $apiUrl);
+		$contactApi->edit($entity->data['mautic_id'], $send2mautic);
 	}
 }
 
