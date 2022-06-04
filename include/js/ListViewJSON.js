44e97a9a3d71e6349827e22ff854ff8bdeef2563
joebordes <joe@tsolucio.com>
2022-06-04 17:20:53 +0200

    fix(ListView) supprt Application_DetailView_Inline_Edit on tuigrid

--- include/js/ListViewJSON.js
+++ include/js/ListViewJSON.js
@@ -20,10 +20,15 @@ let lvdataGridInstance = Array();
 let SearchColumns = 0;
 let ListViewCopy = 0;
 let Application_Filter_All_Edit = 1;
+let Application_DetailView_Inline_Edit = 1;
 let DocumentFolderView = 1;
 let Application_MassAction_Multipage = 0;
 let lastPage = sessionStorage.getItem(gVTModule+'_lastPage');
 let urlParams = new URLSearchParams(window.location.search);
+GlobalVariable_getVariable('Application_DetailView_Inline_Edit', 1).then(function (response) {
+	let obj = JSON.parse(response);
+	Application_DetailView_Inline_Edit = parseInt(obj.Application_DetailView_Inline_Edit);
+});
 GlobalVariable_getVariable('Application_ListView_PageSize', 20, gVTModule, '').then(function (response) {
 	let obj = JSON.parse(response);
 	PageSize = obj.Application_ListView_PageSize;
@@ -182,7 +187,7 @@ const ListView = {
 				edit = false;
 				sortable = false;
 			}
-			if (edit) {
+			if (edit && Application_DetailView_Inline_Edit) {
 				if (uitype == 10) {
 					ListView.RelatedModule = headerObj[index].relatedModule;
 				}
