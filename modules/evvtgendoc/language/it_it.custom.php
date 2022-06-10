96569cc77485bcb6c8412d5c5445b7cb75427165
mukades.sharka <m.sharka@spikeassociates.com>
2022-06-10 13:02:16 +0200

    i18n() custom translations

---
+++ modules/evvtgendoc/language/it_it.custom.php
@@ -0,0 +1,51 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2016 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************/
+
+$custom_strings = array(
+	'SINGLE_evvtgendoc' => 'Generazione documenti',
+	'evvtgendoc' => 'Generazione documenti',
+	'OOMerge GENDOC' => 'Generare documenti',
+	'LBL_SELECT_TEMPLATE' => 'Si prega di selezionare un modello!',
+	'LBL_INFORMATION' => 'Informazioni per ',
+	'LBL_WITH' => 'con ID',
+	'LBL_TEMPLATE' => 'utilizzando modello',
+	'LBL_DOCUMENT_TEMPLATE' => 'Modello Documento:',
+	'LBL_NOTEMPLATE' => 'Non esiste modello! Aggiungi un nuovo modello documento.',
+	'GenXMLTplRequired' => 'Nome file modello XML richiesto come parametro: genxmltemplate',
+	'GenXMLIDRequired' => 'Entità modello XML CRMID richiesta come parametro: recordval',
+	'Export PDF' => 'Genera PDF',
+	'EMail PDF' => 'Inviare PDF',
+	'Save PDF' => 'Salvare PDF',
+	'Export Doc' => 'Genera Documento',
+	'EMail Doc' => 'Invia Documento',
+	'Save Doc' => 'Salva Documento',
+	'LBL_CONFIGURATION_DESCRIPTION' => 'Attivare/Disattiva la funzionalità per ogni modulo.',
+	'LBL_evvtgendoc_SETTINGS' => 'Moduli Attivati',
+	'LBL_evvtgendoc_SERVERSETTINGS' => 'Impostazioni Server',
+	'LBL_SERVERCONFIGURATION_DESCRIPTION' => 'Impostazioni di accesso servizio esterno, da convertire/Documenti processo.',
+	'Evvtgendoc_title' => 'Impostazioni Server',
+	'Evvtgendoc_subtitle' => 'Impostazioni di accesso servizio esterno, da convertire/Documenti processo.',
+	'LBL_URL_SERVER' => 'URL Servizio Esterno',
+	'LBL_ACCESSKEY' => 'Chiave Accesso',
+	'External Server' => 'Uso Server Esterno',
+	'LBL_DEBUG' => 'Debug',
+	'LBL_LANG' => 'Lingua',
+	'UpdatePrevious' => 'Aggiorna Unione Precedente',
+	'shareOnCreate' => 'Condividi Dopo Creazione',
+	'is set to' => 'è impostato a',
+	'is not set' => 'NON è impostato',
+);
+?>
---
+++ modules/pricebookproductrel/language/it_it.custom.php
@@ -0,0 +1,29 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$custom_strings = array(
+	'pricebookproductrel' => 'Listino Prezzi',
+	'SINGLE_pricebookproductrel' => 'Listino Prezzi',
+	'pricebookproductrel ID' => 'Listino Prezzi ID',
+	'LBL_CUSTOM_INFORMATION' => 'Informazione Personalizzata',
+	'LBL_PBPDO_INFORMATION' => 'Listino Prezzi Informazione',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descrizione',
+	'pricebookid' => 'Listino',
+	'pricebookproductrel_no' => 'Listino Prezzi No',
+	'productid' => 'Prodotto',
+	'listprice' => 'Prezzo',
+	'usedcurrency' => 'Valuta',
+	'Assigned To' => 'Assegnato a ',
+	'Created By' => 'Creato da',
+	'Created Time' => 'Orario creazione',
+	'Modified Time' => 'Orario modifica',
+	'Description' => 'Descrizione',
+);
+?>
