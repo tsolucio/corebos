50e79f37a5b9c1289bb58cb3580043afdf220096
Mohamed Abdallah Shibe <mohamed13abdallah@gmail.com>
2022-07-31 10:33:40 +0300

    fix(csrfmagic) fix by use check by icoming hostname

--- include/csrfmagic/csrf-magic.php
+++ include/csrfmagic/csrf-magic.php
@@ -201,8 +201,8 @@ function csrf_ob_handler($buffer, $flags) {
  * @return boolean true if check passes or is not necessary, false if failure.
  */
 function csrf_check($fatal = true) {
-	$check_query_string = strpos($_SERVER['QUERY_STRING'], '_operationarg');
-	if (empty($check_query_string) && (empty($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] !== 'POST')) {
+	global $site_URL;
+	if ((strpos($site_URL, $_SERVER['HTTP_HOST']) !== false) && (empty($_SERVER['REQUEST_METHOD']) || $_SERVER['REQUEST_METHOD'] !== 'POST')) {
 		return true;
 	}
 	$valid_ips = explode(',', GlobalVariable::getVariable('Application_CSRF_Valid_IP', '', '', Users::getActiveAdminId()));
