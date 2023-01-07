001d9485e1b7a53306c430c77ffcf60c88b7a041
joebordes <joe@tsolucio.com>
2023-01-08 00:44:54 +0100

    php(App) class properties for PHP 8.2

--- include/QueryGenerator/QueryGenerator.php
+++ include/QueryGenerator/QueryGenerator.php
@@ -35,6 +35,7 @@ class QueryGenerator {
 	private $fields;
 	private $addJoinFields;
 	private $referenceModuleMetaInfo;
+	private $referenceModuleField;
 	private $moduleNameFields;
 	private $referenceFieldInfoList;
 	private $referenceFieldList;
