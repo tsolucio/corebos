1ca21c778a70b0d8ae31bae338dd943735e400ce
Radiola <r.dervishaj@spikeassociates.com>
2023-01-10 14:23:33 +0100

    add language files

---
+++ modules/ModuleBPMInfo/language/it_it.custom.php
@@ -0,0 +1,52 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$custom_strings = array(
+	'ModuleBPMInfo' => 'Compenenti Cassetta',
+	'SINGLE_ModuleBPMInfo' => 'Compenenti Cassetta Info',
+	'ModuleBPMInfo ID' => 'Compenenti Cassetta ID',
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_MODULEBPMINFO_INFORMATION' => 'Compenenti Cassetta Information',
+	'LBL_STATS_INFORMATION' => 'Information Statistics',
+	'LBL_USER_INFORMATION' => 'User Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description',
+	'bpminforef' => 'Reference',
+	'bpminfo_no' => 'Compenenti Cassetta No',
+	'bpminfomaster' => 'Related Produkt Component',
+	'timetofill' => 'Fill Time',
+	'Assigned To' => 'Assegnato a ',
+	'Created By' => 'Creato da',
+	'Created Time' => 'Orario creazione',
+	'Modified Time' => 'Orario modifica',
+	'Description' => 'Descrizione',
+	'COPERCHIO LEGG' => 'COPERCHIO LEGG',
+	'COPERCHIO PES' => 'COPERCHIO PES',
+	'CERNIERA' => 'CERNIERA',
+	'Piastra' => 'Piastra',
+	'Staffe' => 'Staffe',
+	'IP66' => 'IP66',
+	'Verniciatura Esterna' => 'Verniciatura Esterna',
+	'Verniciatura Interna' => 'Verniciatura Interna',
+	'Quantity' => 'Quantita',
+	'Posizione' => 'Posizione',
+	'Costo Materiali' => 'Costo Materiali',
+	'Costo Lavoro' => 'Costo Lavoro',
+	'Tempo di Lavoro' => 'Tempo di Lavoro',
+	'Totale dei Costi' => 'Totale dei Costi',
+	'Posizione1' => 'Posizione1',
+	'Posizione2' => 'Posizione2',
+	'Posizione3' => 'Posizione3',
+	'Posizione4' => 'Posizione4',
+	'Quantity4' => 'Quantity4',
+	'Quantity2' => 'Quantity2',
+	'Quantity3' => 'Quantity3',
+	'Quantity1' => 'Quantity1',
+);
+?>
