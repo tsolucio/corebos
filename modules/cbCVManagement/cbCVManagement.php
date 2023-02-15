e9028926c44ffe68c3babee1161403dd400fc8ff
malik najjar <malik446644@gmail.com>
2023-02-15 17:27:04 +0100

    feat(customViewFilter) Intergrate cbCVManagement into filter's customView

--- modules/cbCVManagement/cbCVManagement.php
+++ modules/cbCVManagement/cbCVManagement.php
@@ -403,6 +403,39 @@ class cbCVManagement extends CRMEntity {
 				$allViews[] = $value['cvid'];
 			}
 		}
+
+		// push all public filters if the user is admin
+		if ($current_user->id == 1) {
+			$cvsql = 'SELECT vtiger_cbcvmanagement.cvid FROM `vtiger_cbcvmanagement` WHERE setpublic = 1 AND module_list = ?';
+			$queryResult = $adb->pquery($cvsql, array($module));
+			while ($row=$adb->fetch_array($queryResult)) {
+				$allViews[] = $row['cvid'];
+			}
+		}
+
+		// push all approved public filters
+		$cvsql = "SELECT * FROM `vtiger_customview` WHERE status = 3 AND entitytype = ?";
+		$queryResult = $adb->pquery($cvsql, array($module));
+		while ($row=$adb->fetch_array($queryResult)) {
+			$allViews[] = $row['cvid'];
+		}
+
+		// filter private records views
+		$cvsql = 'SELECT vtiger_cbcvmanagement.cvid, vtiger_customview.userid 
+			FROM `vtiger_cbcvmanagement` 
+			INNER JOIN vtiger_customview 
+			ON vtiger_cbcvmanagement.cvid = vtiger_customview.cvid 
+			WHERE setprivate = 1';
+		$queryResult = $adb->query($cvsql);
+		while ($row=$adb->fetch_array($queryResult)) {
+			for ($i=0; $i < count($allViews); $i++) {
+				if ($allViews[$i] == $row['cvid'] && $row['userid'] !== $cvuserid) {
+					unset($allViews[$i]);
+					$allViews = array_values($allViews);
+				}
+			}
+		}
+
 		VTCacheUtils::updateCachedInformation($key, $value);
 		return array_unique($allViews);
 	}
@@ -478,6 +511,23 @@ class cbCVManagement extends CRMEntity {
 		if (empty($cvuserid)) {
 			return false;
 		}
+
+		// checks if the record is Private
+		$cvsql = 'SELECT vtiger_cbcvmanagement.cvid, vtiger_customview.userid 
+			FROM `vtiger_cbcvmanagement` 
+			INNER JOIN vtiger_customview 
+			ON vtiger_cbcvmanagement.cvid = vtiger_customview.cvid 
+			WHERE setprivate = 1 AND vtiger_cbcvmanagement.cvid = ? AND vtiger_customview.userid != ?';
+		$queryResult = $adb->pquery($cvsql, array($cvid, $cvuserid));
+		if ($adb->num_rows($queryResult)) {
+			return array('C' => 0, 'R' => 0, 'U' => 0, 'D' => 0, 'A' => 0);
+		}
+
+		// giving all permissions if its the admin user
+		if ($cvuserid == 1) {
+			return array('C' => 1, 'R' => 1, 'U' => 1, 'D' => 1, 'A' => 1);
+		}
+
 		$module = $adb->query_result($cvrs, 0, 'entitytype');
 		$cvname = $adb->query_result($cvrs, 0, 'viewname');
 		$cvuid = $adb->query_result($cvrs, 0, 'userid');
@@ -604,6 +654,15 @@ class cbCVManagement extends CRMEntity {
 
 	public static function getValidationInfo() {
 		return self::$validationinfo;
+	}
+
+	public static function getFieldValuesByCvId($cvid) {
+		global $adb;
+		$result = $adb->pquery("SELECT * FROM `vtiger_cbcvmanagement` WHERE cvid = ?", array($cvid));
+		if (!$result) {
+			return false;
+		}
+		return $result->fields;
 	}
 }
 ?>
