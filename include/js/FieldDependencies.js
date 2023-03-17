46c2cf916648c64596ed667e31464897571806dd
joebordes <joe@tsolucio.com>
2023-03-17 15:30:24 +0100

    fix(FieldDep) avoid assigning values to fields we cannot find

--- include/js/FieldDependencies.js
+++ include/js/FieldDependencies.js
@@ -512,13 +512,16 @@ FieldDependencies.prototype.fieldHideDetailView = function (hideFields) {
 	var field='';
 	for (var i=0; i<hideFields.length; i++) {
 		field=hideFields[i]['field'];
-		document.getElementById('mouseArea_'+field).style.visibility='hidden';
-		document.getElementById('mouseArea_'+field).previousSibling.previousSibling.style.visibility='hidden';
-		let parentRow = document.getElementById('mouseArea_'+field).parentElement;
-		let allGone = [...parentRow.getElementsByTagName('TD')].filter(cell => cell.style.visibility === 'hidden');
-		if (allGone.length == 4) {
-			parentRow.style.visibility = 'collapse';
-			parentRow.style.height = '0px';
+		let hfld = document.getElementById('mouseArea_'+field);
+		if (hfld) {
+			hfld.style.visibility='hidden';
+			hfld.previousSibling.previousSibling.style.visibility='hidden';
+			let parentRow = hfld.parentElement;
+			let allGone = [...parentRow.getElementsByTagName('TD')].filter(cell => cell.style.visibility === 'hidden');
+			if (allGone.length == 4) {
+				parentRow.style.visibility = 'collapse';
+				parentRow.style.height = '0px';
+			}
 		}
 	}
 };
