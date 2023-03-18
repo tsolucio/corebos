b7a8e509435ba73e0efbff9f6cb1d87a2b366fce
joebordes <joe@tsolucio.com>
2023-03-18 20:47:58 +0100

    cbSR(products) whitespace in multifile script

--- modules/Products/multifile.js
+++ modules/Products/multifile.js
@@ -38,8 +38,7 @@
  *      [for duplicate name bug]
  *         'neal'
  */
-function MultiSelector( list_target, max ){
-
+function MultiSelector(list_target, max) {
 	// Where to write the list
 	this.list_target = list_target;
 	// How many elements?
@@ -56,10 +55,9 @@ function MultiSelector( list_target, max ){
 	/**
 	 * Add a new file input element
 	 */
-	this.addElement = function ( element ) {
-
+	this.addElement = function (element) {
 		// Make sure it's a file input element
-		if ( element.tagName == 'INPUT' && element.type == 'file' ) {
+		if (element.tagName == 'INPUT' && element.type == 'file') {
 			// Element name -- what number am I?
 			element.name = 'file_' + this.id++;
 
@@ -68,7 +66,6 @@ function MultiSelector( list_target, max ){
 
 			// What to do when a file is selected
 			element.onchange = function () {
-
 				// New file input
 				var new_element = document.createElement('input');
 				new_element.type = 'file';
@@ -81,7 +78,7 @@ function MultiSelector( list_target, max ){
 				if (typeof(element.form) != 'undefined') {
 					new_element_hdn = element.form ['\'' + this.name + '_hidden' + '\''];
 				}
-				if ( new_element_hdn == null ) {
+				if (new_element_hdn == null) {
 					new_element_hdn = document.createElement('input');
 					new_element_hdn.name = this.name + '_hidden';
 					new_element_hdn.type = 'hidden';
@@ -100,10 +97,9 @@ function MultiSelector( list_target, max ){
 				// Hide this: we can't use display:none because Safari doesn't like it
 				this.style.position = 'absolute';
 				this.style.left = '-1000px';
-
 			};
 			// If we've reached maximum number, disable input element
-			if ( this.max != -1 && this.count >= this.max ) {
+			if (this.max != -1 && this.count >= this.max) {
 				element.disabled = true;
 			}
 
@@ -120,8 +116,7 @@ function MultiSelector( list_target, max ){
 	/**
 	 * Add a new row to the list of files
 	 */
-	this.addListRow = function ( element ) {
-
+	this.addListRow = function (element) {
 		// Row div
 		var new_row = document.createElement('div');
 
@@ -136,7 +131,6 @@ function MultiSelector( list_target, max ){
 
 		// Delete function
 		new_row_button.onclick= function () {
-
 			// Remove element from form
 			this.parentNode.element.parentNode.removeChild(this.parentNode.element);
 
