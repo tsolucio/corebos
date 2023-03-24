6f4108bd0c15f9d16b562c3cf7ca00e95dc4f2ba
denald <denalddh@gmail.com>
2023-03-24 17:23:01 +0100

    feat(Workflow) add workflow api

---
+++ modules/com_vtiger_workflow/WorkflowAPI.php
@@ -0,0 +1,32 @@
+<?php
+/*************************************************************************************************
+ * Copyright 2023 JPL TSolucio, S.L. -- This file is a part of TSOLUCIO coreBOS Customizations.
+ * Licensed under the vtiger CRM Public License Version 1.1 (the "License"); you may not use this
+ * file except in compliance with the License. You can redistribute it and/or modify it
+ * under the terms of the License. JPL TSolucio, S.L. reserves all rights not expressly
+ * granted by the License. coreBOS distributed by JPL TSolucio S.L. is distributed in
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Unless required by
+ * applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License. You may obtain a copy of the License
+ * at <http://corebos.org/documentation/doku.php?id=en:devel:vpl11>
+ *************************************************************************************************
+ *  Author       : JPL TSolucio, S. L.
+ *************************************************************************************************/
+require_once 'include/utils/utils.php';
+require_once 'VTWorkflow.php';
+if (!isset($_REQUEST['operation'])) {
+	echo json_encode(false);
+	exit;
+}
+$focus = new Workflow();
+switch ($_REQUEST['operation']) {
+	case 'getModules':
+		$ret = $focus->getModuleList();
+		break;
+	default:
+		break;
+}
+echo json_encode($ret);
